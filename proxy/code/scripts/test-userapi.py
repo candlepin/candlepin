bf9e1bdf9bb0d3498259b8ac183b3365f935eabe
jesus m. rodriguez <jesusr@redhat.com>
2009-07-31 13:33:56 -0400

    use json instead of a string

--- proxy/code/scripts/test-userapi.py
+++ proxy/code/scripts/test-userapi.py
@@ -2,9 +2,21 @@
 
 import httplib, urllib
 import sys
+import simplejson as json
 
-# GET candlepin user
-response = urllib.urlopen('http://localhost:8080/candlepin/user/candlepin')
+# POST new user
+params = {"login":"testapi","password":"sw3etnothings"}
+headers = {"Content-type":"application/json",
+           "Accept": "application/json"}
+conn = httplib.HTTPConnection("localhost", 8080)
+conn.request("POST", '/candlepin/user/', json.dumps(params), headers)
+response = conn.getresponse()
+print("Status: %d Response: %s" % (response.status, response.reason))
+rsp = response.read()
+conn.close()
+
+# GET testapi user
+response = urllib.urlopen('http://localhost:8080/candlepin/user/testapi')
 rsp = response.read()
 print("get: %s" % rsp)
 
