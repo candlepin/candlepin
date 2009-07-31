c814a67b6c67a43e920975fdd6e66b42fe3fbab5
jesus m. rodriguez <jesusr@redhat.com>
2009-07-31 13:33:26 -0400

    use correct urls

--- proxy/code/scripts/test-testapi.py
+++ proxy/code/scripts/test-testapi.py
@@ -2,18 +2,19 @@
 
 import httplib, urllib
 import sys
+import simplejson as json
 
 if len(sys.argv) < 1:
     print("please supply a message")
     sys.exit(1)
 
 print("------------ TESTING json create")
-params = '{"name":"now","uuid":"thiswork"}'
+params = {"name":"now","uuid":"thiswork"}
 headers = {"Content-type":"application/json",
            "Accept": "application/json"}
 conn = httplib.HTTPConnection("localhost", 8080)
 print("creating object with %s" % params)
-conn.request("POST", '/candlepin/test/', params, headers)
+conn.request("POST", '/candlepin/test/', json.dumps(params), headers)
 response = conn.getresponse()
 print("Status: %d Response: %s" % (response.status, response.reason))
 rsp = response.read()
