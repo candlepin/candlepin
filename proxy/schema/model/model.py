95da771375a35edfecf89117a12ea0f93f9b9535
jesus m. rodriguez <jesusr@redhat.com>
2009-11-04 16:14:45 -0500

    initial cut at schema

--- proxy/schema/model/model.py
+++ proxy/schema/model/model.py
@@ -4,56 +4,35 @@ from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy.orm import mapper
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///:memory:', echo=True)
-#engine = create_engine('postgres://mmccune:mmccune@127.0.0.1:5432/candlepin')
+#engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('postgres://candlepin:candlepin@127.0.0.1:5432/candlepin')
 
 metadata = MetaData()
 
-org_table = Table('org', metadata, 
+owner_table = Table('cp_owner', metadata, 
 	Column('uuid', String, primary_key=True),
 	Column('name', String))
 
-entitlement_table = Table('entitlement', metadata, 
+entitlement_table = Table('cp_entitlement', metadata, 
 	Column('uuid', String, primary_key=True),
 	Column('name', String),
-	Column('org', String, ForeignKey('org.uuid')),
-	Column('parent', String, ForeignKey('entitlement.uuid')))
+	Column('owner', String, ForeignKey('cp_owner.uuid')),
+	Column('parent', String, ForeignKey('cp_entitlement.uuid')))
 
-consumer_table = Table('consumer', metadata, 
+consumer_table = Table('cp_consumer', metadata, 
 	Column('uuid', String, primary_key=True),
 	Column('name', String),
-	Column('type', String),
-	Column('org', String, ForeignKey('org.uuid')),
-	Column('parent', String, ForeignKey('consumer.uuid')))
+	Column('owner', String, ForeignKey('cp_owner.uuid')),
+	Column('parent', String, ForeignKey('cp_consumer.uuid')))
 
-product_definition_table = Table('product_definition', metadata, 
+product_definition_table = Table('cp_product', metadata, 
 	Column('uuid', String, primary_key=True),
 	Column('name', String))
 
-entitlement_pool_table = Table('entitlement_pool', metadata, 
+entitlement_pool_table = Table('cp_entitlement_pool', metadata, 
 	Column('uuid', String, primary_key=True),
 	Column('name', String),
-	Column('org', String, ForeignKey('org.uuid')),
-	Column('product_definition', String, ForeignKey('product_definition.uuid')))
+	Column('owner', String, ForeignKey('cp_owner.uuid')),
+	Column('product', String, ForeignKey('cp_product.uuid')))
 
 metadata.create_all(engine)
-
-
-class Org(object):
-	def __init__(self, uuid, name):
-		self.uuid = uuid
-		self.name = name
-
-	def __repr__(self):
-		return "<Org('%s','%s')>" % (self.uuid, self.name)
-
-mapper(Org, org_table) 
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-org1 = Org('289489090234', 'test org')
-session.add(org1)
-
-lookedup = session.query(Org).filter_by(name='test org').first()
-print lookedup.uuid
