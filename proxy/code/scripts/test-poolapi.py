6a5cf056d3493d40716c2ec1c487862301c7d427
jesus m. rodriguez <jesusr@redhat.com>
2009-11-16 16:30:31 -0500

    adding scripts to test entitlement and pool api

--- proxy/code/scripts/test-poolapi.py
+++ proxy/code/scripts/test-poolapi.py
@@ -7,3 +7,7 @@ import simplejson
 response = urllib.urlopen('http://localhost:8080/candlepin/entitlementpool')
 rsp = response.read()
 print("list: %s" % rsp)
+pool = simplejson.loads(rsp)
+print(type(pool))
+for (k, v) in pool.iteritems():
+    print("pool[%s] = '%s'" % (k, v))
