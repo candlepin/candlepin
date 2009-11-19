b3f1d75169c16cae604e6ed6f07e4c6db359f7d1
jesus m. rodriguez <jesusr@redhat.com>
2009-11-18 23:19:28 -0500

    pool api that reads from certificate

--- proxy/code/scripts/test-poolapi.py
+++ proxy/code/scripts/test-poolapi.py
@@ -6,8 +6,12 @@ import simplejson
 
 response = urllib.urlopen('http://localhost:8080/candlepin/entitlementpool')
 rsp = response.read()
-print("list: %s" % rsp)
+#print("list: %s" % rsp)
 pool = simplejson.loads(rsp)
-print(type(pool))
-for (k, v) in pool.iteritems():
-    print("pool[%s] = '%s'" % (k, v))
+#print(type(pool))
+for p in pool['entitlementPool']:
+    prod = p['product']
+    print("%s with qty of %s valid from %s to %s" % (prod['name'], p['maxMembers'], p['startDate'], p['endDate']))
+    #print(p)
+#for (k, v) in pool.iteritems():
+#    print("pool[%s] = '%s'" % (k, v))
