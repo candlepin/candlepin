678bcb6c065a6ecac89f429309d46b0888299189
Adrian Likins <adrian@alikins.usersys.redhat.com>
2009-11-20 12:54:07 -0500

    call owner/$uuid  (aka, OwnerResource.get())

--- proxy/code/scripts/test-ownerapi.py
+++ proxy/code/scripts/test-ownerapi.py
@@ -3,8 +3,21 @@
 import httplib, urllib
 import sys
 import simplejson
+import pprint
 
+
+# test OwnerResourse.list
 response = urllib.urlopen('http://localhost:8080/candlepin/owner')
 rsp = response.read()
 print("get: %s" % rsp)
 
+result = simplejson.loads(rsp)
+pprint.pprint(result)
+
+
+# test OwnerResourse.get
+for owner in result['owner']:
+    response = urllib.urlopen('http://localhost:8080/candlepin/owner/%s' % owner['uuid'])
+    rsp = response.read()
+    result = simplejson.loads(rsp)
+    pprint.pprint(result)
