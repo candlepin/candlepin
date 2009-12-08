f2d2125c147caf0387b87d0c5c92ac042e9a351c
jesus m. rodriguez <jesusr@redhat.com>
2009-12-08 15:31:51 -0500

    checkstyle: header fail, static not ALLCAPS, needed private ctor.

--- proxy/code/src/org/fedoraproject/candlepin/util/EntityManagerUtil.java
+++ proxy/code/src/org/fedoraproject/candlepin/util/EntityManagerUtil.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package org.fedoraproject.candlepin.util;
 
 import javax.persistence.EntityManager;
@@ -8,7 +22,11 @@ public class EntityManagerUtil {
     
     public static final String DEFAULT_PERSISTENCE_UNIT = "production";
     public static final String PERSISTENCE_UNIT_PROPERTY = "persistenceUnit";
-    public static final EntityManagerFactory emf = buildEntityManagerFactory();
+    public static final EntityManagerFactory EMF = buildEntityManagerFactory();
+
+    private EntityManagerUtil() {
+        // do nothing
+    }
     
     private static EntityManagerFactory buildEntityManagerFactory() {
         // Allow test environments to override the persistence unit to load 
@@ -16,13 +34,13 @@ public class EntityManagerUtil {
         String persistenceUnit = System.getProperty(PERSISTENCE_UNIT_PROPERTY, 
                 DEFAULT_PERSISTENCE_UNIT);
         System.out.println("Loading persistence unit: " + persistenceUnit);
-        EntityManagerFactory emf = 
+        EntityManagerFactory EMF =
             Persistence.createEntityManagerFactory(persistenceUnit);
-        return emf;
+        return EMF;
     }
     
     public static EntityManager createEntityManager() {
-        return emf.createEntityManager();
+        return EMF.createEntityManager();
     }
 
 }
