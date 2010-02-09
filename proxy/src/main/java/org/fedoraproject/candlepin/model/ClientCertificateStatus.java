8b154f23021f9031c1354d482e577a185b7b36d1
Adrian Likins <adrian@alikins.usersys.redhat.com>
2010-02-09 13:51:20 -0500

    Make these objects implement Persisted (add id and get/set id)

--- proxy/src/main/java/org/fedoraproject/candlepin/model/ClientCertificateStatus.java
+++ proxy/src/main/java/org/fedoraproject/candlepin/model/ClientCertificateStatus.java
@@ -2,6 +2,9 @@ package org.fedoraproject.candlepin.model;
 
 
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -9,16 +12,33 @@ import javax.xml.bind.annotation.XmlRootElement;
 /**
  * Represents a client X509 certificate, used to obtain access to some content.
  */
-@XmlRootElement(name="certs")
+@XmlRootElement(name="clientCertStatus")
 @XmlAccessorType(XmlAccessType.PROPERTY)
-public class ClientCertificateStatus {
+public class ClientCertificateStatus implements Persisted {
     public String serialNumber;
     public String status;
-    public ClientCertificate clientCertificate;
+    public ClientCertificate certificate;
+    private Long id;
     
     public ClientCertificateStatus() {
-        
+     
     }
+    
+    public ClientCertificateStatus(String serialNumber, String status, ClientCertificate clientCertificate) {
+        this.serialNumber = serialNumber;
+        this.status = status;
+        this.certificate = clientCertificate;
+    }
+    
+    public ClientCertificate getClientCertificate() {
+        return certificate;
+    }
+
+    public void setClientCertificate(ClientCertificate clientCertificate) {
+        this.certificate = clientCertificate;
+    }
+
+  
 
     public String getSerialNumber() {
         return serialNumber;
@@ -34,6 +54,16 @@ public class ClientCertificateStatus {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    @Override
+    public Long getId() {
+        // TODO Auto-generated method stub
+        return this.id;
     };
     
     
