674194c37331e6e09dbfa30bde10aba3bfcfb848
Adrian Likins <adrian@alikins.usersys.redhat.com>
2010-02-15 13:54:35 -0500

    Make this actually check against the consumer to return only it's certs, not all.

--- proxy/src/main/java/org/fedoraproject/candlepin/resource/EntitlementResource.java
+++ proxy/src/main/java/org/fedoraproject/candlepin/resource/EntitlementResource.java
@@ -72,6 +72,8 @@ public class EntitlementResource {
         this.prodAdapter = prodAdapter;
         this.entitler = entitler;
     }
+    
+    
 
     private void verifyExistence(Object o, String id) {
         if (o == null) {
@@ -179,10 +181,10 @@ public class EntitlementResource {
 
 //        log.debug("consumerCurator: " + consumerCurator.toString());
 //        log.debug("epCurator: " + epCurator.toString());
-//        Consumer consumer = consumerCurator.find(consumerUuid);
+        Consumer consumer = consumerCurator.find(consumerUuid);
 //        log.debug("consumer: " + consumer.toString());
-//        return epCurator.listByConsumer(consumer);
-        return epCurator.findAll();
+        return epCurator.listByConsumer(consumer);
+//        return epCurator.findAll();
         
 //        Consumer c = consumerCurator.find(consumerId);
 //        List<EntitlementPool> entitlementPools = epCurator.findAll();
