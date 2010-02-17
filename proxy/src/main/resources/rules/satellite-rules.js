8c77cca2bc34f1d12968ef00be78e536971bf182
Adrian Likins <adrian@alikins.usersys.redhat.com>
2010-02-17 17:04:14 -0500

    Add defaults rules file to the properly place to install in the war

---
+++ proxy/src/main/resources/rules/satellite-rules.js
@@ -0,0 +1,53 @@
+// Checks common for both virt host and virt platform entitlements:
+function virtualization_common() {
+	pre.checkQuantity(pool);
+	
+	// Can only be given to a physical system:
+	if (consumer.getType() != "system") {
+		pre.addError("rulefailed.virt.ents.only.for.physical.systems");
+		return;
+	}
+	
+	// Host must not have any guests currently (could be changed but for simplicities sake):
+	if (parseInt(consumer.getFact("total_guests")) > 0) {
+		pre.addError("rulefailed.host.already.has.guests");
+	}
+}
+
+function pre_virtualization_host() {
+	virtualization_common();
+}
+
+function post_virtualization_host() {
+	post_global();
+	post.createConsumerPool("virt_guest", product.getAttribute("allowed_guests"));
+}
+function pre_virtualization_host_platform() {
+	virtualization_common();
+}
+
+function post_virtualization_host_platform() {
+	// unlimited guests:
+	post_global();
+	post.createConsumerPool("virt_guest", product.getAttribute("allowed_guests"));
+}
+
+function pre_global() {
+	
+	// Support free entitlements for guests, if their parent has virt host or platform,
+	// and is entitled to the product the guest is requesting:
+	if (consumer.getType() == "virt_system" && consumer.getParent() != null) {
+		if ((consumer.getParent().hasEntitlement("virtualization_host") || 
+				consumer.getParent().hasEntitlement("virtualization_host_platform")) && 
+				consumer.getParent().hasEntitlement(product.getLabel())) {
+			pre.grantFreeEntitlement();
+		}
+	}
+	else {
+		pre.checkQuantity(pool);
+	}
+}
+
+function post_global() {
+	
+}
