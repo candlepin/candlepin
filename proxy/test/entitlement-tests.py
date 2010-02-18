a131b3040dc4b1d9c3a7415f7faf1d214c004da2
Wes Hayutin <whayutin@redhat.com>
2010-02-18 10:49:03 -0500

    added a couple tests just to get started.  - test uuid  - test get cert

--- proxy/test/entitlement-tests.py
+++ proxy/test/entitlement-tests.py
@@ -7,8 +7,10 @@ from testfixture import CandlepinTests
 
 class EntitlementTests(CandlepinTests):
 
-    def setUp(self):
-        conn = httplib.HTTPConnection("localhost", 8080)
+ def setUp(self):
+        
+	#self.conn = httplib.HTTPConnection("localhost", 8080)
+	self.conn = httplib.HTTPConnection("statler.usersys.redhat.com", 8080)
         # Create a consumer and store it's UUID:
         consumer_type = {"label": "system"}
         facts_metadata = {"entry": [
@@ -17,14 +19,30 @@ class EntitlementTests(CandlepinTests):
             {"key": "total_guests", "value": "0"},
         ]}
         facts = {"metadata": facts_metadata}
-        params = {"type": consumer_type, "name": "helloworld", "facts": facts}
-        headers = {"Content-type": "application/json",
+        self.params = {"type": consumer_type, "name": "helloworld", "facts": facts}
+        self.headers = {"Content-type": "application/json",
                    "Accept": "application/json"}
-        conn.request("POST", '/candlepin/consumer/', json.dumps(params), headers)
-        response = conn.getresponse()
-        rsp = response.read()
-        conn.close()
-        self.consumer_uuid = json.loads(rsp)['uuid']
+        self.conn.request("POST", '/candlepin/consumer/', json.dumps(self.params), self.headers)
+        self.response = self.conn.getresponse()
+        self.rsp = self.response.read()
+        self.consumer_uuid = json.loads(self.rsp)['uuid']
+        self.conn.close()
+	#TODO: remove print lines after tests have been added
+	print('conn.request='+self.rsp) 
 
-    def test_something(self):
-        pass
+ def test_uuid(self):
+        if self.consumer_uuid != None:
+	    pass
+	else:
+	    assert False
+   
+ def test_dbid(self):
+        self.conn.request("GET", '/candlepin/consumer/'+self.consumer_uuid+'/certificates', json.dumps(self.params), self.headers)
+        self.response = self.conn.getresponse()
+        self.rsp = self.response.read()
+        self.consumer_cert = json.loads(self.rsp)['certs']
+   	if self.consumer_cert != None:
+	    pass
+	else:
+	    assert False		
+
