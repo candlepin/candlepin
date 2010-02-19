afa436908b18930308ce6b426e611b60253650c8
Dmitri Dolguikh <ddolguik@redhat.com>
2010-02-19 13:23:04 -0400

    added scaffolding for a basic auth filter

---
+++ proxy/src/main/java/org/fedoraproject/candlepin/auth/servletfilter/BasicAuthFilter.java
@@ -0,0 +1,35 @@
+package org.fedoraproject.candlepin.auth.servletfilter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.log4j.Logger;
+
+public class BasicAuthFilter implements Filter {
+    
+    Logger log = Logger.getLogger(BasicAuthFilter.class);
+    
+    private FilterConfig filterConfig = null;
+    
+    public void init(FilterConfig filterConfig) throws ServletException {
+       this.filterConfig = filterConfig;
+    }
+    
+    public void destroy() {
+       this.filterConfig = null;
+    }
+    
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+           throws IOException, ServletException {
+        
+        log.debug("in basic auth filter");
+        chain.doFilter(request, response);
+        log.debug("leaving basic auth filter");
+    }
+}
