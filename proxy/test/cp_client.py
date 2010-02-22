430331c1d7609e299303d3ef7a654a3a4bd11d39
Adrian Likins <adrian@alikins.usersys.redhat.com>
2010-02-22 12:54:36 -0500

    add license header

--- proxy/test/cp_client.py
+++ proxy/test/cp_client.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python
 
+#
+# Copyright (c) 2009 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+
+
 import candlepinapi
 import sys
 import optparse
