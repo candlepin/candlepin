7b448916757755a7660025d4bee3b4cbb281a873
Bryan Kearney <bkearney@redhat.com>
2010-02-24 09:52:07 -0500

    Add a request logger so I can see the body of what is passed in

---
+++ proxy/src/main/java/org/fedoraproject/candlepin/LoggingFilter.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.fedoraproject.candlepin;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+
+/**
+ * BasicAuthFilter
+ */
+public class LoggingFilter implements Filter {
+    
+    private static Logger log = Logger.getLogger(LoggingFilter.class);
+    
+    private FilterConfig filterConfig = null;
+    
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+    
+    public void destroy() {
+        this.filterConfig = null;
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain)
+        throws IOException, ServletException {
+        
+        if (log.isDebugEnabled()) {
+            LoggingRequestWrapper lRequest = new LoggingRequestWrapper((HttpServletRequest) request) ;
+            log.debug("Request Body: "+ lRequest.getBody()) ;
+            chain.doFilter(lRequest, response);
+        }
+        else {
+            chain.doFilter(request, response) ;
+        }
+
+    }
+}
---
+++ proxy/src/main/java/org/fedoraproject/candlepin/LoggingRequestWrapper.java
@@ -0,0 +1,72 @@
+package org.fedoraproject.candlepin;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class LoggingRequestWrapper extends HttpServletRequestWrapper {
+
+    private final String body;
+
+    public LoggingRequestWrapper(HttpServletRequest request) throws IOException {
+        super(request);
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(
+                    inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            }
+            else {
+                stringBuilder.append("");
+            }
+        }
+        catch (IOException ex) {
+            throw ex;
+        }
+        finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                }
+                catch (IOException ex) {
+                    throw ex;
+                }
+            }
+        }
+        body = stringBuilder.toString();
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(
+            body.getBytes());
+        ServletInputStream servletInputStream = new ServletInputStream() {
+            public int read() throws IOException {
+                return byteArrayInputStream.read();
+            }
+        };
+        return servletInputStream;
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(this.getInputStream()));
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+}
