dfe04a8564c361d1b92942e6e2b509f0e095e4ad
Bryan Kearney <bkearney@redhat.com>
2010-02-24 16:57:29 -0500

    Create a consumer from an external call

---
+++ proxy/src/test/java/org/fedoraproject/candlepin/resource/test/OwnerResourceTest.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.fedoraproject.candlepin.resource.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.fedoraproject.candlepin.model.Owner;
+import org.fedoraproject.candlepin.resource.OwnerResource;
+import org.fedoraproject.candlepin.test.DatabaseTestFixture;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * OwnerResourceTest
+ */
+public class OwnerResourceTest extends DatabaseTestFixture {
+
+    private static final String OWNER_NAME = "Jar Jar Binks";
+
+    private OwnerResource ownerResource;
+
+    @Before
+    public void setUp() {
+        ownerResource = new OwnerResource(ownerCurator, entitlementPoolCurator);
+    }
+
+    @Test
+    public void testCreateOwner() {
+        Owner toSubmit = new Owner(OWNER_NAME);
+
+        Owner submitted = ownerResource.createOwner(toSubmit);
+
+        assertNotNull(submitted);
+        assertNotNull(ownerCurator.find(submitted.getId()));
+    }
+
+}
