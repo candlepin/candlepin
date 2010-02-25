0b857755f28e3d10f7d7f7fe419de0a8f2dac49c
Dmitri Dolguikh <ddolguik@redhat.com>
2010-02-25 13:35:57 -0400

    fixed a problem when candlepin would end up being destroyed and all requests to it result in NPE

--- proxy/src/main/java/org/fedoraproject/candlepin/guice/JerseyGuiceConfiguration.java
+++ proxy/src/main/java/org/fedoraproject/candlepin/guice/JerseyGuiceConfiguration.java
@@ -16,6 +16,7 @@ package org.fedoraproject.candlepin.guice;
 
 import static com.google.inject.name.Names.*;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 import javax.servlet.Filter;
@@ -23,10 +24,12 @@ import javax.servlet.Filter;
 import org.fedoraproject.candlepin.LoggingFilter;
 import org.fedoraproject.candlepin.servletfilter.auth.FilterConstants;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.Module;
+import com.google.inject.Singleton;
 import com.google.inject.util.Modules;
 import com.sun.jersey.spi.container.servlet.ServletContainer;
 import com.wideplay.warp.persist.PersistenceService;
@@ -39,6 +42,8 @@ import com.wideplay.warp.servlet.WarpServletContextListener;
  * configure Guice with the resource classes.
  */
 public class JerseyGuiceConfiguration extends WarpServletContextListener {
+    
+    private final static String CANDLEPIN_SERVLET = "CANDLEPIN";
 
     @Override
     protected Injector getInjector() {
@@ -56,12 +61,25 @@ public class JerseyGuiceConfiguration extends WarpServletContextListener {
                             .filter("/*").through(LoggingFilter.class)
                             .filter("/*").through(Key.get(Filter.class, named(FilterConstants.BASIC_AUTH)))
                         .servlets()
-                            .serve("/*").with(ServletContainer.class)
+                            .serve("/*").with(
+                                Key.get(ServletContainer.class, named(CANDLEPIN_SERVLET)),
+                                new HashMap<String, String>() {{
+                                    put("com.sun.jersey.config.property.packages", "org.fedoraproject.candlepin.resource");
+                                }})
                      .buildModule()
                 );
+                
+                add(new ServletConfig());
 
                 add(Modules.override(new DefaultConfig()).with(new CustomizableModules().load()));
             }
         });
+    }
+    
+    protected class ServletConfig extends AbstractModule {
+        @Override
+        protected void configure() {
+            bind(ServletContainer.class).annotatedWith(named(CANDLEPIN_SERVLET)).to(ServletContainer.class).in(Singleton.class);
+        }
     }
 }
