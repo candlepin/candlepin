4b7f232a10d30afb6599612b62c7784e8abcc60f
Dmitri Dolguikh <ddolguik@redhat.com>
2010-02-26 18:20:44 -0400

    figured out how to authenticate certificate chain (using signing CA certificate)

--- proxy/src/test/java/org/fedoraproject/candlepin/auth/SSLAuthFilterTest.java
+++ proxy/src/test/java/org/fedoraproject/candlepin/auth/SSLAuthFilterTest.java
@@ -1,34 +1,84 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package org.fedoraproject.candlepin.auth;
 
+import static org.junit.Assert.*;
+
+import java.security.cert.CertPath;
+import java.security.cert.CertPathValidator;
+import java.security.cert.CertPathValidatorException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
+import java.security.cert.PKIXCertPathValidatorResult;
+import java.security.cert.PKIXParameters;
+import java.security.cert.TrustAnchor;
+import java.security.cert.X509Certificate;
+import java.util.Collections;
+import java.util.LinkedList;
 
 import org.junit.Before;
 import org.junit.Test;
 
+/*
+ * some useful sources of certificate-related information: 
+ *      http://www.herongyang.com/Cryptography/index.html
+ *      (in particular: http://www.herongyang.com/Cryptography/OpenSSL-as-CA-Sign-CSR-Generated-by-keytool.html)
+ *      http://www.chrissearle.org/blog/technical/installing_ca_signed_certificate_glassfish
+ */
 public class SSLAuthFilterTest {
     
-    private Certificate certificatePath;
-    private Certificate selfSignedCertificate;
-    private Certificate caCertificate;
+    private X509Certificate certificatePath;
+    private X509Certificate selfSignedCertificate;
+    private X509Certificate caCertificate;
+    private CertificateFactory certificateFactory;
+    private PKIXParameters PKIXparams;
 
     @Before
     public void setUp() throws Exception {
-        certificatePath = CertificateFactory
-            .getInstance("X.509")
-            .generateCertificate(getClass().getResourceAsStream("CertChain.cer"));
+        certificateFactory = CertificateFactory.getInstance("X.509");
         
-        selfSignedCertificate = CertificateFactory
-            .getInstance("X.509")
-            .generateCertificate(getClass().getResourceAsStream("SelfSignedCert.cer"));
+        certificatePath = (X509Certificate) certificateFactory
+            .generateCertificate(getClass().getResourceAsStream("certchain.crt"));
         
-        caCertificate = CertificateFactory
-            .getInstance("X.509")
-            .generateCertificate(getClass().getResourceAsStream("CACert.pem"));
+        selfSignedCertificate = (X509Certificate) certificateFactory
+            .generateCertificate(getClass().getResourceAsStream("selfsigned.crt"));
+        
+        caCertificate = (X509Certificate) certificateFactory
+            .generateCertificate(getClass().getResourceAsStream("ca.crt"));
+        
+        TrustAnchor anchor = new TrustAnchor((X509Certificate) caCertificate, null);
+        PKIXparams = new PKIXParameters(Collections.singleton(anchor));
+        PKIXparams.setRevocationEnabled(false);
     }
     
     @Test
-    public void validCertificateShouldPassAuthentication() {
+    public void validCertificateShouldPassVerification() throws Exception {
+        CertPathValidator cpv = CertPathValidator.getInstance("PKIX");
+        CertPath cp = certificateFactory.generateCertPath(new LinkedList<Certificate>() {{ add(certificatePath); }});
+        PKIXCertPathValidatorResult result = (PKIXCertPathValidatorResult) cpv.validate(cp, PKIXparams);
         
+        assertEquals(
+            "CN=Robert Paulson, OU=org unit, O=org, L=Halifax, ST=NS, C=CA", 
+            certificatePath.getSubjectDN().getName()
+        );
     }
+    
+    @Test(expected = CertPathValidatorException.class)
+    public void invalidCertificateShouldFailVerification() throws Exception {
+        CertPathValidator cpv = CertPathValidator.getInstance("PKIX");
+        CertPath cp = certificateFactory.generateCertPath(new LinkedList<Certificate>() {{ add(selfSignedCertificate); }});
+        PKIXCertPathValidatorResult result = (PKIXCertPathValidatorResult) cpv.validate(cp, PKIXparams);
+    }    
 }
