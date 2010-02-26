7dd226a20ec22ce98bef62368f0cd9934060dcbd
Wes Hayutin <whayutin@redhat.com>
2010-02-26 14:39:12 -0500

    cleaned up entitlement tests

--- proxy/test/entitlement-tests.py
+++ proxy/test/entitlement-tests.py
@@ -48,55 +48,6 @@ class EntitlementTests(CandlepinTests):
         result = self.cp.getEntitlements(self.uuid)
         print result
 
-
-
-    # TODO: Not really testing anything yet:
-    #def test_entitlements(self):
-    #    self.conn.request("GET", '/candlepin/entitlement', json.dumps(self.params), self.headers)
-    #    response = self.conn.getresponse()
-    #    rsp = response.read()
-    #    if self.debug:
-    #        print('\n')
-    #        print('list of entitlements:'+ rsp)
-    #        print('\n')
-    #    if rsp != None:
-    #        pass
-    #    else:
-    #        assert False 
- 
-    #def test_entitlement_id(self):
-    #    # TODO: test not ready yet, need a way to create an entitlement first
-    #    self.conn.request("GET", '/candlepin/entitlement/1', json.dumps(self.params), self.headers)
-    #    response = self.conn.getresponse()
-    #    self.assertResponse(200, response)
-    #    rsp = response.read()
-    #    entitlement_id = json.loads(rsp)['id']
-    #    self.assertEquals('1', entitlement_id)
- 
-    #def test_entitlement_productId(self):
-    #    # TODO: test not ready yet, need a way to create an entitlement first
-    #    self.conn.request("GET", '/candlepin/entitlement/1','',  self.headers)
-    #    response = self.conn.getresponse()
-    #    rsp = response.read()
-    #    if self.debug:
-    #        print 'response',response
-    #        print rsp
-    #        print('rsp')
-    #    dict_outer = json.loads(rsp)
-    #    for key, value in dict_outer.items():
-    #        if self.debug:
-    #            print key, value
-    #            print('\n')
-    #            print "product pool =",dict_outer["pool"]
-    #            dict_inner = dict_outer["pool"]
-    #            for key, value in dict_inner.items():
-    #                if self.debug:
-    #                    print key,value
-    #                    print ('\n')
-    #        #WILL EVENTUALLY TEST IF productId is member of Products
-    #        entitlement_productId = dict_inner["productId"]
-    #        self.assertTrue(entitlement_productId != None)
-
     def test_products(self):
         result = self.cp.getProducts()
         self.assertTrue("product" in result)
