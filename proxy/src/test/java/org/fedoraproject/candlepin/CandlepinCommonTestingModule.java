6a5d180df3838100eeee17a7762a1b8462323c8a
Chris Alfonso <calfonso@redhat.com>
2010-03-09 10:41:18 -0500

    Fixing tests

--- proxy/src/test/java/org/fedoraproject/candlepin/CandlepinCommonTestingModule.java
+++ proxy/src/test/java/org/fedoraproject/candlepin/CandlepinCommonTestingModule.java
@@ -35,8 +35,10 @@ import org.fedoraproject.candlepin.resource.PoolResource;
 import org.fedoraproject.candlepin.resource.ProductResource;
 import org.fedoraproject.candlepin.resource.TestResource;
 import org.fedoraproject.candlepin.service.IdentityCertServiceAdapter;
+import org.fedoraproject.candlepin.service.CertificateServiceAdapter;
 import org.fedoraproject.candlepin.service.ProductServiceAdapter;
 import org.fedoraproject.candlepin.service.SubscriptionServiceAdapter;
+import org.fedoraproject.candlepin.service.impl.DefaultCertificateServiceAdapter;
 import org.fedoraproject.candlepin.service.impl.DefaultProductServiceAdapter;
 import org.fedoraproject.candlepin.service.impl.DefaultSubscriptionServiceAdapter;
 import org.fedoraproject.candlepin.service.impl.stub.StubIdentityCertServiceAdapter;
@@ -73,6 +75,7 @@ public class CandlepinCommonTestingModule extends AbstractModule {
         bind(IdentityCertServiceAdapter.class).to(
             StubIdentityCertServiceAdapter.class);
         bind(Config.class);
+        bind(CertificateServiceAdapter.class).to(DefaultCertificateServiceAdapter.class);
         bind(RulesCurator.class).to(TestRulesCurator.class);
         bind(ScriptEngine.class).toProvider(ScriptEngineProvider.class);
         bind(Reader.class).annotatedWith(Names.named("RulesReader"))
