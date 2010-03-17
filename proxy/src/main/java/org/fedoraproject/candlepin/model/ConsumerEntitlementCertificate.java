69f940e87526c8fa2ede550bf4d229d521a5a3e9
Chris Alfonso <calfonso@redhat.com>
2010-03-17 18:02:12 -0400

    Adding a foreign key constraint to the Entitlement entity

--- proxy/src/main/java/org/fedoraproject/candlepin/model/ConsumerEntitlementCertificate.java
+++ proxy/src/main/java/org/fedoraproject/candlepin/model/ConsumerEntitlementCertificate.java
@@ -21,11 +21,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hibernate.annotations.ForeignKey;
 
 /**
  * Represents certificate used to entitle a consumer
@@ -52,6 +56,11 @@ public class ConsumerEntitlementCertificate implements Persisted {
     @Column(nullable = false)
     private BigInteger serialNumber;
 
+    @ManyToOne
+    @ForeignKey(name = "fk_cert_entitlement")
+    @JoinColumn(nullable = false)
+    private Entitlement entitlement;
+    
     public BigInteger getSerialNumber() {
         return serialNumber;
     }
