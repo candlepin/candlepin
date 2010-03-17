c8475476c6375e9cf33849d5f65b1e88a3b0d6d2
jesus m. rodriguez <jesusr@redhat.com>
2010-03-17 15:47:34 -0400

    fix checkstyle

--- proxy/src/main/java/org/fedoraproject/candlepin/config/ConfigurationParser.java
+++ proxy/src/main/java/org/fedoraproject/candlepin/config/ConfigurationParser.java
@@ -24,12 +24,13 @@ import java.util.Properties;
 abstract class ConfigurationParser {
 
     /**
-     * Converts the given Map into a Properties object. 
+     * Converts the given Map into a Properties object.
+     *
      * @param inputConfiguration Configuration to be converted.
      * @return config as a Properties file
      */
     public Properties parseConfig(Map<String, String> inputConfiguration) {
-        
+
         Properties toReturn = new Properties();
         toReturn.putAll(stripPrefixFromConfigKeys(inputConfiguration));
         return toReturn;
@@ -37,21 +38,25 @@ abstract class ConfigurationParser {
 
     /**
      * Return a copy of the input without the prefixes.
+     *
      * @param inputConfiguration Configuration to be converted.
      * @return config as a Properties object without the prefixes.
      */
-    public Properties stripPrefixFromConfigKeys(Map<String, String> inputConfiguration) {
+    public Properties stripPrefixFromConfigKeys(
+        Map<String, String> inputConfiguration) {
         Properties toReturn = new Properties();
-        
+
         for (String key : inputConfiguration.keySet()) {
-            if(key.startsWith(getPrefix())){
-                toReturn.put(key.substring(getPrefix().length() + 1), inputConfiguration.get(key));
-            }else{
+            if (key.startsWith(getPrefix())) {
+                toReturn.put(key.substring(getPrefix().length() + 1),
+                    inputConfiguration.get(key));
+            }
+            else {
                 toReturn.put(key, inputConfiguration.get(key));
             }
         }
         return toReturn;
-    }    
+    }
 
     public abstract String getPrefix();
 }
