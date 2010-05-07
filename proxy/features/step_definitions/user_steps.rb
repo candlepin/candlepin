804bc78319a1d03ea0114443ce8b0b56b0096cd4
Devan Goodwin <dgoodwin@redhat.com>
2010-05-07 11:45:26 -0300

    cuke: Introduce auth_steps.rb.

--- proxy/features/step_definitions/user_steps.rb
+++ proxy/features/step_definitions/user_steps.rb
@@ -7,6 +7,3 @@ Given /^an owner admin "([^\"]*)"$/ do |username|
     @candlepin.create_user(@test_owner['id'], @username, @password)
 end
 
-Given /^I am logged in as "([^\"]*)"$/ do |username|
-    @owner_admin_cp = connect(username=username, password="password")
-end
