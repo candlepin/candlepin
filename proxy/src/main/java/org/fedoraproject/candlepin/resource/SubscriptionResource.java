9abbc2909dcdd2c36859624fef280d68434e93df
Justin Harris <jharris@redhat.com>
2010-05-07 16:52:42 -0400

    cukes: fixing subscription feature

--- proxy/src/main/java/org/fedoraproject/candlepin/resource/SubscriptionResource.java
+++ proxy/src/main/java/org/fedoraproject/candlepin/resource/SubscriptionResource.java
@@ -19,15 +19,12 @@ import java.util.List;
 
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.log4j.Logger;
-import org.fedoraproject.candlepin.auth.Principal;
 import org.fedoraproject.candlepin.exceptions.BadRequestException;
 import org.fedoraproject.candlepin.model.Subscription;
 import org.fedoraproject.candlepin.model.SubscriptionCurator;
