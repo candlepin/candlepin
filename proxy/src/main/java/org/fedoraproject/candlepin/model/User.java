1e716ab5f879f0345158ed9fb6f786f81de8fb75
James Bowes <jbowes@redhat.com>
2010-05-06 21:45:17 -0400

    Fix checkstyle error

--- proxy/src/main/java/org/fedoraproject/candlepin/model/User.java
+++ proxy/src/main/java/org/fedoraproject/candlepin/model/User.java
@@ -25,7 +25,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
