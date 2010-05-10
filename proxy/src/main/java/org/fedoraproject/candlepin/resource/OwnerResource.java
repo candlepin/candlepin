7603d68996055121d17cc1119062644871a3bf36
jesus m. rodriguez <jesusr@redhat.com>
2010-05-10 16:30:26 -0400

    remove unused imports

--- proxy/src/main/java/org/fedoraproject/candlepin/resource/OwnerResource.java
+++ proxy/src/main/java/org/fedoraproject/candlepin/resource/OwnerResource.java
@@ -25,11 +25,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.log4j.Logger;
-import org.fedoraproject.candlepin.auth.Principal;
 import org.fedoraproject.candlepin.auth.Role;
 import org.fedoraproject.candlepin.auth.interceptor.AllowRoles;
 import org.fedoraproject.candlepin.controller.Entitler;
