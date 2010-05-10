1ddeeafc0c3e5c5c7a88c42cf96e02972de69072
Chris Alfonso <calfonso@redhat.com>
2010-05-10 09:31:35 -0400

    Adding the OIDUtil the to api jar

--- proxy/src/main/java/com/redhat/candlepin/util/OIDUtil.java
+++ proxy/src/main/java/com/redhat/candlepin/util/OIDUtil.java
@@ -19,14 +19,14 @@ import java.util.Map;
 
 /**
  * implements the oid structure found here
- * 
  * https://docspace.corp.redhat.com/clearspace/docs/DOC-30244
+ * 
  * @author jomara
- *
  */
 public final class OIDUtil {
 
-    private OIDUtil() { }
+    private OIDUtil() {
+    }
 
     public static final String REDHAT_OID = "1.3.6.1.4.1.2312.9";
     public static final String SYSTEM_NAMESPACE_KEY = "System";
@@ -38,6 +38,7 @@ public final class OIDUtil {
     public static final String HOST_UUID_KEY = "Host UUID";
     public static final String ORDER_NAME_KEY = "Name";
     public static final String ORDER_NUMBER_KEY = "Order Number";
+    public static final String ORDER_CONTRACT_NUMBER_KEY = "Contract Number";
     public static final String ORDER_SKU_KEY = "SKU";
     public static final String ORDER_SUBSCRIPTION_NUMBER_KEY = "Subscription Number";
     public static final String ORDER_QUANTITY_KEY = "Quantity";
@@ -58,43 +59,32 @@ public final class OIDUtil {
     public static final String CF_PHYS_QUANTITY_KEY = "Physical Entitlement Quantity";
     public static final String CF_FLEX_QUANTITY_KEY = "Flex Guest Entitlement Quantity";
     public static final String CF_CHANNELS_NAMESPACE = "Channel Namespace";
-    public static final String CF_VENDOR_ID_KEY = "Vendor ID";   
+    public static final String CF_VENDOR_ID_KEY = "Vendor ID";
     public static final String CF_DOWNLOAD_URL_KEY = "Download URL";
     public static final String CF_GPG_URL_KEY = "GPG Key URL";
     public static final String CF_ENABLED = "Enabled";
 
-
     public static final String ROLE_NAME_KEY = "Name";
     public static final String ROLE_LABEL_KEY = "Label";
     public static final String ROLE_QUANTITY_KEY = "Quantity";
-    
+
     public static final String CF_REPO_TYPE_FILE_KEY = "file";
     public static final String CF_REPO_TYPE_YUM_KEY = "yum";
 
-    
     public static final Map<String, String> SYSTEM_OIDS = new HashMap<String, String>();
     public static final Map<String, String> ORDER_OIDS = new HashMap<String, String>();
-    public static final Map<String, String> ORDER_PRODUCT_OIDS = 
-        new HashMap<String, String>();
-    public static final Map<String, String> TOPLEVEL_NAMESPACES = 
-        new HashMap<String, String>();
-    public static final Map<String, String> CONTENT_ENTITLEMENT_OIDS = 
-        new HashMap<String, String>();
-    public static final Map<String, String> ROLE_ENTITLEMENT_OIDS = 
-        new HashMap<String, String>();
-    public static final Map<String, String> CONTENT_ENTITLEMENT_NAMESPACES = 
-        new HashMap<String, String>();
-    public static final Map<String, String> SERVER_ENTITLEMENT_NAMESPACES = 
-        new HashMap<String, String>();
-    public static final Map<String, String> CONTENT_ARCHITECTURES = 
-        new HashMap<String, String>();
-    public static final Map<String, String> CHANNEL_FAMILY_OIDS = 
-        new HashMap<String, String>();
-    public static final Map<String, String> CHANNEL_OIDS = 
-        new HashMap<String, String>();
+    public static final Map<String, String> ORDER_PRODUCT_OIDS = new HashMap<String, String>();
+    public static final Map<String, String> TOPLEVEL_NAMESPACES = new HashMap<String, String>();
+    public static final Map<String, String> CONTENT_ENTITLEMENT_OIDS = new HashMap<String, String>();
+    public static final Map<String, String> ROLE_ENTITLEMENT_OIDS = new HashMap<String, String>();
+    public static final Map<String, String> CONTENT_ENTITLEMENT_NAMESPACES = new HashMap<String, String>();
+    public static final Map<String, String> SERVER_ENTITLEMENT_NAMESPACES = new HashMap<String, String>();
+    public static final Map<String, String> CONTENT_ARCHITECTURES = new HashMap<String, String>();
+    public static final Map<String, String> CHANNEL_FAMILY_OIDS = new HashMap<String, String>();
+    public static final Map<String, String> CHANNEL_OIDS = new HashMap<String, String>();
     public static final Map<String, String> CF_REPO_TYPE = new HashMap<String, String>();
 
-    static { 
+    static {
         // load toplevel namespaces
         TOPLEVEL_NAMESPACES.put(PRODUCT_CERT_NAMESPACE_KEY, "1");
         TOPLEVEL_NAMESPACES.put(CHANNEL_FAMILY_NAMESPACE_KEY, "2");
@@ -117,13 +107,13 @@ public final class OIDUtil {
         ORDER_OIDS.put(ORDER_VIRTLIMIT_KEY, "9");
         ORDER_OIDS.put(ORDER_SOCKETLIMIT_KEY, "10");
         ORDER_OIDS.put(ORDER_OPTIONCODE_KEY, "11");
+        ORDER_OIDS.put(ORDER_CONTRACT_NUMBER_KEY, "12");
 
         // load order product oids
         ORDER_PRODUCT_OIDS.put(OP_NAME_KEY, "1");
         ORDER_PRODUCT_OIDS.put(OP_DESC_KEY, "2");
         ORDER_PRODUCT_OIDS.put(OP_ARCH_KEY, "3");
         ORDER_PRODUCT_OIDS.put(OP_VERSION_KEY, "4");
-
 
         // role entitlement oids
         ROLE_ENTITLEMENT_OIDS.put(ROLE_NAME_KEY, "1");
@@ -141,7 +131,8 @@ public final class OIDUtil {
         CHANNEL_FAMILY_OIDS.put(CF_ENABLED, "8");
 
         // server entitlement namespaces
-        // this could come from the RHN DB, but we will wait until we actually need it...
+        // this could come from the RHN DB, but we will wait until we actually
+        // need it...
         SERVER_ENTITLEMENT_NAMESPACES.put("sw_mgr_entitled", "2");
         SERVER_ENTITLEMENT_NAMESPACES.put("enterprise_entitled", "3");
         SERVER_ENTITLEMENT_NAMESPACES.put("provisioning_entitled", "24");
