bf6e854d24c232d1622510ff64ab31cbe97ba754
Ajay Kumar <ajay@localhost.localdomain>
2010-05-18 16:20:04 -0400

    refractored model objects to include AbstractHibernateObject as base class.

---
+++ proxy/src/main/java/org/fedoraproject/candlepin/model/AbstractHibernateObject.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.fedoraproject.candlepin.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * Abstract class for hibernate entities
+ */
+@MappedSuperclass
+@XmlType(name = "CandlepinObject")
+public abstract class AbstractHibernateObject implements Persisted {
+
+    @XmlElement
+    private Date created;
+
+    @XmlElement
+    private Date updated;
+
+    @PrePersist
+    protected void onCreate() {
+        created = new Date();
+        updated = created;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updated = new Date();
+    }
+
+    @Column(nullable = false, unique = true)
+    public Date getCreated() {
+        return created;
+    }
+
+    @Column(nullable = false, unique = true)
+    public Date getUpdated() {
+        return updated;
+    }
+}
