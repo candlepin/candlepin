d304ec7b45e3afa7ac85d2996cbe459163f8d3e9
Adrian Likins <adrian@alikins.usersys.redhat.com>
2010-05-19 11:47:39 -0400

    Add some content test cases and a Content curator

---
+++ proxy/src/test/java/org/fedoraproject/candlepin/model/test/ContentTest.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.fedoraproject.candlepin.model.test;
+
+import org.fedoraproject.candlepin.model.Content;
+import org.fedoraproject.candlepin.test.DatabaseTestFixture;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * ContentTest
+ */
+public class ContentTest extends DatabaseTestFixture {
+
+    @Test
+    public void testContent() {
+        Content content =  new Content("test_content", "test_vendor", 
+                                "test_url", "test_gpg_url", "1");
+        
+        contentCurator.create(content);
+        
+        Content lookedUp = contentCurator.find(content.getId());
+        assertEquals(content.getContentUrl(), lookedUp.getContentUrl());
+        
+    }
+    
+    
+    @Test
+    public void testContentFoo() {
+        Content content = new Content("test_content", "test_vendor", 
+            "test_url", "test_gpg_url", "1");
+           
+            
+    }
+}
