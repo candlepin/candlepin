0808ff33a9d3578b16649ee1e3f0ff87f37bd7ac
Adrian Likins <adrian@alikins.usersys.redhat.com>
2010-06-02 18:14:09 -0400

    Shut up checkstyle

--- proxy/src/main/java/org/fedoraproject/candlepin/model/Content.java
+++ proxy/src/main/java/org/fedoraproject/candlepin/model/Content.java
@@ -16,8 +16,6 @@ package org.fedoraproject.candlepin.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
