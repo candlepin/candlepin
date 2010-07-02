e269fbc42a871e728596cf84b7ce02056919751e
James Bowes <jbowes@redhat.com>
2010-07-02 09:18:22 -0400

    Revert accidental commit to adminresource

--- proxy/src/main/java/org/fedoraproject/candlepin/resource/AdminResource.java
+++ proxy/src/main/java/org/fedoraproject/candlepin/resource/AdminResource.java
@@ -19,13 +19,11 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.log4j.Logger;
@@ -114,7 +112,7 @@ public class AdminResource {
     @Path("import")
     @AllowRoles(roles = Role.SUPER_ADMIN)
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public void importData(@Context HttpServletRequest request, MultipartFormDataInput input) {
+    public void importData(MultipartInput input) {
 
         try {
             InputPart part = input.getParts().get(0);
