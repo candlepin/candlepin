99b60f70356894a7bf3fa994bfab2e805b81afff
Justin Harris <jharris@redhat.com>
2010-07-28 17:20:04 -0400

    Reverting to resteasy 2.0-beta-4 to fix the exception mapper issue.
    Also adding an rspec test to catch this in the future.

---
+++ proxy/spec/authorization_spec.rb
@@ -0,0 +1,15 @@
+require 'candlepin_scenarios'
+require 'candlepin_api'
+
+require 'rubygems'
+require 'rest_client'
+
+describe 'Authorization' do
+  include CandlepinMethods
+  it_should_behave_like 'Candlepin Scenarios'
+
+  it 'should return a 401 if user credentials are invalid' do
+    cp = Candlepin.new('random', 'not valid')
+    lambda {cp.get_owners}.should raise_exception(RestClient::Request::Unauthorized)
+  end
+end
