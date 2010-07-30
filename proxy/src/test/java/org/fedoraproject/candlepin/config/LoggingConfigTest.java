3263acafead365eb87888e2968718625fb14ada8
jesus m rodriguez <jmrodri@gmail.com>
2010-07-30 17:00:29 -0400

    test the LoggingConfig

---
+++ proxy/src/test/java/org/fedoraproject/candlepin/config/LoggingConfigTest.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.fedoraproject.candlepin.config;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * LoggingConfigTest
+ */
+public class LoggingConfigTest {
+    
+    private Config config;
+    
+    private LoggingConfig lc;
+    
+    @Before
+    public void setup() {
+        config = mock(Config.class);
+        lc = new LoggingConfig(config);
+    }
+
+    @Test
+    public void configure() {
+        Logger l = Logger.getLogger(LoggingConfigTest.class);
+        assertNotNull(l);
+        assertNull(l.getLevel());
+        
+        Map<String, String> loglevels = new HashMap<String, String>();
+        loglevels.put(LoggingConfigTest.class.getName(), "DEBUG");
+        
+        when(config.configurationWithPrefix(LoggingConfig.PREFIX)).thenReturn(loglevels);
+        
+        lc.configure(config);
+        assertNotNull(l.getLevel());
+        assertEquals(Level.DEBUG, l.getLevel());
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void expectNull() {
+        when(config.configurationWithPrefix(LoggingConfig.PREFIX)).thenReturn(null);
+        lc.configure(config);
+    }
+}
