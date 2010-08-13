caf7083f4cb45df777a94e4b330fa583e8402488
James Bowes <jbowes@redhat.com>
2010-08-13 13:24:31 -0400

    rspec: convert entitlement regen tests from cukes to rspec

--- proxy/features/entitlement_certificates.feature
+++ proxy/features/entitlement_certificates.feature
@@ -10,12 +10,6 @@ Feature: Entitlement Certificates
         And test owner has 2 entitlements for "virtualization_host"
         And test owner has 4 entitlements for "monitoring"
 
-    Scenario: Regenerate entitlement certificates of a product
-        Given consumer "test_consumer" consumes an entitlement for the "virtualization_host" product
-        And consumer "another_test_consumer" consumes an entitlement for the "virtualization_host" product
-        When I regenerate entitlement certificates for "virtualization_host" product
-        Then consumers have new entitlement certificates
-
     Scenario: List certificates
         Given I am a consumer "test_consumer"
         When I consume an entitlement for the "monitoring" product
@@ -32,18 +26,3 @@ Feature: Entitlement Certificates
         Given I am a consumer "test_consumer"
         When I consume an entitlement for the "monitoring" product
         Then I have 1 certificate serial number
-
-    Scenario: Regenerate entitlement certificates for a consumer
-        Given I am a consumer "test_consumer"
-        When I consume an entitlement for the "virtualization_host" product
-        And I regenerate all my entitlement certificates
-        Then I have new entitlement certificates
-
-    Scenario: Regenerate multiple entitlement certificates of a consumer
-        Given I am a consumer "test_consumer"
-        When I consume an entitlement for the "monitoring" product
-        And I consume an entitlement for the "virtualization_host" product
-        And I regenerate all my entitlement certificates
-        Then I have new entitlement certificates
-        
-        
--- proxy/spec/entitlement_certificate_spec.rb
+++ proxy/spec/entitlement_certificate_spec.rb
@@ -4,26 +4,52 @@ describe 'Entitlement Certificate' do
   include CandlepinMethods
   it_should_behave_like 'Candlepin Scenarios'
 
-  it 'is available after consuming an entitlement' do
-    # Given
-    owner = create_owner 'test_owner'
+  before(:each) do
+    @owner = create_owner 'test_owner'
     monitoring = create_product()
 
-    # Create a subscription
-    @cp.create_subscription(owner.id, monitoring.id, 10)
-    @cp.refresh_pools owner.key
+    @cp.create_subscription(@owner.id, monitoring.id, 10)
+    @cp.refresh_pools @owner.key
 
-    # Set up user
-    billy = user_client(owner, 'billy')
+    user = user_client(@owner, 'billy')
 
-    # And register a consumer
-    system1 = consumer_client(billy, 'system1')
+    @system = consumer_client(user, 'system1')
+    @system.consume_product monitoring.id
+  end
 
-    #When
-    system1.consume_product monitoring.id
+  it 'is available after consuming an entitlement' do
+    @system.list_certificates.length.should == 1
+  end
 
-    #Then
-    system1.list_certificates.length.should == 1
+  it 'can be manually regenerated for a consumer' do
+    @system.list_certificates.length.should == 1
+    old_certs = @system.list_certificates()
+    @system.regenerate_entitlement_certificates()
+    
+    new_certs = @system.list_certificates()
+    old_certs.size.should == new_certs.size
+    old_ids = old_certs.map { |cert| cert['serial']['id']}
+    new_ids = new_certs.map { |cert| cert['serial']['id']}
+    (old_ids & new_ids).size.should == 0
+  end
+
+  it 'can be manually regenerated for a product' do
+    coolapp = create_product
+    @cp.create_subscription(@owner.id, coolapp.id, 10)
+    @cp.refresh_pools @owner.key
+    @system.consume_product coolapp.id
+    
+    @cp.regenerate_entitlement_certificates_for_product(coolapp.id)  
+    
+    @system.list_certificates.length.should == 2
+    old_certs = @system.list_certificates()
+    @system.regenerate_entitlement_certificates()
+    
+    new_certs = @system.list_certificates()
+    old_certs.size.should == new_certs.size
+    old_ids = old_certs.map { |cert| cert['serial']['id']}
+    new_ids = new_certs.map { |cert| cert['serial']['id']}
+    (old_ids & new_ids).size.should == 0
   end
 
 end
