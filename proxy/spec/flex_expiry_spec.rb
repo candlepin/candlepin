c7e15026baac9be1d83f6f7818bf03eff6daa09c
Devan Goodwin <dgoodwin@redhat.com>
2010-08-13 11:17:37 -0300

    rspec: Flex expiry test for refresh pools.
    Subscription end date changes, refresh pools is called, entitlement
    certs will get regenerated and should again get the flex expiry days
    added.

--- proxy/spec/flex_expiry_spec.rb
+++ proxy/spec/flex_expiry_spec.rb
@@ -6,35 +6,47 @@ describe 'Flex Expiry' do
   include CandlepinMethods
   it_should_behave_like 'Candlepin Scenarios'
 
-  def create_flex_expiry_product
+  before(:each) do
+    @owner = create_owner random_string('test_owner')
+    owner_client = user_client(@owner, random_string('testuser'))
     product_id = random_string('testproduct')
-    return create_product(product_id, product_id, {:attributes =>
-          {'flex_expiry' => '30'}})
+    @flex_days = 30
+    flex_product =  create_product(product_id, product_id, {:attributes =>
+          {'flex_expiry' => @flex_days}})
+
+    # Create a subscription and refresh pools:
+    @end_date = Date.new(2050, 5, 1)
+    @sub = @cp.create_subscription(@owner.id, flex_product.id, 100, [], '',
+        "2010-05-01", @end_date)
+    @cp.refresh_pools @owner.key
+    @pool = @cp.list_pools(:owner => @owner.id).first
+
+    # Bind to create an entitlement cert:
+    @consumer_client = consumer_client(owner_client, random_string('testsystem'))
+    @consumer_client.consume_pool @pool.id
   end
 
   it 'creates extended entitlement certificates' do
-    owner = create_owner random_string('test_owner')
-    owner_client = user_client(owner, random_string('testuser'))
-    flex_product = create_flex_expiry_product
-
-    end_date = Date.new(2050, 5, 1)
-    sub = @cp.create_subscription(owner.id, flex_product.id, 100, [], '',
-        "2010-05-01", end_date)
-    @cp.refresh_pools owner.key
-    pool = @cp.list_pools(:owner => owner.id).first
-
     # Make sure we got the right pool:
-    pool.subscriptionId.should == sub.id
-    sub['endDate'].should == pool['endDate']
+    @pool.subscriptionId.should == @sub.id
+    @sub['endDate'].should == @pool['endDate']
 
-    # Bind to create an entitlement cert:
-    consumer_client = consumer_client(owner_client, random_string('testsystem'))
+    ent_cert = @consumer_client.list_certificates.first
+    verify_cert_dates(ent_cert, @end_date, @flex_days)
+  end
 
-    result = consumer_client.consume_pool pool.id
-    ent_cert = consumer_client.list_certificates.first
+  it 're-applies after a subscription end date change' do
+    ent_cert = @consumer_client.list_certificates.first
+    verify_cert_dates(ent_cert, @end_date, @flex_days)
 
-    verify_cert_dates(ent_cert, end_date, 30)
+    # Add ten days to the subscription end date:
+    @end_date = @end_date + 10
+    @sub['endDate'] = @end_date
+    @cp.update_subscription(nil, @sub)
+    @cp.refresh_pools(@owner.key)
 
+    ent_cert = @consumer_client.list_certificates.first
+    verify_cert_dates(ent_cert, @end_date, @flex_days)
   end
 
 end
