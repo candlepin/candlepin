c9f73c8b9f79dd9fff8c00eaa71b8cbb55c1ac76
Devan Goodwin <dgoodwin@redhat.com>
2010-08-25 11:36:58 -0300

    Test entitlement serialization.

--- proxy/spec/serialization_spec.rb
+++ proxy/spec/serialization_spec.rb
@@ -106,11 +106,32 @@ describe 'Pool serialization' do
     consumer_client.consume_pool(@pool.id)
 
     @pool = @cp.get_pool(@pool.id)
-    pp @pool
     @pool.has_key?('entitlements').should be_true
     @pool['entitlements'].each do |e|
       check_for_hateoas(e)
     end
+  end
+end
+
+describe 'Entitlement serialization' do
+
+  include CandlepinMethods
+  it_should_behave_like 'Candlepin Scenarios'
+
+  before(:each) do
+    @owner = @cp.create_owner(random_string("test_owner"))
+    @owner_client = user_client(@owner, random_string('testuser'))
+    product1 = create_product()
+    @pool = @cp.create_pool(product1.id, @owner.id, 2)
+    consumer_client = consumer_client(@owner_client, random_string(),
+        "candlepin")
+    ent_id = consumer_client.consume_pool(@pool.id)[0]['id']
+    @ent = @cp.get_entitlement(ent_id)
+  end
+
+  it 'references pool as a link' do
+    @ent.has_key?('pool').should be_true
+    check_for_hateoas(@ent['pool'])
   end
 end
 
