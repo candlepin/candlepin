4ecba5f52eb0c0ba8ccca2269f6c4e9e2b51c493
James Bowes <jbowes@redhat.com>
2010-09-20 15:47:18 -0300

    spec: add missing localization_spec (forgot to git add)

---
+++ proxy/spec/localization_spec.rb
@@ -0,0 +1,18 @@
+require 'candlepin_scenarios'
+require 'candlepin_api'
+
+require 'json'
+
+describe 'Localization' do
+  include CandlepinMethods
+  it_should_behave_like 'Candlepin Scenarios'
+
+  it 'returns a translated error message on a failed login' do
+      lambda { Candlepin.new('admin', 'badpass', nil, nil, 'localhost', 8443,
+                             'de-DE') }.should raise_error(
+        RestClient::Unauthorized) { |error|
+        expected = "Ungültiger Benutzername oder Kennwort"
+        JSON.parse(error.http_body)["displayMessage"].should == expected
+      }
+  end
+end
