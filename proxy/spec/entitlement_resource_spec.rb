f64e64edc214dc27de95fdf75a0c1966ad5bd021
James Bowes <jbowes@redhat.com>
2010-09-21 13:34:20 -0300

    spec: convert entitlement_certificates.feature to rspec

--- proxy/spec/entitlement_resource_spec.rb
+++ proxy/spec/entitlement_resource_spec.rb
@@ -8,9 +8,11 @@ describe 'Entitlement Resource' do
   before do
     @owner = create_owner 'test_owner'
     @monitoring_prod = create_product(name='monitoring')
+    @virt_prod= create_product(name='virtualization')
 
     #entitle owner for the virt and monitoring products.
     @cp.create_subscription(@owner.key, @monitoring_prod.id, 6)
+    @cp.create_subscription(@owner.key, @virt_prod.id, 6)
 
     @cp.refresh_pools(@owner.key)
 
@@ -20,11 +22,28 @@ describe 'Entitlement Resource' do
   end 
   
   it 'should allow entitlement certificate regeneration based on product id' do
-      @system.consume_product(@monitoring_prod.id)
-      old_ent = @system.list_certificate_serials()[0]
-      @cp.regenerate_entitlement_certificates_for_product(@monitoring_prod.id)
-      new_ent = @system.list_certificate_serials()[0]
-      old_ent.serial.should_not == new_ent.serial
+    @system.consume_product(@monitoring_prod.id)
+    old_ent = @system.list_certificate_serials()[0]
+    @cp.regenerate_entitlement_certificates_for_product(@monitoring_prod.id)
+    new_ent = @system.list_certificate_serials()[0]
+    old_ent.serial.should_not == new_ent.serial
+  end
+
+  it 'allows filtering certificates by serial number' do
+    @system.consume_product(@monitoring_prod.id)
+    @system.consume_product(@virt_prod.id)
+    certificates = @system.list_certificates()
+    found = certificates.find {|item|
+      ent = @system.get_entitlement(item['entitlement']['id'])
+      pool = @system.get_pool(ent['pool']['id'])
+      pool['productId'] == @monitoring_prod.id}
+
+    @system.list_certificates([found['serial']['id']]).length.should == 1
+  end
+
+  it 'allows listing certificates by serial numbers' do
+    @system.consume_product(@monitoring_prod.id)
+    @system.list_certificate_serials.length.should == 1
   end
  
 end
