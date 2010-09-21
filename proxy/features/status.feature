40ff7ab71371ccdd551bfe65af337da7be117f61
James Bowes <jbowes@redhat.com>
2010-09-21 11:12:49 -0300

    spec: convert status.feature to rspec

--- proxy/features/status.feature
+++
@@ -1,5 +0,0 @@
-Feature: Status URL
-
-    Scenario: Status should not be Unknown 
-        When I visit the status URI
-        Then status should be known 
---
+++ proxy/spec/status_resource_spec.rb
@@ -0,0 +1,19 @@
+require 'candlepin_scenarios'
+
+describe 'Status Resource' do
+
+  include CandlepinMethods
+  it_should_behave_like 'Candlepin Scenarios'
+
+  it 'should return a valid candlepin version and release' do
+    json = @cp.get_status()
+    json['release'].should_not == "Unknown"
+    json['release'].should_not == "${hash}"
+    json['version'].should_not == "Unknown"
+    json['version'].should_not == "${version}"
+
+    # Try a cast on the release to be sure, it should be an integer I think:
+    release = json['release'].to_i() 
+  end
+
+end
