5296f78ed9c7aecde70cabded5fac4a06d2e3573
Adrian Likins <alikins@redhat.com>
2010-11-08 14:58:47 -0500

    add rspec test for list_users_by_owner

--- proxy/spec/owner_resource_spec.rb
+++ proxy/spec/owner_resource_spec.rb
@@ -25,4 +25,15 @@ describe 'Owner Resource' do
       @cp.create_owner(random_string('child owner'), fake_parent_owner)
     end.should raise_exception(RestClient::BadRequest)
   end
+
+  it "lets owners list users" do
+    owner = create_owner random_string("test_owner1")
+    
+    user1 = @cp.create_user(owner.key, random_string("test_user1"), "password")
+    user2 = @cp.create_user(owner.key, random_string("test_user2"), "password")
+
+    users = @cp.list_users_by_owner owner.key
+   
+    users.length.should == 2
+  end
 end
