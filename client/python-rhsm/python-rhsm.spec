a34dda8673eb929d28c6657e990fcba3bc04b599
Devan Goodwin <dgoodwin@redhat.com>
2010-12-10 14:35:05 -0400

    Adding the python-rhsm sub-project.

---
+++ client/python-rhsm/python-rhsm.spec
@@ -0,0 +1,55 @@
+Name: python-rhsm
+Version: 0.94.0
+Release: 1%{?dist}
+
+Summary: A Python library to communicate with a Red Hat Unified Entitlement Platform
+Group: Development/Libraries
+License: GPLv2
+Requires: m2crypto
+Requires: python-simplejson
+BuildArch: noarch
+
+Source0: %{name}-%{version}.tar.gz
+
+URL: http://fedorahosted.org/candlepin
+BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+
+BuildRequires: python2-devel
+BuildRequires: gettext
+BuildRequires: intltool
+
+
+%description 
+A small library for communicating with the REST interface of a Red Hat Unified
+Entitlement Platform. This interface is used for the management of system
+entitlements, certificates, and access to content.
+
+%prep
+%setup -q
+
+%build
+%{__python} setup.py build
+
+%install
+rm -rf $RPM_BUILD_ROOT
+%{__python} setup.py install -O1 --skip-build --root $RPM_BUILD_ROOT
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%files
+%defattr(-,root,root,-)
+%attr(755,root,root) %dir %{_var}/log/rhsm
+
+%dir %{python_sitelib}/rhsm
+
+%{_datadir}/rhsm/__init__.py*
+
+%files 
+%{python_sitelib}/rhsm/connection.py*
+%{python_sitelib}/rhsm/logutil.py*
+%{python_sitelib}/rhsm/config.py*
+
+%changelog
+* Fri Dec 10 2010 Devan Goodwin <dgoodwin@redhat.com> 
+- Initial tagging as separate python-rhsm package.
---
+++ client/python-rhsm/src/rhsm/config.py
@@ -0,0 +1,65 @@
+#
+# This module has been originally modified and enhanced from Red Hat Update Agent's config module.
+#
+# Copyright (c) 2010 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+# Red Hat trademarks are not licensed under GPLv2. No permission is
+# granted to use or replicate Red Hat trademarks that are incorporated
+# in this software or its documentation.
+#
+
+import ConfigParser
+
+
+DEFAULT_CONFIG_DIR = "/etc/rhsm"
+DEFAULT_CONFIG_PATH = "%s/rhsm.conf" % DEFAULT_CONFIG_DIR
+DEFAULT_PROXY_PORT="3128"
+
+DEFAULTS = {
+        'hostname': 'localhost',
+        'prefix': '/candlepin',
+        'port': '8443',
+        'ca_cert_dir': '/etc/rhsm/ca/',
+        'repo_ca_cert': '/etc/rhsm/ca/redhat-uep.pem',
+        'ssl_verify_depth': '3',
+        'proxy_hostname': None,
+        'proxy_port': None,
+        'proxy_user': None,
+        'proxy_password': None
+        }
+
+class RhsmConfigParser(ConfigParser.SafeConfigParser):
+    def __init__(self, config_file=None, defaults=None):
+        self.config_file = config_file
+        ConfigParser.ConfigParser.__init__(self, defaults=defaults)
+        self.read(self.config_file)
+
+    def save(self, config_file=None):
+        fo = open(self.config_file, "wb")
+        self.write(fo)
+
+def initConfig(config_file=None):
+
+    global CFG
+    # If a config file was specified, assume we should overwrite the global config
+    # to use it. This should only be used in testing. Could be switch to env var?
+    if config_file:
+        CFG = RhsmConfigParser(config_file=config_file, defaults=DEFAULTS)
+        return CFG
+
+    # Normal application behavior, just read the default file if we haven't
+    # already:
+    try:
+        CFG = CFG
+    except NameError:
+        CFG = None
+    if CFG == None:
+        CFG = RhsmConfigParser(config_file=DEFAULT_CONFIG_PATH, defaults=DEFAULTS)
+    return CFG
