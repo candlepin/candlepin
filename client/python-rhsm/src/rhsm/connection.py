1e2e75eb308a9302112a976d22f28efe49da076c
Devan Goodwin <dgoodwin@redhat.com>
2010-12-13 13:24:20 -0400

    python-rhsm: Remove some rogue tab characters.

--- client/python-rhsm/src/rhsm/connection.py
+++ client/python-rhsm/src/rhsm/connection.py
@@ -60,11 +60,11 @@ class RhsmProxyHTTPSConnection(httpslib.ProxyHTTPSConnection):
         if not self._proxy_auth:
             self._proxy_auth = self._encode_auth()
 
-	if body:
-	    httpslib.HTTPSConnection.endheaders(self, body)
-	else:
-	    httpslib.HTTPSConnection.endheaders(self)
-#        httpslib.HTTPSConnection.endheaders(self, body)
+        if body:
+            httpslib.HTTPSConnection.endheaders(self, body)
+        else:
+            httpslib.HTTPSConnection.endheaders(self)
+    #        httpslib.HTTPSConnection.endheaders(self, body)
 
         
 
