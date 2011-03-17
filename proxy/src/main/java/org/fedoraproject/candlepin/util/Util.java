c6b48d452e402f44df972e46c9a401b51dbd06f1
John Eckersberg <jeckersb@redhat.com>
2011-03-17 16:17:10 -0400

    Changed the logic behind serial number generation. See the comment for the dirty details.

--- proxy/src/main/java/org/fedoraproject/candlepin/util/Util.java
+++ proxy/src/main/java/org/fedoraproject/candlepin/util/Util.java
@@ -29,8 +29,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
-import java.net.Inet4Address;
-import java.net.UnknownHostException;
 import java.security.SecureRandom;
 import java.security.cert.X509Extension;
 import java.text.ParseException;
@@ -260,17 +258,45 @@ public class Util {
     }
     
     public static long generateUniqueLong() {
-        String id = "";
-        try {
-            long time = System.currentTimeMillis();
-            byte[] ipaddress = Inet4Address.getLocalHost().getAddress();
-            Random random = new SecureRandom();
-            id = id + (long) ipaddress[3] + time + random.nextInt(255);
-        }
-        catch (UnknownHostException ex) {
-            ex.printStackTrace();
-        }
-        return Math.abs(Long.valueOf(id));
+        /*
+          This deserves explanation.
+
+          A random positive Long has 63 bits of hash space.  We want
+          to have a given amount of certainty about the probability of
+          collisions within this space.  This is an instance of the
+          Birthday Problem[1].  We can get the probability that any
+          two random numbers collide with the approximation:
+
+          1-e**((-(N**2))/(2H))
+
+          Where e is Euler's number, N is the number of random numbers
+          generated, and H is the number of possible random outcomes.
+
+          Suppose then that we generated one billion serials, with
+          each serial being a 63-bit positive Long.  Then our
+          probability of having a collision would be:
+
+          irb(main):001:0> 1-Math.exp((-(1000000000.0**2))/(2.0*(2**63)))
+          => 0.052766936243662
+
+          So, if we generated a *billion* such serials, there is only
+          a 5% chance that any two of them would be the same.  In
+          other words, there is 95% chance that we would not have a
+          single collision in one billion entries.
+
+          The chances obviously get even less likely with smaller
+          numbers.  With one million, the probability of a collision
+          is:
+
+          irb(main):002:0> 1-Math.exp((-(1000000.0**2))/(2.0*(2**63)))
+          => 5.42101071809853e-08
+
+          Or, 1 in 18,446,744.
+
+          [1] http://en.wikipedia.org/wiki/Birthday_problem
+         */
+
+        return Math.abs(new SecureRandom().nextLong());
     }
     
     public static String toBase64(byte [] data) {
