9a7b84976a4539dca0ec0938e7e592ab61bb174b
jesus m. rodriguez <jesusr@redhat.com>
2011-03-18 13:48:38 -0400

    remove unused imports

--- proxy/src/main/java/org/fedoraproject/candlepin/util/Util.java
+++ proxy/src/main/java/org/fedoraproject/candlepin/util/Util.java
@@ -39,7 +39,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.Set;
 import java.util.TimeZone;
 import java.util.UUID;
