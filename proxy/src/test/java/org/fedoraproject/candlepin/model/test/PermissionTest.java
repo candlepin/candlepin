9ab60f5809935215bd1f693dd49189f496bdb795
Devan Goodwin <dgoodwin@redhat.com>
2011-05-18 13:28:31 -0300

    Commit missed new test class.

---
+++ proxy/src/test/java/org/fedoraproject/candlepin/model/test/PermissionTest.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.fedoraproject.candlepin.model.test;
+
+import static org.junit.Assert.*;
+
+import org.fedoraproject.candlepin.auth.Role;
+import org.fedoraproject.candlepin.model.Owner;
+import org.fedoraproject.candlepin.model.Permission;
+import org.fedoraproject.candlepin.test.DatabaseTestFixture;
+
+
+import org.junit.Test;
+
+public class PermissionTest extends DatabaseTestFixture {
+
+    @Test
+    public void testCreate() throws Exception {
+        
+        Owner o = createOwner();
+        Permission p = new Permission(o, Role.OWNER_ADMIN);
+        permissionCurator.create(p);
+        
+        Permission lookedUp = permissionCurator.find(p.getId());
+        assertNotNull(lookedUp);
+        assertEquals(o.getId(), lookedUp.getOwner().getId());
+        assertEquals(Role.OWNER_ADMIN, lookedUp.getRole());
+    }
+    
+    @Test
+    public void testEquality() throws Exception {
+        Owner o = createOwner();
+        Permission basePerm = new Permission(o, Role.OWNER_ADMIN);
+        
+        Permission equalPerm = new Permission(o, Role.OWNER_ADMIN);
+        assertFalse(basePerm == equalPerm);
+    }
+
+}
