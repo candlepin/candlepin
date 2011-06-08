5954091d83bd35b52c3832c15c8ba41d59afa5a3
Devan Goodwin <dgoodwin@redhat.com>
2011-06-08 12:18:32 -0300

    Checkstyle fixes.

--- proxy/src/main/java/org/fedoraproject/candlepin/service/impl/DefaultUserServiceAdapter.java
+++ proxy/src/main/java/org/fedoraproject/candlepin/service/impl/DefaultUserServiceAdapter.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.LinkedList;
 
-import org.fedoraproject.candlepin.exceptions.NotFoundException;
 import org.fedoraproject.candlepin.model.Owner;
 import org.fedoraproject.candlepin.model.OwnerPermission;
 import org.fedoraproject.candlepin.model.OwnerPermissionCurator;
