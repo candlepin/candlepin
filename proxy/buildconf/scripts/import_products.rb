f372061a4cb652e20dac0e098fa403e5dd6781e5
Adrian Likins <alikins@redhat.com>
2011-07-01 12:57:02 -0400

    Populate displayname for owners
    Cleanup some of the indenting, and fix up some of the output
    formatting.

--- proxy/buildconf/scripts/import_products.rb
+++ proxy/buildconf/scripts/import_products.rb
@@ -34,69 +34,87 @@ end
 cp = Candlepin.new(username='admin', password='admin', cert=nil, key=nil, host='localhost', post=8443)
 
 # create some owners
+puts "Create some owners"
 data["owners"].each do |new_owner|
   owner_name = new_owner['name']
-  puts "owner: #{owner_name}"
+  displayName = new_owner['displayName']
 
-  owner = cp.create_owner(owner_name)
+  puts "owner: #{owner_name}"
+  puts "\t displayName: #{displayName}"
+
+  owner = cp.create_owner(owner_name, new_owner)
 
   # Create one dummy activation key for the owner
-  cp.create_activation_key({"name" => "Default Key"}, owner['key']) 
+  cp.create_activation_key({"name" => "Default Key"}, owner['key'])
 end
 
+puts
+
 # create some users
+puts "Create some users"
 data["users"].each do |new_user|
   user_name = new_user['username']
   user_pass = new_user['password']
   user_super = new_user['superadmin'] || false
 
-  puts "user: #{user_name}, #{user_pass}, #{user_super}"
+  puts "user: #{user_name}"
+  puts "\t password: #{user_pass}"
+  puts "\t super_user: #{user_super}"
 
   owner = cp.create_user(user_name, user_pass, user_super)
 
 end
 
-
+puts
 # Create roles:
+puts "Create some roles"
 data['roles'].each do |new_role|
   role_name = new_role['name']
   perms = new_role['permissions']
   users = new_role['users']
 
-  puts " permissions: #{perms}"
+#  puts " permissions: \t #{pp perms}"
+  puts "role_name: #{role_name}"
+  perms.each do |perm|
+    puts "\t owner: #{perm['owner']}"
+    puts "\t access: #{perm['access']}"
+  end
 
   role = cp.create_role(role_name, perms)
 
   users.each do |user|
-    puts "   user: #{user['username']}"
+    puts "\t user: #{user['username']}"
     cp.add_role_user(role['id'], user['username'])
   end
-  
+
 end
 
+puts
 owners = cp.list_owners({:fetch => true})
 owner_key = owners[0]['key']
 
 # import all the content sets
-puts "importing content set data..."
+puts "Importing content set data..."
 data['content'].each do |c|
 
-	puts c['name']
+  puts c['name']
 
   params = {}
   modified_products = c['modified_products'] || []
   if c.has_key?('metadata_expire')
     params[:metadata_expire] = c['metadata_expire']
   end
+
   if c.has_key?('required_tags')
     params[:required_tags] = c['required_tags']
   end
+
   params[:content_url] = c['content_url']
   params[:gpg_url] = c['gpg_url']
   params[:modified_products] = modified_products
 
-	cp.create_content(c['name'], c['id'], c['label'], c['type'],
-      c['vendor'], params)
+  cp.create_content(c['name'], c['id'], c['label'], c['type'],
+                    c['vendor'], params)
 end
 
 
@@ -105,70 +123,69 @@ if not File.directory? CERT_DIR
 	Dir.mkdir(CERT_DIR)
 end
 
-
-puts "import product data..."
+puts
+puts "Import product data..."
 contract_number = 0
 data['products'].each do |product|
 
-          name = product['name']
-          id = product['id']
-          multiplier = product['multiplier'] || 1
-          version = product['version'] || "1.0"
-          variant = product['variant'] || "ALL"
-          arch = product['arch'] || "ALL"
-          type = product['type'] || "SVC"
-          provided_products = product['provided_products'] || []
-          attrs = product['attributes'] || {}
-          product_content = product['content'] || []
-          dependent_products = product['dependencies'] || []
+  name = product['name']
+  id = product['id']
+  multiplier = product['multiplier'] || 1
+  version = product['version'] || "1.0"
+  variant = product['variant'] || "ALL"
+  arch = product['arch'] || "ALL"
+  type = product['type'] || "SVC"
+  provided_products = product['provided_products'] || []
+  attrs = product['attributes'] || {}
+  product_content = product['content'] || []
+  dependent_products = product['dependencies'] || []
 
-          attrs['version'] = version
-          attrs['variant'] = variant
-          attrs['arch'] = arch
-          attrs['type'] = type
-          product_ret = cp.create_product(id, name, {:multiplier => multiplier,
-              :attributes => attrs}, dependent_products)
-          puts "product name: " + name + " version: " + version + " arch: " + \
-            arch + " type: " + type
-          startDate1 =  Date.today
-	      endDate1 = startDate1 + 365
-          startDate2 = endDate1 - 10
-          endDate2 = startDate2 + 365
-          startDate3 = endDate1 + 1
-          endDate3 = startDate2 + 365
+  attrs['version'] = version
+  attrs['variant'] = variant
+  attrs['arch'] = arch
+  attrs['type'] = type
+  product_ret = cp.create_product(id, name, {:multiplier => multiplier,
+                                    :attributes => attrs}, dependent_products)
+  puts "product name: " + name + " version: " + version + \
+       " arch: " + arch + " type: " + type
+  startDate1 =  Date.today
+  endDate1 = startDate1 + 365
+  startDate2 = endDate1 - 10
+  endDate2 = startDate2 + 365
+  startDate3 = endDate1 + 1
+  endDate3 = startDate2 + 365
 
-          # If product ID is non-numeric, we assume it's a marketing product
-          # and create subscriptions for it:
-          if id.to_i.to_s != id:
+  # If product ID is non-numeric, we assume it's a marketing product
+  # and create subscriptions for it:
+  if id.to_i.to_s != id:
+    # Create a SMALL and a LARGE with the slightly similar begin/end dates.
+    subscription = cp.create_subscription(owner_key,
+                                          product_ret['id'], SMALL_SUB_QUANTITY, provided_products,
+                                          contract_number, '12331131231', startDate1, endDate1)
+    contract_number += 1
+    subscription = cp.create_subscription(owner_key,
+                                          product_ret['id'], LARGE_SUB_QUANTITY, provided_products,
+                                          contract_number, '12331131231', startDate1, endDate1)
 
-              # Create a SMALL and a LARGE with the slightly similar begin/end dates.
-              subscription = cp.create_subscription(owner_key,
-                  product_ret['id'], SMALL_SUB_QUANTITY, provided_products,
-                  contract_number, '12331131231', startDate1, endDate1)
-              contract_number += 1
-              subscription = cp.create_subscription(owner_key,
-                  product_ret['id'], LARGE_SUB_QUANTITY, provided_products,
-                  contract_number, '12331131231', startDate1, endDate1)
+    # Create a subscription for the future:
+    subscription = cp.create_subscription(owner_key, product_ret['id'],
+                                          15, provided_products,
+                                          contract_number, '12331131231', startDate2, endDate2)
+    contract_number += 1
+  end
 
-              # Create a subscription for the future:
-              subscription = cp.create_subscription(owner_key, product_ret['id'],
-                  15, provided_products,
-                  contract_number, '12331131231', startDate2, endDate2)
-              contract_number += 1
-          end
+  # TODO: not sure what's going on here?
+  if id.to_i.to_s == id:
+    product_cert = cp.get_product_cert(product_ret['id'])
+    cert_file = File.new(CERT_DIR + '/' + product_ret['id'] + '.pem', 'w+')
+    cert_file.puts(product_cert['cert'])
+  end
 
-          # TODO: not sure what's going on here?
-          if id.to_i.to_s == id:
-              product_cert = cp.get_product_cert(product_ret['id'])
-              cert_file = File.new(CERT_DIR + '/' + product_ret['id'] + '.pem', 'w+')
-              cert_file.puts(product_cert['cert'])
-          end
+  product_content.each do |content|
+    cp.add_content_to_product(product_ret['id'], content[0], content[1])
+  end
 
-	  product_content.each do |content|
-		  cp.add_content_to_product(product_ret['id'], content[0], content[1])
-	  end
 end
 
 # Refresh to create pools for all subscriptions just created:
 cp.refresh_pools(owner_key)
-
