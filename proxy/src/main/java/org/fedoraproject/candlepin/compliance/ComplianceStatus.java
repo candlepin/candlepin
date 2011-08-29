18cd7a6e22032a027360c1cc3e2c311473d5f975
Devan Goodwin <dgoodwin@redhat.com>
2011-08-29 10:33:39 -0300

    Sketch out interface for compliance checking.

---
+++ proxy/src/main/java/org/fedoraproject/candlepin/compliance/ComplianceStatus.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.fedoraproject.candlepin.compliance;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.fedoraproject.candlepin.model.Entitlement;
+
+/**
+ * ComplianceStatus
+ * 
+ * Represents the compliance status for a given consumer. Carries information
+ * about which products are fully entitled, not entitled, or partially entitled. (stacked)
+ */
+public class ComplianceStatus {
+    
+    private Date date;
+    private List<String> nonCompliantProducts;
+    private Map<String, List<Entitlement>> compliantProducts;
+    private Map<String, List<Entitlement>> partiallyCompliantProducts; // stacked
+
+    public ComplianceStatus(Date date) {
+        this.date = date;
+    }
+    
+    /**
+     * @return Map of compliant product IDs and the entitlements that provide them.
+     */
+    public Map<String, List<Entitlement>> getCompliantProducts() {
+        return compliantProducts;
+    }
+
+    /**
+     * 
+     * @return Date this compliance status was checked for.
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @return List of product IDs installed on the consumer, but not provided by any 
+     * entitlement. (not even partially) 
+     */
+    public List<String> getNonCompliantProducts() {
+        return nonCompliantProducts;
+    }
+
+    /**
+     * @return Map of compliant product IDs and the entitlements that partially 
+     * provide them. (i.e. partially stacked)
+     */
+    public Map<String, List<Entitlement>> getPartiallyCompliantProducts() {
+        return partiallyCompliantProducts;
+    }
+
+    public void setCompliantProducts(
+        Map<String, List<Entitlement>> compliantProducts) {
+        this.compliantProducts = compliantProducts;
+    }
+}
