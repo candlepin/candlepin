df3fb3ee78784433c2d8f9f07e9b4c0b90a4e275
Devan Goodwin <dgoodwin@redhat.com>
2012-01-20 16:38:14 -0400

    Add two files that were missed.

---
+++ proxy/src/main/java/org/candlepin/pinsetter/tasks/RegenEnvEntitlementCertsJob.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.pinsetter.tasks;
+
+import java.util.Set;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import com.google.inject.Inject;
+import org.candlepin.controller.PoolManager;
+import org.candlepin.model.Environment;
+
+/**
+ * RegenEnvEntitlementCertsJob
+ *
+ * Regenerates entitlements within an environment which are affected by the
+ * promotion/demotion of the given content sets.
+ */
+public class RegenEnvEntitlementCertsJob implements Job {
+
+    private PoolManager poolManager;
+    public static final String ENV = "env_id";
+    public static final String CONTENT = "content_ids";
+
+    @Inject
+    public RegenEnvEntitlementCertsJob(PoolManager poolManager) {
+        this.poolManager = poolManager;
+    }
+
+    @Override
+    public void execute(JobExecutionContext arg0) throws JobExecutionException {
+        Environment env = (Environment) arg0.getJobDetail().getJobDataMap().get(
+            ENV);
+        Set<String> contentIds = (Set<String>)
+            arg0.getJobDetail().getJobDataMap().get(CONTENT);
+
+        this.poolManager.regenerateCertificatesOf(env, contentIds);
+    }
+}
