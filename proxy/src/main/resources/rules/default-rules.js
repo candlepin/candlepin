16cd1f813391d073a22b74235318a488a56e8582
James Bowes <jbowes@redhat.com>
2012-01-23 19:42:32 -0400

    Update productId on refreshPools if changed

--- proxy/src/main/resources/rules/default-rules.js
+++ proxy/src/main/resources/rules/default-rules.js
@@ -849,6 +849,7 @@ var Pool = {
             if (productsChanged) {
                 log.info("   Subscription products changed.");
                 existingPool.setProductName(sub.getProduct().getName());
+		existingPool.setProductId(sub.getProduct().getId());
                 existingPool.getProvidedProducts().clear();
 
                 if (sub.getProvidedProducts() != null) {
