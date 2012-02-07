c4ef07465735ceb3ad2178dbe41324bad674d03b
jesus m. rodriguez <jmrodri@gmail.com>
2012-02-06 21:59:12 -0500

    class to track deleted consumers.
    Curator has a number of utility methods, by Consumer or by the id alone.
    Also added a count method because we really only need to know if there are
    0 or >0 available.
    
    We can get rid of the methods we're not using.

---
+++ proxy/src/main/java/org/candlepin/model/DeletedConsumerCurator.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.model;
+
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.List;
+
+/**
+ * DeletedConsumerCurator
+ */
+public class DeletedConsumerCurator extends
+    AbstractHibernateCurator<DeletedConsumer> {
+
+    public DeletedConsumerCurator() {
+        super(DeletedConsumer.class);
+    }
+
+    public DeletedConsumer findByConsumer(Consumer c) {
+        return findByConsumerId(c.getId());
+    }
+
+    public DeletedConsumer findByConsumerId(String cid) {
+        return (DeletedConsumer) currentSession().createCriteria(DeletedConsumer.class)
+            .add(Restrictions.eq("consumerId", cid))
+            .uniqueResult();
+    }
+
+    public List<DeletedConsumer> findByOwner(Owner o) {
+        return findByOwnerId(o.getId());
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<DeletedConsumer> findByOwnerId(String oid) {
+        return currentSession().createCriteria(DeletedConsumer.class)
+            .add(Restrictions.eq("ownerId", oid))
+            .list();
+    }
+
+    public int countByConsumer(Consumer c) {
+        return countByConsumerId(c.getId());
+    }
+
+    public int countByConsumerId(String cid) {
+        return (Integer) currentSession().createCriteria(DeletedConsumer.class)
+                        .add(Restrictions.eq("consumerId", cid))
+                        .setProjection(Projections.rowCount()).uniqueResult();
+
+    }
+}
---
+++ proxy/src/test/java/org/candlepin/model/test/DeletedConsumerCuratorTest.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.model.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.candlepin.model.Consumer;
+import org.candlepin.model.DeletedConsumer;
+import org.candlepin.model.DeletedConsumerCurator;
+import org.candlepin.model.Owner;
+import org.candlepin.test.DatabaseTestFixture;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * DeletedConsumerCuratorTest
+ */
+public class DeletedConsumerCuratorTest extends DatabaseTestFixture {
+
+    private DeletedConsumerCurator dcc;
+
+    @Before
+    public void init() {
+        super.init();
+        dcc = injector.getInstance(DeletedConsumerCurator.class);
+
+        DeletedConsumer dc = new DeletedConsumer();
+        dc.setConsumerId("abcde");
+        dc.setOwnerId("10");
+        dcc.create(dc);
+
+        dc = new DeletedConsumer();
+        dc.setConsumerId("fghij");
+        dc.setOwnerId("10");
+        dcc.create(dc);
+
+        dc = new DeletedConsumer();
+        dc.setConsumerId("klmno");
+        dc.setOwnerId("20");
+        dcc.create(dc);
+    }
+
+    @Test
+    public void byConsumerId() {
+        DeletedConsumer found = dcc.findByConsumerId("abcde");
+        assertEquals("abcde", found.getConsumerId());
+    }
+
+    @Test
+    public void byConsumer() {
+        Consumer c = mock(Consumer.class);
+        when(c.getId()).thenReturn("abcde");
+        DeletedConsumer found = dcc.findByConsumer(c);
+        assertEquals("abcde", found.getConsumerId());
+    }
+
+    @Test
+    public void byOwnerId() {
+        List<DeletedConsumer> found = dcc.findByOwnerId("10");
+        assertEquals(2, found.size());
+    }
+
+    @Test
+    public void byOwner() {
+        Owner o = mock(Owner.class);
+        when(o.getId()).thenReturn("20");
+        List<DeletedConsumer> found = dcc.findByOwner(o);
+        assertEquals(1, found.size());
+    }
+
+    @Test
+    public void countByConsumerId() {
+        assertEquals(1, dcc.countByConsumerId("abcde"));
+        assertEquals(0, dcc.countByConsumerId("dontfind"));
+        assertEquals(1, dcc.countByConsumerId("fghij"));
+    }
+
+    @Test
+    public void countByConsumer() {
+        Consumer c = mock(Consumer.class);
+        when(c.getId()).thenReturn("abcde");
+        assertEquals(1, dcc.countByConsumer(c));
+
+        c = mock(Consumer.class);
+        when(c.getId()).thenReturn("dontfind");
+        assertEquals(0, dcc.countByConsumer(c));
+    }
+}
