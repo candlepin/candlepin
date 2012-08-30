94e8889e4650a8e478a095001dcbe133f25584e1
Adrian Likins <alikins@redhat.com>
2012-08-30 15:58:10 -0400

    spec files fixes for entitlement bind changes
    Since we often fail entitlements because all
    pools have been filtered before the rules, instead
    of being forbidden by the rules, these tests needed
    to be updated just an empty return from the bind.

--- spec/virt_spec.rb
+++ spec/virt_spec.rb
@@ -60,9 +60,8 @@ describe 'Standalone Virt-Limit Subscriptions' do
 
     # Should not be able to use the pool as this guest is not on the correct
     # host:
-    lambda do
-      @guest2_client.consume_pool(@guest_pool['id'])
-    end.should raise_exception(RestClient::Forbidden)
+    entitlements = @guest2_client.consume_pool(@guest_pool['id'])
+    entitlements.should be_nil
   end
 
   it 'should list host restricted pool only for its guests' do
