4ecca90236f91c9b40d8a314e1cb9cf2c0fcdbb6
Devan Goodwin <dgoodwin@redhat.com>
2012-09-04 16:53:27 -0300

    Improvements for the performance test script.
    With performance work done recently we can now run this much quicker so
    bumping up the subscription count to 1000 (very high) and the system
    count to 5000. (probably still a little low but I want to limit the setup
    time)
    
    Now uses 5 installed products, and the subscriptions each provide some
    random combination of those.
    
    Tried threading the registrations but there is no gain, each autobind
    attempt arrives and gets the same pools as the preferred best fit, but
    then the binds are locked so they all queue up waiting for each other to
    finish. Even worse the entitlements quickly exhaust as they're all
    trying to get the same pools.
    
    Stacking is still disabled, suspect there is a bug here in behavior I am
    seeing but can't pin it down yet.

--- buildconf/scripts/performance-dataloader.rb
+++ buildconf/scripts/performance-dataloader.rb
@@ -13,38 +13,73 @@
 
 require  "../../client/ruby/candlepin_api"
 require 'pp'
+require 'benchmark'
 
 ADMIN_USERNAME = "admin"
 ADMIN_PASSWORD = "admin"
 HOST = "localhost"
 PORT = 8443
 
-# This is low, we need more but not until there's a list pools fix, the performance is just too slow:
-SYSTEM_COUNT = 100
-POOL_COUNT = 300
+# Number of systems to register and autobind:
+SYSTEM_COUNT = 5000
 
-# Using eng products from our test data so we can test clients manually as
-# well. (easier as we have product certs laying around for these)
-ENG_PROD = '37060'
-STACKED_ENG_PROD = '100000000000002'
+# Number of pools/subscriptions to create:
+POOL_COUNT = 1000
+
+# Array of installed products each system will have. When we create pools,
+# we'll have them provide some random combination of these. These are all
+# from our test data, as the products must already exist, and we can more
+# easily test our own clients against the org this script creates.
+INSTALLED_PRODUCTS = ['37060', '100000000000002', '37065', '37070', '37068']
+
+ARCH = "x86_64"
 
 def random_string prefix=nil
   prefix ||= "rand"
   return "#{prefix}-#{rand(100000000)}"
 end
 
-def create_systems(cp, owner_key, count, installed_pids, facts)
+def random_provided_products
+  return INSTALLED_PRODUCTS.slice(0, rand(INSTALLED_PRODUCTS.size + 1))
+end
+
+def create_systems(cp, owner_key, count, facts)
   installed_products = []
-  installed_pids.each do |pid|
+  INSTALLED_PRODUCTS.each do |pid|
     installed_products << {'productId' => pid, 'productName' => pid}
   end
   (1..count).each do |i|
     sys = cp.register(random_string("sys-#{i}"), :system, nil, facts, nil, owner_key, [], installed_products)
     # Do an autobind:
-    ents = cp.consume_product(nil, {:uuid => sys['uuid']})
-    puts "   #{i} - #{sys['name']} (#{sys['uuid']})"
+    ents = nil
+    time = Benchmark.realtime do
+      ents = cp.consume_product(nil, {:uuid => sys['uuid']})
+    end
+    quantity = 0
+    if ! ents.nil?
+      ents.each do |e|
+        quantity += e["quantity"]
+      end
+    end
+    puts "#{i} - (#{sys['uuid']}) ents=#{(ents.nil? ? 0 : ents.size)} quantity=#{quantity} time=#{time}\n"
   end
 end
+
+def create_pools(cp, owner_key, count, attributes, quantity)
+  end_date = Date.new(2025, 5, 29)
+  time = Benchmark.realtime do
+    (1..(POOL_COUNT)).each do |i|
+      mkt_prod_id = random_string('mkt-prod')
+      mkt_prod = cp.create_product(mkt_prod_id, mkt_prod_id, {
+        :attributes => attributes,
+      })
+      sub1 = cp.create_subscription(owner_key, mkt_prod['id'], quantity,
+        random_provided_products(), rand(10000), rand(10000), nil, end_date)
+    end
+  end
+  puts "   #{time} seconds"
+end
+
 cp = Candlepin.new(ADMIN_USERNAME, ADMIN_PASSWORD, nil, nil, HOST, PORT)
 
 # Create the owner we will load up with data:
@@ -52,41 +87,27 @@ owner = cp.create_owner random_string("perf-owner")
 
 puts "Created test owner: #{owner['key']}"
 
-# Create subscriptions, each of which is for a different marketing product,
-# but all of which provide the same provided products.
-end_date = Date.new(2025, 5, 29)
-puts "Creating #{POOL_COUNT / 2} virt-limit subscriptions/pools:"
-(1..(POOL_COUNT / 2)).each do |i|
-  mkt_prod_id = random_string('mkt-prod')
-  mkt_prod = cp.create_product(mkt_prod_id, mkt_prod_id, {
-    :attributes => {
-      :virt_limit => "4",
-      :requires_consumer_type => "system",
-    }
-  })
-  sub1 = cp.create_subscription(owner['key'], mkt_prod['id'], 50,
-    [ENG_PROD], rand(10000), rand(10000), nil, end_date)
-end
+puts "Creating #{POOL_COUNT} subscriptions/pools:"
+attributes = {
+  :virt_limit => "4",
+  :arch => ARCH,
+  :requires_consumer_type => "system",
+  :support_level => "Amazing",
+  :support_type => "Everything",
+  #:stacking_id => "sostacked",
+  #:sockets => "2",
+  #"multi-entitlement" => "yes",
+}
+# To enable stacking, uncomment the attributes above and adjust the quantity
+# here:
+create_pools(cp, owner['key'], POOL_COUNT, attributes, 500)
 
-puts "Creating #{POOL_COUNT / 2} stacked subscriptions/pools:"
-(1..(POOL_COUNT / 2)).each do |i|
-  mkt_prod_id = random_string('mkt-prod')
-  mkt_prod = cp.create_product(mkt_prod_id, mkt_prod_id, {
-    :attributes => {
-      :stacking_id => "sostacked",
-      :sockets => "2",
-      "multi-entitlement" => "yes",
-    }
-  })
-  # Quantity 2 to force stacking to kick in:
-  sub1 = cp.create_subscription(owner['key'], mkt_prod['id'], 2,
-    [STACKED_ENG_PROD], rand(10000), rand(10000), nil, end_date)
-end
 puts "Refreshing pools..."
-cp.refresh_pools(owner['key'])
+time = Benchmark.realtime do
+  cp.refresh_pools(owner['key'])
+end
+puts "   #{time} seconds"
 
-puts "Creating #{SYSTEM_COUNT / 2} regular systems:"
-create_systems(cp, owner['key'], SYSTEM_COUNT / 2, [ENG_PROD], {})
-puts "Creating #{SYSTEM_COUNT / 2} stacked systems:"
-create_systems(cp, owner['key'], SYSTEM_COUNT / 2, [STACKED_ENG_PROD], {"cpu.cpu_socket(s)" => 8})
+puts "Creating #{SYSTEM_COUNT} systems:"
+create_systems(cp, owner['key'], SYSTEM_COUNT, {"cpu.cpu_socket(s)" => 8, "uname.machine" => ARCH})
 
