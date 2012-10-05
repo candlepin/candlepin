3e4521cbf0760bb9cd4e55aa0fdd9494719c7fe3
Bryan Kearney <bkearney@redhat.com>
2012-10-05 16:37:45 -0400

    857918: Add quotes around the invalid service level

--- src/main/java/org/candlepin/resource/ConsumerResource.java
+++ src/main/java/org/candlepin/resource/ConsumerResource.java
@@ -463,7 +463,7 @@ public class ConsumerResource {
             }
             throw new BadRequestException(
                 i18n.tr(
-                    "Service level {0} is not available " +
+                    "Service level '{0}' is not available " +
                     "to consumers of organization {1}.",
                     serviceLevel, owner.getKey()));
         }
