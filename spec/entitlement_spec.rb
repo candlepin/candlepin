f8c012572491565bd19885d78280b59a9ed46ef6
Devan Goodwin <dgoodwin@redhat.com>
2012-10-11 17:05:40 -0300

    Revert "Update spec tests to reflect new return code"
    This reverts commit b88d07521a3e697ebbb5157c0fe1eea4659f8a2e.

--- spec/entitlement_spec.rb
+++ spec/entitlement_spec.rb
@@ -99,9 +99,8 @@ describe 'Entitlements' do
   it 'should not allow consuming two entitlements for the same product' do
     @system.consume_product @super_awesome.id
 
-    lambda do
-      entitlements = @system.consume_product @super_awesome.id
-    end.should raise_exception(RestClient::BadRequest)
+    entitlements = @system.consume_product @super_awesome.id
+    entitlements.should be_nil
   end
 
   it 'should not allow consuming two entitlements in same pool' do
