fef0fb684af87fc1fce9ad161c8ba0ea89fdbc67
Dennis Crissman <dcrissma@redhat.com>
2013-03-05 15:29:40 -0500

    Make JsonProvider more re-usable

--- src/main/java/org/candlepin/resteasy/JsonProvider.java
+++ src/main/java/org/candlepin/resteasy/JsonProvider.java
@@ -14,8 +14,24 @@
  */
 package org.candlepin.resteasy;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.ext.Provider;
+
 import org.candlepin.config.Config;
 import org.candlepin.jackson.HateoasBeanPropertyFilter;
+import org.codehaus.jackson.jaxrs.Annotations;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+import org.codehaus.jackson.map.AnnotationIntrospector;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.map.introspect.JacksonAnnotationIntrospector;
+import org.codehaus.jackson.map.ser.impl.SimpleFilterProvider;
+import org.codehaus.jackson.xc.JaxbAnnotationIntrospector;
+import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+
+import com.google.inject.Inject;
 
 /**
  * JsonProvider
@@ -28,44 +44,44 @@ import org.candlepin.jackson.HateoasBeanPropertyFilter;
 @Consumes({"application/*+json", "text/json"})
 public class JsonProvider extends JacksonJsonProvider {
 
-    public static register(boolean indentJson){
-        ResteasyProviderFactory rpf = ResteasyProviderFactory.getInstance();
-        JsonProvider jsonprovider = new JsonProvider(indentJson);
-        rpf.addMessageBodyReader(jsonprovider);
-        rpf.addMessageBodyWriter(jsonprovider);
-        RegisterBuiltin.register(rpf);
-    }
+	public static void register(boolean indentJson){
+		ResteasyProviderFactory rpf = ResteasyProviderFactory.getInstance();
+		JsonProvider jsonprovider = new JsonProvider(indentJson);
+		rpf.addMessageBodyReader(jsonprovider);
+		rpf.addMessageBodyWriter(jsonprovider);
+		RegisterBuiltin.register(rpf);
+	}
 
-    @Inject
-    public JsonProvider(Config config){
-        this(config.indentJson());
-    }
+	@Inject
+	public JsonProvider(Config config){
+		this(config.indentJson());
+	}
 
-    public JsonProvider(boolean indentJson) {
-        // Prefer jackson annotations, but use jaxb if no jackson.
-        super(Annotations.JACKSON, Annotations.JAXB);
+	public JsonProvider(boolean indentJson) {
+		// Prefer jackson annotations, but use jaxb if no jackson.
+		super(Annotations.JACKSON, Annotations.JAXB);
 
-        ObjectMapper mapper = _mapperConfig.getDefaultMapper();
-        configureHateoasObjectMapper(mapper, config);
-        setMapper(mapper);
-    }
+		ObjectMapper mapper = _mapperConfig.getDefaultMapper();
+		configureHateoasObjectMapper(mapper, indentJson);
+		setMapper(mapper);
+	}
 
-    private void configureHateoasObjectMapper(ObjectMapper mapper, boolean indentJson) {
-        mapper.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
+	private void configureHateoasObjectMapper(ObjectMapper mapper, boolean indentJson) {
+		mapper.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
 
-        if (indentJson) {
-            mapper.configure(SerializationConfig.Feature.INDENT_OUTPUT, true);
-        }
+		if (indentJson) {
+			mapper.configure(SerializationConfig.Feature.INDENT_OUTPUT, true);
+		}
 
-        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
-        filterProvider = filterProvider.addFilter("ApiHateoas",
-            new HateoasBeanPropertyFilter());
-        filterProvider.setFailOnUnknownId(false);
-        mapper.setFilters(filterProvider);
+		SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+		filterProvider = filterProvider.addFilter("ApiHateoas",
+				new HateoasBeanPropertyFilter());
+		filterProvider.setFailOnUnknownId(false);
+		mapper.setFilters(filterProvider);
 
-        AnnotationIntrospector primary = new JacksonAnnotationIntrospector();
-        AnnotationIntrospector secondary = new JaxbAnnotationIntrospector();
-        AnnotationIntrospector pair = new AnnotationIntrospector.Pair(primary, secondary);
-        mapper.setAnnotationIntrospector(pair);
-    }
+		AnnotationIntrospector primary = new JacksonAnnotationIntrospector();
+		AnnotationIntrospector secondary = new JaxbAnnotationIntrospector();
+		AnnotationIntrospector pair = new AnnotationIntrospector.Pair(primary, secondary);
+		mapper.setAnnotationIntrospector(pair);
+	}
 }
