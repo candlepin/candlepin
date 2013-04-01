d95f80d4d409b14109b3b88397199077767c82fd
William Poteat <wpoteat@redhat.com>
2013-04-01 09:37:31 -0400

    Updates to manifest data.
    Account, Contract, and Order are on Pool json.
    Entitlement json did not need these fields as they became redundant.

--- src/main/java/org/candlepin/model/Entitlement.java
+++ src/main/java/org/candlepin/model/Entitlement.java
@@ -100,10 +100,6 @@ public class Entitlement extends AbstractHibernateObject implements Linkable, Ow
 
     private Integer quantity;
 
-    private String accountNumber;
-    private String contractNumber;
-    private String orderNumber;
-
     private boolean dirty = false;
 
     /**
@@ -143,10 +139,6 @@ public class Entitlement extends AbstractHibernateObject implements Linkable, Ow
         endDate = endDateIn;
         quantity = quantityIn == null || quantityIn.intValue() < 1 ?
             1 : quantityIn;
-
-        this.accountNumber = pool.getAccountNumber();
-        this.contractNumber = pool.getContractNumber();
-        this.orderNumber = pool.getOrderNumber();
     }
 
     /**
@@ -269,30 +261,6 @@ public class Entitlement extends AbstractHibernateObject implements Linkable, Ow
          * No-op, here to aid with updating objects which have nested objects that were
          * originally sent down to the client in HATEOAS form.
          */
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public String getContractNumber() {
-        return contractNumber;
-    }
-
-    public void setContractNumber(String contractNumber) {
-        this.contractNumber = contractNumber;
-    }
-
-    public String getOrderNumber() {
-        return orderNumber;
-    }
-
-    public void setOrderNumber(String orderNumber) {
-        this.orderNumber = orderNumber;
     }
 
     @Override
