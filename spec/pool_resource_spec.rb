cb3523840ca122ba5184eaa2b20f392c1489094c
Alex Wood <awood@redhat.com>
2013-05-03 13:34:29 -0400

    Remove some extra code from pool resource spec test.

--- spec/pool_resource_spec.rb
+++ spec/pool_resource_spec.rb
@@ -146,7 +146,7 @@ describe 'Pool Resource' do
     owner = create_owner random_string('test_owner')
     product = create_product(name='some_product')
 
-    sub = @cp.create_subscription(owner['key'], product['id'], 25) 
+    @cp.create_subscription(owner['key'], product.id, 25)
     @cp.refresh_pools(owner['key'])
     pools = @cp.list_pools
     pool = pools.select { |p| p['owner']['key'] == owner['key'] }.first
@@ -154,7 +154,6 @@ describe 'Pool Resource' do
     user = user_client(owner, 'billy')
     system = consumer_client(user, 'system')
 
-    system.consume_product(product.id)
     @cp.get_pool(pool.id, system.uuid)['calculatedAttributes']['suggested_quantity'].should == "1"
   end
 
