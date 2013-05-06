49f4cdc47304cff60af8d63a25a000cddc0f4cb2
Adrian Likins <alikins@redhat.com>
2013-05-06 15:53:37 -0400

    Use database test fixture for the Arch tests

--- src/test/java/org/candlepin/model/test/ArchTest.java
+++ src/test/java/org/candlepin/model/test/ArchTest.java
@@ -16,14 +16,19 @@ package org.candlepin.model.test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.candlepin.model.Arch;
+import org.candlepin.model.ConsumerType;
+import org.candlepin.test.DatabaseTestFixture;
 import org.junit.Test;
 
 /**
  * ArchTest
  */
-public class ArchTest {
+public class ArchTest extends DatabaseTestFixture {
 
+    /* FIXME: inherit DatabaseTestFixture and read/write to Arch'es to db */
     @Test
     public void testArch() {
         Arch arch = new Arch("i386", "i386");
@@ -41,7 +46,6 @@ public class ArchTest {
         assertEquals("intel i386", arch.getLabel());
     }
 
-
     @Test
     public void testSetLabel() {
         Arch arch = new Arch("i386", "i386");
@@ -49,4 +53,18 @@ public class ArchTest {
         assertEquals("intel i386", arch.getLabel());
     }
 
+    @Test
+    public void testPersistArch() {
+        beginTransaction();
+        String archHash = String.valueOf(
+            Math.abs(Long.valueOf("test-arch-1".hashCode())));
+        Arch arch = new Arch(archHash, "test-arch-1");
+        entityManager().persist(arch);
+
+        commitTransaction();
+
+        List<?> results = entityManager().createQuery(
+                "select arch from Arch as arch").getResultList();
+        assertEquals(1, results.size());
+    }
 }
