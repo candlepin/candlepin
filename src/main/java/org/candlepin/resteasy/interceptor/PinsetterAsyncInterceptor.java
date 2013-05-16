0f0d8d9f5f75badd1b191e0f9421f655a4035c09
jesus m. rodriguez <jesusr@redhat.com>
2013-05-16 17:17:41 -0400

    heal entire org
    Added a new API that will trigger the healing of the all the Consumers in an
    Owner. The API returns a list of JobStatuses which the caller can use to check
    the status of the of the jobs.
    
    Added coded to handle scheduling multiple jobs based on the return of an array
    of JobDetails.

--- src/main/java/org/candlepin/resteasy/interceptor/PinsetterAsyncInterceptor.java
+++ src/main/java/org/candlepin/resteasy/interceptor/PinsetterAsyncInterceptor.java
@@ -32,6 +32,7 @@ import org.quartz.JobDetail;
 import org.quartz.impl.JobDetailImpl;
 
 import javax.ws.rs.ext.Provider;
+import org.apache.log4j.Logger;
 
 /**
  * Resteasy interceptor that handles scheduling a one-time pinsetter job if the
@@ -43,7 +44,7 @@ import javax.ws.rs.ext.Provider;
 @Provider
 @ServerInterceptor
 public class PinsetterAsyncInterceptor implements PostProcessInterceptor {
-
+    private static Logger log = Logger.getLogger(PinsetterAsyncInterceptor.class);
     private PinsetterKernel pinsetterKernel;
     private com.google.inject.Provider<Principal> principalProvider;
 
@@ -67,15 +68,32 @@ public class PinsetterAsyncInterceptor implements PostProcessInterceptor {
             JobDetail jobDetail = (JobDetail) entity;
             setJobPrincipal(jobDetail);
 
-            try {
-                JobStatus status = this.pinsetterKernel.scheduleSingleJob(jobDetail);
+            JobStatus status = this.scheduleJob(jobDetail);
+            response.setEntity(status);
+            response.setStatus(HttpResponseCodes.SC_ACCEPTED);
+        }
+        else if (entity instanceof JobDetail[]) {
+            JobDetail[] details = (JobDetail[]) entity;
+            JobStatus[] statuses = new JobStatus[details.length];
 
-                response.setEntity(status);
-                response.setStatus(HttpResponseCodes.SC_ACCEPTED);
+            int i = 0;
+            for (JobDetail jobDetail : details) {
+                setJobPrincipal(jobDetail);
+                JobStatus status = this.scheduleJob(jobDetail);
+                statuses[i++] = status;
             }
-            catch (PinsetterException e) {
-                throw new ServiceUnavailableException("Error scheduling refresh job.", e);
-            }
+
+            response.setEntity(statuses);
+            response.setStatus(HttpResponseCodes.SC_ACCEPTED);
+        }
+    }
+
+    private JobStatus scheduleJob(JobDetail detail) {
+        try {
+            return this.pinsetterKernel.scheduleSingleJob(detail);
+        }
+        catch (PinsetterException e) {
+            throw new ServiceUnavailableException("Error scheduling refresh job.", e);
         }
     }
 
