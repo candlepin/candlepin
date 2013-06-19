ab52bbb2a1da55b2547539f25b00fb1865206b26
Chris Duryee (beav) <cduryee@redhat.com>
2013-06-19 12:29:23 -0400

    remove extraneous use of @ variables in spec

--- spec/deleted_consumer_spec.rb
+++ spec/deleted_consumer_spec.rb
@@ -13,13 +13,13 @@ describe 'Deleted Consumer Resource' do
   it 'find all deleted consumers' do
     date = Time.now.utc.iso8601
     # make sure we have at least one deleted record
-    @owner1 = create_owner random_string('test_owner1')
-    @username1 = random_string("user1")
-    @consumername1 = random_string("consumer1")
-    @user1 = user_client(@owner1, @username1)
-    @consumer1 = consumer_client(@user1, @consumername1)
-    uuid = @consumer1.uuid
-    @cp.unregister(@consumer1.uuid)
+    owner1 = create_owner random_string('test_owner1')
+    username1 = random_string("user1")
+    consumername1 = random_string("consumer1")
+    user1 = user_client(owner1, username1)
+    consumer1 = consumer_client(user1, consumername1)
+    uuid = consumer1.uuid
+    @cp.unregister(consumer1.uuid)
 
     @cp.get_deleted_consumers().length.should_not == 0
     deleted_consumers = @cp.get_deleted_consumers(date=date)
