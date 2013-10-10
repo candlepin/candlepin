2f3fcfd7deb000ed40de28c1f549cb0527d5dde0
Devan Goodwin <dgoodwin@redhat.com>
2013-10-10 16:14:24 -0300

    Logging touchups.
    As requested by beav.

--- src/main/java/org/candlepin/resource/ConsumerResource.java
+++ src/main/java/org/candlepin/resource/ConsumerResource.java
@@ -840,7 +840,7 @@ public class ConsumerResource {
         }
 
         if (changesMade) {
-            log.info("Consumer " + toUpdate.getUuid() + " updated.");
+            log.debug("Consumer " + toUpdate.getUuid() + " updated.");
 
             ComplianceStatus compliance = complianceRules.getStatus(toUpdate,
                 Calendar.getInstance().getTime());
