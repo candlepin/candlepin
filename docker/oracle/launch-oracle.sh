09e3288f0f67abcd0d1622b2e61bafdd4e42b795
Devan Goodwin <dgoodwin@redhat.com>
2014-07-23 11:23:22 -0300

    Oracle launcher script for within containers.

---
+++ docker/oracle/launch-oracle.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+# Need to adjust hostname for the running container:
+mkdir -p /run/lock/subsys
+source /etc/profile
+sed -i -E "s/HOST = [^)]+/HOST = $HOSTNAME/g" /u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora
+sed -i -E "s/HOST = [^)]+/HOST = $HOSTNAME/g" /u01/app/oracle/product/11.2.0/xe/network/admin/tnsnames.ora
+
+/etc/init.d/oracle-xe start
