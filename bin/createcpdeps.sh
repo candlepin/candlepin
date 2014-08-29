a869322f6a88d9b07b9039ef68548555a9dec0cd
jesus m. rodriguez <jesusr@redhat.com>
2014-08-28 21:31:18 -0400

    create script uses new local_artifacts task

--- bin/createcpdeps.sh
+++ bin/createcpdeps.sh
@@ -3,19 +3,33 @@ if [ "$1" == "" ] ; then
  exit
 fi
 
-export M2_REPO="$1/repo"
+deps_dir=$1
 
 function flatten ()
 {
-    cd $M2_REPO
+    pushd $M2_REPO > /dev/null
     rm -rf *.jar
     # move all the jars to the top
     find . -name '*.jar' | xargs -n 1 -I jarfile mv jarfile .
     # remove all the directories
     find . -type d | cut -c 3- | xargs -n 1 rm -rf
-    cd ..
+    popd > /dev/null
     echo "candlepin deps located in $M2_REPO"
 }
 
-buildr nocheckstyle=1 artifacts
-flatten
+function create_project_deps ()
+{
+    cd $1
+    buildr nocheckstyle=1 candlepin:$1:local_artifacts
+    cd ..
+}
+
+# abort on errors
+set -e
+
+# create the deps for each project
+for proj in "server" "gutterball" "common"; do
+    export M2_REPO="$deps_dir/repo/$proj"
+    create_project_deps $proj
+    flatten
+done
