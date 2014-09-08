6ac29d2bb428031e795fa591b3ad8d074fc5f1b6
ckozak <ckozak@redhat.com>
2014-09-08 12:04:42 -0400

    Support dot notation in MongoDBCurator.getValuesByKey
    Added testing for this feature

--- gutterball/src/main/java/org/candlepin/gutterball/curator/MongoDBCurator.java
+++ gutterball/src/main/java/org/candlepin/gutterball/curator/MongoDBCurator.java
@@ -14,20 +14,6 @@
  */
 
 package org.candlepin.gutterball.curator;
-/**
- * Copyright (c) 2009 - 2012 Red Hat, Inc.
- *
- * This software is licensed to you under the GNU General Public License,
- * version 2 (GPLv2). There is NO WARRANTY for this software, express or
- * implied, including the implied warranties of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
- * along with this software; if not, see
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
- *
- * Red Hat trademarks are not licensed under GPLv2. No permission is
- * granted to use or replicate Red Hat trademarks that are incorporated
- * in this software or its documentation.
- */
 
 import java.util.LinkedList;
 import java.util.List;
@@ -91,9 +77,23 @@ public abstract class MongoDBCurator<M extends DBObject> {
 
     @SuppressWarnings("unchecked")
     protected <T> List<T> getValuesByKey(String fromKey, Iterable<DBObject> dbos) {
+        String[] keyPath = fromKey.split("\\.");
         List<T> results = new LinkedList<T>();
         for (DBObject obj : dbos) {
-            results.add((T) obj.get(fromKey));
+            DBObject currentObject = obj;
+            for (int i = 0; i < keyPath.length - 1; i++) {
+                currentObject = (DBObject) currentObject.get(keyPath[i]);
+                if (currentObject == null) {
+                    // Avoid exceptions, this lets us return null
+                    currentObject = new BasicDBObject();
+                    break;
+                }
+            }
+            // We don't want lists of null, only include the value if it exists
+            T result = (T) currentObject.get(keyPath[keyPath.length - 1]);
+            if (result != null) {
+                results.add(result);
+            }
         }
         return results;
     }
