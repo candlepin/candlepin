81e5edced49ef583ec9d952d249f9fe790483f29
Devan Goodwin <dgoodwin@redhat.com>
2014-09-10 14:53:04 -0300

    Checkstyle.

--- server/src/test/java/org/candlepin/resource/HypervisorResourceTest.java
+++ server/src/test/java/org/candlepin/resource/HypervisorResourceTest.java
@@ -15,7 +15,6 @@
 package org.candlepin.resource;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
