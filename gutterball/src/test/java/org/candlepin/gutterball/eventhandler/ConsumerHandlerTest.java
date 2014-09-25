fa751bdc282a2641f311b498c6bb866eb416bf18
Michael Stead <mstead@redhat.com>
2014-09-25 12:36:13 -0300

    Added tests for event handlers

---
+++ gutterball/src/test/java/org/candlepin/gutterball/eventhandler/ConsumerHandlerTest.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2009 - 2012 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package org.candlepin.gutterball.eventhandler;
+
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import org.candlepin.gutterball.curator.jpa.ConsumerStateCurator;
+import org.candlepin.gutterball.model.jpa.ConsumerState;
+import org.candlepin.gutterball.model.jpa.Event;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Date;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ConsumerHandlerTest {
+
+    @Mock
+    private ConsumerStateCurator consumerStateCurator;
+
+    @Mock
+    private ObjectMapper mapper;
+
+    private ConsumerHandler handler;
+
+    @Before
+    public void setupTest() {
+        handler = new ConsumerHandler(mapper, consumerStateCurator);
+    }
+
+    @Test
+    public void testCuratorCreatesNewConsumerStateOnHandlingCreatedEvent() throws Exception {
+        Event event = new Event();
+        event.setNewEntity("test-string");
+
+        ConsumerState state = new ConsumerState();
+        when(mapper.readValue(eq(event.getNewEntity()), eq(ConsumerState.class))).thenReturn(state);
+
+        handler.handleCreated(event);
+        verify(consumerStateCurator).create(eq(state));
+    }
+
+    @Test
+    public void testCuratorUpdatesDeletedConsumerStateOnDeletedEvent() throws Exception {
+        Event event = new Event();
+        event.setOldEntity("test-string");
+
+        ConsumerState state = new ConsumerState("test-uuid", "owner-key", new Date());
+        when(mapper.readValue(eq(event.getOldEntity()), eq(ConsumerState.class))).thenReturn(state);
+
+        handler.handleDeleted(event);
+        verify(consumerStateCurator).setConsumerDeleted(eq(state.getUuid()), any(Date.class));
+    }
+
+    @Test
+    public void testConsumerStateIsNotUpdatedOnUpdateEvent() {
+        handler.handleUpdated(new Event());
+        verifyZeroInteractions(consumerStateCurator);
+    }
+
+}
