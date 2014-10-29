b3c08d906f40a673ab5262a8c6802283c2776769
Michael Stead <mstead@redhat.com>
2014-10-29 14:50:15 -0300

    Reuse candlepin's consumer UUID as ConsumerStatus key.
    No id needed as we expect there to be only one consumer
    checking in with that id.

--- gutterball/src/main/java/org/candlepin/gutterball/model/ConsumerState.java
+++ gutterball/src/main/java/org/candlepin/gutterball/model/ConsumerState.java
@@ -50,13 +50,6 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class ConsumerState {
 
     @Id
-    @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid")
-    @Column(length = 32)
-    @NotNull
-    @JsonIgnore
-    private String id;
-
     @Column(nullable = false, unique = true)
     @Size(max = 255)
     @NotNull
@@ -85,14 +78,6 @@ public class ConsumerState {
         this.uuid = uuid;
         this.ownerKey = ownerKey;
         this.created = created;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getUuid() {
