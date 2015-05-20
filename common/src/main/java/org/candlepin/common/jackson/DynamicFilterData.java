b74d9eec0d5982a4823338f8ac9c106eec683ee3
Chris Rog <crog@redhat.com>
2015-05-20 17:10:19 -0400

    Added enhancements to JSON attribute filtering
    - include and exclude may now be used together to create more precise
      filters; using both will default to blacklist mode
    - filter mode (whitelist/blacklist) is selectable internally, but
      API users have no way to set this as of yet

--- common/src/main/java/org/candlepin/common/jackson/DynamicFilterData.java
+++ common/src/main/java/org/candlepin/common/jackson/DynamicFilterData.java
@@ -22,6 +22,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+
 /**
  * DynamicFilterData
  *
@@ -31,21 +33,62 @@ import java.util.Map;
 public class DynamicFilterData {
     private static Logger log = LoggerFactory.getLogger(DynamicFilterData.class);
 
-    private Map<String, List<String>> filters;
-    private boolean excluding = true;
+    private static class Match {
+        private final int level;
+        private final boolean exact;
 
-    public DynamicFilterData(boolean excluding) {
-        this.filters = new HashMap<String, List<String>>();
-        this.excluding = excluding;
+        public Match(int level, boolean exact) {
+            this.level = level;
+            this.exact = exact;
+        }
+
+        public int getLevel() {
+            return this.level;
+        }
+
+        public boolean isExact() {
+            return this.exact;
+        }
     }
 
-    public void addAttributeFilter(String path) {
+
+    private Map<String, List<String>> includeFilters;
+    private Map<String, List<String>> excludeFilters;
+    private boolean whitelist;
+
+    public DynamicFilterData() {
+        this(false);
+    }
+
+    public DynamicFilterData(boolean whitelist) {
+        this.includeFilters = new HashMap<String, List<String>>();
+        this.excludeFilters = new HashMap<String, List<String>>();
+        this.whitelist = whitelist;
+    }
+
+    public void setWhitelistMode(boolean whitelist) {
+        this.whitelist = whitelist;
+    }
+
+    public void includeAttribute(String path) {
         if (path == null) {
             throw new IllegalArgumentException("path is null");
         }
 
+        this.addAttributeFilter(this.includeFilters, path);
+    }
+
+    public void excludeAttribute(String path) {
+        if (path == null) {
+            throw new IllegalArgumentException("path is null");
+        }
+
+        this.addAttributeFilter(this.excludeFilters, path);
+    }
+
+    private void addAttributeFilter(Map<String, List<String>> filters, String path) {
         String[] chunklets = path.split("\\.");
-        this.filters.put(path.toLowerCase(), Arrays.asList(chunklets));
+        filters.put(path.toLowerCase(), Arrays.asList(chunklets));
     }
 
     public boolean isAttributeExcluded(String path) {
@@ -54,25 +97,46 @@ public class DynamicFilterData {
     }
 
     public boolean isAttributeExcluded(List<String> path) {
-        boolean match = false;
+        Match iLevel = this.getFilterLevel(this.includeFilters, path);
+        Match eLevel = this.getFilterLevel(this.excludeFilters, path);
 
-        for (List<String> apath : this.filters.values()) {
-            int i = 0;
-            do {
-                String achunk = apath.get(i);
-                String pchunk = path.get(i);
-
-                match = achunk != null && achunk.equalsIgnoreCase(pchunk);
-                ++i;
-            } while (match && i < path.size() && i < apath.size());
-
-            if (match && (!this.excluding || i >= apath.size())) {
-                break;
-            }
-
-            match = false;
+        if (iLevel.isExact() && iLevel.getLevel() > eLevel.getLevel()) {
+            return false;
         }
 
-        return this.excluding ? match : !match;
+        if (eLevel.isExact() && eLevel.getLevel() > iLevel.getLevel()) {
+            return true;
+        }
+
+        return this.whitelist && (iLevel.getLevel() < 1 || iLevel.getLevel() < eLevel.getLevel());
+    }
+
+    private Match getFilterLevel(Map<String, List<String>> filters, List<String> path) {
+        int level = 0;
+        boolean exact = false;
+
+        for (List<String> fpath : filters.values()) {
+            boolean match = false;
+            int i = 0;
+
+            do {
+                String fchunk = fpath.get(i);
+                String pchunk = path.get(i);
+
+                match = fchunk != null && fchunk.equalsIgnoreCase(pchunk);
+                ++i;
+            } while (match && i < path.size() && i < fpath.size());
+
+            if (match && i > level) {
+                level = i + 1;
+                exact = i >= fpath.size();
+
+                if (exact) {
+                    break;
+                }
+            }
+        }
+
+        return new Match(level, exact);
     }
 }
