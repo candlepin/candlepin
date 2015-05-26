fda31d395b8154cc784f9ffd1ae8b254a99a6ff0
Devan Goodwin <dgoodwin@redhat.com>
2015-05-26 15:02:21 -0300

    Add Dockerfile for Candlepin on RHEL 6 from official rpms.
    This will not work externally, but with some modification to the base OS
    (either CentOS, or on a subscribed RHEL system), and use of the public
    Candlepin repos, it should work.

---
+++ docker/candlepin-rhel6/startup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+env
+
+# TODO: use env variables to check which database we're linked to, for now
+# we'll just assume postgres.
+
+/root/cpsetup -u postgres --dbhost $DB_PORT_5432_TCP_ADDR --dbport $DB_PORT_5432_TCP_PORT
+#/usr/share/candlepin/cpsetup
+
+
+service tomcat6 start
+/usr/bin/supervisord -c /etc/supervisord.conf
