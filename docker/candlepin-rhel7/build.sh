0a05f861377c27d337b1d3dad3df5eb93058a811
Devan Goodwin <dgoodwin@redhat.com>
2015-06-04 12:27:49 -0300

    Update docker build scripts to publish images.

--- docker/candlepin-rhel7/build.sh
+++ docker/candlepin-rhel7/build.sh
@@ -2,9 +2,7 @@ docker build -t candlepin/candlepin-rhel7:latest .
 CP_VERSION=`docker run -ti candlepin/candlepin-rhel7:latest rpm -q --queryformat '%{VERSION}' candlepin`
 echo "Built container for candlepin: $CP_VERSION"
 
-#docker tag -f candlepin/candlepin-rhel7:latest docker.usersys.redhat.com/candlepin/candlepin-rhel7:latest
-
-#docker tag -f candlepin/candlepin-rhel7:latest docker.usersys.redhat.com/candlepin/candlepin-rhel7:$CP_VERSION
-
-#docker push docker.usersys.redhat.com/candlepin/candlepin-rhel7:$CP_VERSION
+docker tag -f candlepin/candlepin-rhel7:latest docker.usersys.redhat.com/candlepin/candlepin-rhel7:latest
+docker tag -f candlepin/candlepin-rhel7:latest docker.usersys.redhat.com/candlepin/candlepin-rhel7:$CP_VERSION
+docker push docker.usersys.redhat.com/candlepin/candlepin-rhel7:$CP_VERSION
 
