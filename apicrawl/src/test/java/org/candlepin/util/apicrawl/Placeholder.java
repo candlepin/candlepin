83e819a635055058522958605bf8d5d8aa67af82
Alex Wood <awood@redhat.com>
2015-06-15 16:54:07 -0400

    Add missing package declaration to dummy class used to trick Buildr.

--- apicrawl/src/test/java/org/candlepin/util/apicrawl/Placeholder.java
+++ apicrawl/src/test/java/org/candlepin/util/apicrawl/Placeholder.java
@@ -12,6 +12,7 @@
  * granted to use or replicate Red Hat trademarks that are incorporated
  * in this software or its documentation.
  */
+package org.candlepin.util.apicrawl;
 
 public class Placeholder {
     private Placeholder() {
