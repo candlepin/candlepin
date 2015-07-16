35fb593b9fa1f862ade58d2a81f45e1927ce6aed
Devan Goodwin <dgoodwin@redhat.com>
2015-07-16 12:40:11 -0300

    Fix EL7 docker rpm container test.
    Addition of set -e introduced problems in the startup.sh script, as the cpsetup
    command does error out on EL7 because the service it not yet running, thus we
    can't check status and initialize. Previously this would fail and continue in
    the script, starting supervisor.
    
    Instead we'll keep the set -e, but add a new cpsetup option that skips service
    stop/restart/init, and eliminates the failed command during startup.
    
    Also added wget calls to fetch the latest cpsetup and cpdb for this base
    container used for rpm installation tests. This isn't used by the more official
    tagged containers with a build in them, only for the base image that doesn't
    yet have cp installed.

--- docker/candlepin-rhel7-base/startup.sh
+++ docker/candlepin-rhel7-base/startup.sh
@@ -15,6 +15,12 @@ CANDLEPIN
 cat /etc/yum.repos.d/candlepin.repo
 yum install -y candlepin
 
-/usr/share/candlepin/cpsetup -u postgres --dbhost $DB_PORT_5432_TCP_ADDR --dbport $DB_PORT_5432_TCP_PORT
+# Fetch the latest cpsetup and cpdb scripts and overwrite whatever came in the RPM so we don't have to tag
+# a build to get a change into the container. This should only be done in the base containers, not official
+# candlepin build containers.
+wget -O /usr/share/candlepin/cpsetup https://raw.githubusercontent.com/candlepin/candlepin/master/server/code/setup/cpsetup
+wget -O /usr/share/candlepin/cpsetup https://raw.githubusercontent.com/candlepin/candlepin/master/server/code/setup/cpsetup
+
+/usr/share/candlepin/cpsetup -u postgres --dbhost $DB_PORT_5432_TCP_ADDR --dbport $DB_PORT_5432_TCP_PORT --skip-service
 
 /usr/bin/supervisord -c /etc/supervisord.conf
