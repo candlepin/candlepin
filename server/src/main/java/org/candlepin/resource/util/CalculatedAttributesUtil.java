5b1fc432afb9f9b8cdc1e4bce2bbe4d49ece67c4
William Poteat <wpoteat@redhat.com>
2015-07-23 15:16:46 -0400

    1243146: Suggested quantity should be calculated for pool listings

--- server/src/main/java/org/candlepin/resource/util/CalculatedAttributesUtil.java
+++ server/src/main/java/org/candlepin/resource/util/CalculatedAttributesUtil.java
@@ -14,13 +14,17 @@
  */
 package org.candlepin.resource.util;
 
+import org.candlepin.model.Consumer;
 import org.candlepin.model.Pool;
 import org.candlepin.model.Pool.PoolComplianceType;
+import org.candlepin.policy.js.quantity.QuantityRules;
+import org.candlepin.policy.js.quantity.SuggestedQuantity;
 
 import com.google.inject.Inject;
 
 import org.xnap.commons.i18n.I18n;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -33,11 +37,13 @@ import java.util.Map;
  */
 public class CalculatedAttributesUtil {
 
+    private QuantityRules quantityRules;
     private I18n i18n;
 
     @Inject
-    public CalculatedAttributesUtil(I18n i18n) {
+    public CalculatedAttributesUtil(QuantityRules quantityRules, I18n i18n) {
         this.i18n = i18n;
+        this.quantityRules = quantityRules;
     }
 
     public Map<String, String> buildCalculatedAttributes(Pool pool, Date date) {
@@ -55,7 +61,41 @@ public class CalculatedAttributesUtil {
 
     public void setCalculatedAttributes(List<Pool> poolList, Date date) {
         for (Pool pool : poolList) {
-            pool.setCalculatedAttributes(this.buildCalculatedAttributes(pool, date));
+            Map<String, String> attrMap = pool.getCalculatedAttributes();
+            if (attrMap == null) {
+                attrMap = new HashMap<String, String>();
+                pool.setCalculatedAttributes(attrMap);
+            }
+            attrMap.putAll(this.buildCalculatedAttributes(pool, date));
+        }
+    }
+
+    public void setQuantityAttributes(Pool pool, Consumer c, Date date) {
+        List<Pool> poolList = new ArrayList<Pool>();
+        poolList.add(pool);
+        setQuantityAttributes(poolList, c, date);
+    }
+
+    public void setQuantityAttributes(List<Pool> poolList, Consumer c, Date date) {
+        if (c == null) {
+            return;
+        }
+        Map<String, SuggestedQuantity> results = quantityRules.getSuggestedQuantities(
+                poolList, c, date);
+
+        for (Pool p : poolList) {
+            SuggestedQuantity suggested = results.get(p.getId());
+
+            Map<String, String> attrMap = p.getCalculatedAttributes();
+            if (attrMap == null) {
+                attrMap = new HashMap<String, String>();
+                p.setCalculatedAttributes(attrMap);
+            }
+
+            attrMap.put("suggested_quantity",
+                String.valueOf(suggested.getSuggested()));
+            attrMap.put("quantity_increment",
+                String.valueOf(suggested.getIncrement()));
         }
     }
 }
