16c30e629a07f22715d78ab1e32f151ae6ef30b2
Vritant Jain <adarshvritant@gmail.com>
2017-02-15 01:53:44 -0500

    Revert "1412113: autobind fails when binding 2 pools together with same stackid"
    This reverts commit 0fb3f3d9e0451833b0f21ae377b2ee1ee52b917d.

--- server/client/ruby/candlepin_api.rb
+++ server/client/ruby/candlepin_api.rb
@@ -532,9 +532,8 @@ class Candlepin
     delete("/consumers/#{uuid}")
   end
 
-  def revoke_all_entitlements(uuid = nil)
-    uuid = @uuid unless uuid
-    delete("/consumers/#{uuid}/entitlements")
+  def revoke_all_entitlements()
+    delete("/consumers/#{@uuid}/entitlements")
   end
 
   def autoheal_org(owner_key)
