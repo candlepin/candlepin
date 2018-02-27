ceee9c4cd22acc2e2328c3e2c4577612b679b100
Michael Stead <mstead@redhat.com>
2018-02-27 12:59:05 -0400

    1546236: Properly handle connections when they are recreated
    Whenever the QpidConnection needed to be rebuilt, the existing
    connection was being disregarded and a new one created. The old
    connections were not getting cleaned up causing candlepin to
    exhaust all connections available to Qpid.
    
    This was primarily surfacing when HornetQ would try to resend
    failed messages but the Qpid queue was in FLOW_STOPPED. In
    this case, QpidConnection would be marked as stale because of the
    exception, and would create a new connection to Qpid, leaving
    the other around. In the case of FLOW_STOPPED, a new connection
    isn't required - we just need to wait for the queue to catch up.
    
    Since the exceptions thrown by Qpid for the various failure
    cases, we could never tell exactly when to recreate the connection
    or when the connection was Ok.
    
    This patch addresses this PR by monitoring the Qpid status and
    only creating new connections when required as well as cleaning
    up after itself.
    
    Notes:
    - Pulled Qpid connection checking via QMF into an isolated service
      separate from SuspendModeTransitioner.
    - Added a listener framework for objects to be notified when the latest
      Qpid status is determined.
    - SuspendModeTransitioner now listens for Qpid status updates instead
      of running the checks itself.
    - QpidConnection now listens for status updates so that it can better
      manage its connection state (connect, reconnect)
      * When Qpid is FLOW_STOPPPED, block message sending to Qpid until
        it is notified that Qpid is able to accept the message.
      * When notified that Qpid went down, close the old connection so that
        it is no longer hanging around.
      * When notified that Qpid is back up, re-create the connection again.

--- server/src/main/java/org/candlepin/controller/SuspendModeTransitioner.java
+++ server/src/main/java/org/candlepin/controller/SuspendModeTransitioner.java
@@ -14,13 +14,8 @@
  */
 package org.candlepin.controller;
 
-import org.candlepin.audit.QpidConnection;
-import org.candlepin.audit.QpidConnection.STATUS;
-import org.candlepin.audit.QpidQmf;
-import org.candlepin.audit.QpidQmf.QpidStatus;
+import org.candlepin.audit.QpidStatus;
 import org.candlepin.cache.CandlepinCache;
-import org.candlepin.common.config.Configuration;
-import org.candlepin.config.ConfigProperties;
 import org.candlepin.model.CandlepinModeChange;
 import org.candlepin.model.CandlepinModeChange.Mode;
 import org.candlepin.model.CandlepinModeChange.Reason;
@@ -30,49 +25,25 @@ import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.math.BigInteger;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Logic to transition Candlepin between different modes (SUSPEND, NORMAL) based
- * on what is the current status of Qpid Broker.
- *
- * This logic can also be run periodically using startPeriodicExecutions
+ * on what the current status of Qpid Broker. This class is notified of state
+ * changes by listening for events from the QpidStatusMonitor.
  *
  * Using this class, clients can attempt to transition to appropriate mode. The
  * attempt may be no-op if no transition is required.
- * @author fnguyen
  *
  */
-public class SuspendModeTransitioner implements Runnable {
+public class SuspendModeTransitioner implements QpidStatusListener {
     private static Logger log = LoggerFactory.getLogger(SuspendModeTransitioner.class);
 
-    private int delay;
-    private BigInteger failedAttempts = BigInteger.ZERO;
-    /**
-     * Single threaded periodic task.
-     */
-    private ScheduledExecutorService execService;
     private ModeManager modeManager;
-    private QpidQmf qmf;
-    private QpidConnection qpidConnection;
     private CandlepinCache candlepinCache;
 
     @Inject
-    public SuspendModeTransitioner(Configuration config, ScheduledExecutorService execService,
-        CandlepinCache cache) {
-        this.execService = execService;
+    public SuspendModeTransitioner(CandlepinCache cache) {
         this.candlepinCache = cache;
-
-        delay = config.getInt(ConfigProperties.QPID_MODE_TRANSITIONER_DELAY);
-        if (delay < 1) {
-            int defaultDelay = Integer.parseInt(
-                ConfigProperties.DEFAULT_PROPERTIES.get(ConfigProperties.QPID_MODE_TRANSITIONER_DELAY));
-            log.warn("{} is an invalid delay setting. Must be greater than 0. Defaulting to {}", delay,
-                defaultDelay);
-            delay = defaultDelay;
-        }
     }
 
     /**
@@ -85,46 +56,16 @@ public class SuspendModeTransitioner implements Runnable {
         this.modeManager = modeManager;
     }
 
-    @Inject
-    public void setQmf(QpidQmf qmf) {
-        this.qmf = qmf;
-    }
-
-    @Inject
-    public void setQpidConnection(QpidConnection qpidConnection) {
-        this.qpidConnection = qpidConnection;
-    }
-
     /**
-     * Enables to run the transitioning logic periodically.
+     * Called each time the QpidStatusMonitor checks for a Qpid status update,
+     * and based on this change, updates the candlepin mode.
+     *
+     * @param oldStatus the status of Qpid on the previous update.
+     * @param newStatus the current status of Qpid.
      */
-    public void startPeriodicExecutions() {
-        log.info("Starting Suspend Mode Transitioner");
-        schedule();
-    }
-
-    /**
-     * Schedules execution of the Suspend Mode check run every N seconds (where N is
-     * configurable).
-     */
-    private void schedule() {
-        log.debug("Next Transitioner check will run after {} seconds", delay);
-        if (failedAttempts.compareTo(BigInteger.ZERO) > 0) {
-            log.info("SuspendModeTransitioner failed to reconnect to the Qpid Broker " +
-                "{} times. Next attempt in {} seconds", failedAttempts, delay);
-        }
-        execService.schedule(this, delay, TimeUnit.SECONDS);
-    }
-
     @Override
-    public void run() {
-        log.debug("Executing periodic transition attempt");
-        try {
-            transitionAppropriately();
-        }
-        finally {
-            schedule();
-        }
+    public void onStatusUpdate(QpidStatus oldStatus, QpidStatus newStatus) {
+        transitionAppropriately(newStatus);
     }
 
     /**
@@ -139,64 +80,47 @@ public class SuspendModeTransitioner implements Runnable {
      * Qpid up, the transitioner will try to reconnect to the broker. This reconnect
      * may fail. In that case the transition to NORMAL mode shouldn't go through.
      */
-    public synchronized void transitionAppropriately() {
+    private synchronized void transitionAppropriately(QpidStatus status) {
         log.debug("Attempting to transition to appropriate Mode");
-        try {
-            QpidStatus status = qmf.getStatus();
-            CandlepinModeChange modeChange = modeManager.getLastCandlepinModeChange();
+        CandlepinModeChange modeChange = modeManager.getLastCandlepinModeChange();
+        log.debug("Qpid status is {}, the current mode is {}", status, modeChange);
 
-            log.debug("Qpid status is {}, the current mode is {}", status, modeChange);
-
-            if (status != QpidStatus.CONNECTED) {
-                qpidConnection.setConnectionStatus(STATUS.JMS_OBJECTS_STALE);
-            }
-
-            if (modeChange.getMode() == Mode.SUSPEND) {
-                switch (status) {
-                    case CONNECTED:
-                        log.info("Connection to qpid is restored! Reconnecting Qpid and" +
-                            " entering NORMAL mode");
-                        failedAttempts = BigInteger.ZERO;
-                        modeManager.enterMode(Mode.NORMAL, Reason.QPID_UP);
-                        cleanStatusCache();
-                        break;
-                    case FLOW_STOPPED:
-                    case DOWN:
-                        failedAttempts = failedAttempts.add(BigInteger.ONE);
-                        log.debug("Staying in {} mode. So far {} failed attempts", status, failedAttempts);
-                        break;
-                    default:
-                        throw new RuntimeException("Unknown status: " + status);
-                }
-            }
-            else if (modeChange.getMode() == Mode.NORMAL) {
-                switch (status) {
-                    case FLOW_STOPPED:
-                        log.debug("Will need to transition Candlepin into SUSPEND Mode because " +
-                            "the Qpid connection is flow stopped");
-                        modeManager.enterMode(Mode.SUSPEND, Reason.QPID_FLOW_STOPPED);
-                        cleanStatusCache();
-                        break;
-                    case DOWN:
-                        log.debug("Will need to transition Candlepin into SUSPEND Mode because " +
-                            "the Qpid connection is down");
-                        modeManager.enterMode(Mode.SUSPEND, Reason.QPID_DOWN);
-                        cleanStatusCache();
-                        break;
-                    case CONNECTED:
-                        log.debug("Connection to Qpid is ok and current mode is NORMAL. No-op!");
-                        break;
-                    default:
-                        throw new RuntimeException("Unknown status: " + status);
-                }
+        if (modeChange.getMode() == Mode.SUSPEND) {
+            switch (status) {
+                case CONNECTED:
+                    log.info("Connection to qpid is restored! Reconnecting Qpid and" +
+                        " entering NORMAL mode");
+                    modeManager.enterMode(Mode.NORMAL, Reason.QPID_UP);
+                    cleanStatusCache();
+                    break;
+                case FLOW_STOPPED:
+                case DOWN:
+                    log.debug("Staying in {} mode.", status);
+                    break;
+                default:
+                    throw new RuntimeException("Unknown status: " + status);
             }
         }
-        catch (Throwable t) {
-            log.error("Error while executing period Suspend Transitioner check", t);
-            /*
-             * Nothing more we can do here, since this is scheduled thread. We must
-             * hope that this error won't infinitely recur with each scheduled execution
-             */
+        else if (modeChange.getMode() == Mode.NORMAL) {
+            switch (status) {
+                case FLOW_STOPPED:
+                    log.debug("Will need to transition Candlepin into SUSPEND Mode because " +
+                        "the Qpid connection is flow stopped");
+                    modeManager.enterMode(Mode.SUSPEND, Reason.QPID_FLOW_STOPPED);
+                    cleanStatusCache();
+                    break;
+                case DOWN:
+                    log.debug("Will need to transition Candlepin into SUSPEND Mode because " +
+                        "the Qpid connection is down");
+                    modeManager.enterMode(Mode.SUSPEND, Reason.QPID_DOWN);
+                    cleanStatusCache();
+                    break;
+                case CONNECTED:
+                    log.debug("Connection to Qpid is ok and current mode is NORMAL. No-op!");
+                    break;
+                default:
+                    throw new RuntimeException("Unknown status: " + status);
+            }
         }
     }
 
@@ -207,4 +131,5 @@ public class SuspendModeTransitioner implements Runnable {
     private void cleanStatusCache() {
         candlepinCache.getStatusCache().clear();
     }
+
 }
