d4f0b4bc973bf519edb5b3c245daea1dbd70caca
Vritant Jain <adarshvritant@gmail.com>
2018-03-20 05:54:36 -0400

    Automatic commit using alias

--- server/src/main/java/org/candlepin/auth/permissions/OwnerPermission.java
+++ server/src/main/java/org/candlepin/auth/permissions/OwnerPermission.java
@@ -51,7 +51,7 @@ public class OwnerPermission implements Permission, Serializable {
         Access requiredAccess) {
         if (target instanceof Owned) {
             // First make sure the owner matches:
-            if (owner.getKey().equals(((Owned) target).getOwner().getKey()) &&
+            if (owner.getId().equals(((Owned) target).getOwner().getId()) &&
                 access.provides(requiredAccess)) {
                 return true;
             }
