36e46b72b4cb7d71eb225574e66cd4e680543a43
Michael Stead <mstead@redhat.com>
2018-03-21 11:03:11 -0300

    1658212: Fixed Artemis resend on error to prevent lost messages
    Before this patch, resend message on error wasn't working
    all the time, which could sometimes lead to lost messages
    that wouldn't make it to Qpid.
    
    This patch moves from having one ClientSession receiving
    messages for all queues, to using a ClientSession per
    queue/listener.
    
    Prior to this patch, client sessions were set to
    auto-acknowledge and auto-commit messages. This is no
    longer the case. Each listener's ListenerWrapper now
    manages its session by committing on success and rolling
    back on failure.
    
    Another important change is that as soon as a listener
    recieves a message, that message should be immediately
    acknowledged so that the session can keep track of the
    number of times a message was sent (despite success or
    failure). This is key to allowing retry on error to
    function correctly.

--- server/src/main/java/org/candlepin/audit/ListenerWrapper.java
+++ server/src/main/java/org/candlepin/audit/ListenerWrapper.java
@@ -14,60 +14,75 @@
  */
 package org.candlepin.audit;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.apache.activemq.artemis.api.core.ActiveMQException;
 import org.apache.activemq.artemis.api.core.client.ClientMessage;
+import org.apache.activemq.artemis.api.core.client.ClientSession;
 import org.apache.activemq.artemis.api.core.client.MessageHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 
 /**
- * ListnerWrapper
+ * A ListenerWrapper serves as a bridge between Artemis' message delevery and candlepin's
+ * Event handling. It is responsible for translating the message to an Event object,
+ * and passing it along to a candlepin EventListener for processing.
+ *
+ * The ListenerWrapper handles all client session management and ensures that the
+ * message remains on the message queue until it has been successfully processed by
+ * the listener.
  */
 public class ListenerWrapper implements MessageHandler {
 
     private EventListener listener;
     private static Logger log = LoggerFactory.getLogger(ListenerWrapper.class);
     private ObjectMapper mapper;
-    public ListenerWrapper(EventListener listener, ObjectMapper mapper) {
+    private ClientSession session;
+
+    public ListenerWrapper(EventListener listener, ObjectMapper mapper, ClientSession session) {
         this.listener = listener;
         this.mapper = mapper;
+        this.session = session;
     }
 
     @Override
     public void onMessage(ClientMessage msg) {
-        String body = msg.getBodyBuffer().readString();
-        log.debug("Got event: {}", body);
-
-        // Exceptions thrown here will cause the event to remain in ActiveMQ:
+        String body = "";
         try {
+            // Acknowledge the message so that the server knows that it was received.
+            // By doing this, the server can update the delivery counts which plays
+            // part in calculating redelivery delays.
+            msg.acknowledge();
+            log.debug("ActiveMQ message {} acknowledged for listener: {}", msg.getMessageID(), listener);
+
+            // Process the message via our EventListener framework.
+            body = msg.getBodyBuffer().readString();
+            log.debug("Got event: {}", body);
             Event event = mapper.readValue(body, Event.class);
             listener.onEvent(event);
-        }
-        catch (JsonMappingException e) {
-            log.error("Unable to deserialize event object from msg: " + body, e);
-            throw new RuntimeException("Error deserializing event", e);
-        }
-        catch (JsonParseException e) {
-            log.error("Unable to deserialize event object from msg: " + body, e);
-            throw new RuntimeException("Error deserializing event", e);
-        }
-        catch (IOException e) {
-            log.error("Unable to deserialize event object from msg: " + body, e);
-            throw new RuntimeException("Error deserializing event", e);
-        }
 
-        try {
-            msg.acknowledge();
-            log.debug("ActiveMQ message acknowledged for listener: " + listener);
+            // Finally commit the session so that the message is taken out of the queue.
+            session.commit();
         }
-        catch (ActiveMQException e) {
-            log.error("Unable to acknowledge ActiveMQ msg", e);
+        catch (Exception e) {
+            // Log a warning instead of a full stack trace to reduce log size.
+            String messageId = (msg == null) ? "" : Long.toString(msg.getMessageID());
+            String reason = (e.getCause() == null) ? e.getMessage() : e.getCause().getMessage();
+            log.error("Unable to process message {}: {}", messageId, reason);
+
+            // If debugging is enabled log a more in depth message.
+            log.debug("Unable to process message. Rolling back client session.\n{}", body, e);
+            try {
+                // When any exception occurs while processing the message, we need to roll back
+                // the session so that the message remains on the queue.
+                session.rollback();
+            }
+            catch (ActiveMQException amqe) {
+                log.error("Unable to roll back client session.", amqe);
+            }
+
+            // Session was rolled back, nothing left to do.
         }
     }
 
--- server/src/test/java/org/candlepin/audit/EventSourceTest.java
+++ server/src/test/java/org/candlepin/audit/EventSourceTest.java
@@ -14,18 +14,15 @@
  */
 package org.candlepin.audit;
 
-import static org.junit.Assert.*;
 import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.apache.activemq.artemis.api.core.ActiveMQException;
-import org.apache.activemq.artemis.api.core.ActiveMQExceptionType;
 import org.apache.activemq.artemis.api.core.client.ClientConsumer;
 import org.apache.activemq.artemis.api.core.client.ClientSession;
 import org.apache.activemq.artemis.api.core.client.ClientSessionFactory;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -39,15 +36,87 @@ import org.mockito.runners.MockitoJUnitRunner;
 public class EventSourceTest {
 
     @Mock private ClientSessionFactory clientSessionFactory;
-    @Mock private ClientSession clientSession;
 
-    @Before
-    public void init() throws Exception {
-        when(clientSessionFactory.createSession(eq(true), eq(true), eq(0)))
-            .thenReturn(clientSession);
+    @Test
+    public void registeringNewListenerCreatesNewSessionAndStartsIt() throws Exception {
+        ClientSession clientSession = mock(ClientSession.class);
+        EventSource source = createEventSourceStubbedWithFactoryCreation();
+        when(clientSessionFactory.createSession(eq(false), eq(false), eq(0))).thenReturn(clientSession);
+        when(clientSession.createConsumer(any(String.class))).thenReturn(mock(ClientConsumer.class));
+
+        source.registerListener(mock(EventListener.class));
+        verify(clientSessionFactory).createSession(eq(false), eq(false), eq(0));
+        verify(clientSession).start();
     }
+
+    @Test
+    public void shouldStopAndCloseSessionsOnShutdown() throws Exception {
+        ClientSession session1 = mock(ClientSession.class);
+        ClientSession session2 = mock(ClientSession.class);
+
+        EventSource eventSource = createEventSourceStubbedWithFactoryCreation();
+        when(clientSessionFactory.createSession(eq(false), eq(false), eq(0)))
+            .thenReturn(session1).thenReturn(session2);
+        when(session1.createConsumer(any(String.class))).thenReturn(mock(ClientConsumer.class));
+        when(session2.createConsumer(any(String.class))).thenReturn(mock(ClientConsumer.class));
+
+        eventSource.registerListener(mock(EventListener.class));
+        eventSource.registerListener(mock(EventListener.class));
+        eventSource.shutDown();
+
+        // Verify that all client sessions were stopped and closed.
+        verify(session1).stop();
+        verify(session1).close();
+
+        verify(session2).stop();
+        verify(session2).close();
+
+        // Verify that the client session factory was closed.
+        verify(clientSessionFactory).close();
+    }
+
+    @Test
+    public void shouldStopAndCloseSessionOnShutdownWhenExceptionThrownOnStop() throws Exception {
+        ClientSession session = mock(ClientSession.class);
+
+        EventSource eventSource = createEventSourceStubbedWithFactoryCreation();
+        when(clientSessionFactory.createSession(eq(false), eq(false), eq(0))).thenReturn(session);
+        when(session.createConsumer(any(String.class))).thenReturn(mock(ClientConsumer.class));
+        doThrow(new ActiveMQException("Forced")).when(session).stop();
+
+        eventSource.registerListener(mock(EventListener.class));
+        eventSource.shutDown();
+
+        // Verify that close was at least still called.
+        verify(session).close();
+
+        // Verify that the client session factory was closed regardless of the exception.
+        verify(clientSessionFactory).close();
+    }
+
+    @Test
+    public void shouldStopAndCloseSessionOnShutdownWhenExceptionThrownOnClose() throws Exception {
+        ClientSession session = mock(ClientSession.class);
+
+        EventSource eventSource = createEventSourceStubbedWithFactoryCreation();
+        when(clientSessionFactory.createSession(eq(false), eq(false), eq(0))).thenReturn(session);
+        when(session.createConsumer(any(String.class))).thenReturn(mock(ClientConsumer.class));
+        doThrow(new ActiveMQException("Forced")).when(session).close();
+
+        eventSource.registerListener(mock(EventListener.class));
+        eventSource.shutDown();
+
+        // Verify that stop was at least still called.
+        verify(session).stop();
+
+        // Verify that the client session factory was closed regardless of the exception.
+        verify(clientSessionFactory).close();
+    }
+
     /**
-     * @return
+     * Creates a new EventSource with a mocked ClientSessionFactory.
+     *
+     * @return the new EventSource
      */
     private EventSource createEventSourceStubbedWithFactoryCreation() {
         return new EventSource(new ObjectMapper()) {
@@ -55,94 +124,6 @@ public class EventSourceTest {
                 return clientSessionFactory;
             }
         };
-    }
-
-    @Test
-    public void shouldStartSessionWhenCreatingEventSource() throws Exception {
-        createEventSourceStubbedWithFactoryCreation();
-        verify(this.clientSession).start();
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void shouldThrowRunTimeExceptionWhenSessionCreateFailsDuringEventSourceCreation()
-        throws Exception {
-        doThrow(new ActiveMQException()).when(clientSession).start();
-        createEventSourceStubbedWithFactoryCreation();
-        fail("Should have thrown runtime exception");
-    }
-
-    @Test
-    public void shouldNotThrowExceptionWhenQueueCreationFails() throws Exception {
-        EventSource eventSource = createEventSourceStubbedWithFactoryCreation();
-        doThrow(new ActiveMQException(ActiveMQExceptionType.QUEUE_DOES_NOT_EXIST))
-            .when(clientSession).createQueue(anyString(), anyString(), eq(true));
-        EventListener eventListener = mock(EventListener.class);
-        eventSource.registerListener(eventListener);
-
-        verify(clientSession, never()).createConsumer(anyString()); //should not be invoked
-    }
-
-    @Test
-    public void shouldCreateNewConsumerWhenQueueDoesNotExistOnRegisterListenerCall()
-        throws Exception {
-        EventSource eventSource = createEventSourceStubbedWithFactoryCreation();
-        ClientConsumer mockCC = mock(ClientConsumer.class);
-        when(clientSession.createConsumer(anyString()))
-            .thenReturn(mockCC);
-        EventListener eventListener = mock(EventListener.class);
-        eventSource.registerListener(eventListener);
-
-        //make sure queue is created.
-        verify(clientSession).createQueue(anyString(), anyString(), eq(true));
-        verify(mockCC).setMessageHandler(any(ListenerWrapper.class));
-    }
-
-    @Test
-    public void shouldCreateNewConsumerWhenQueueExistsOnRegisterListenerCall()
-        throws Exception {
-        EventSource eventSource = createEventSourceStubbedWithFactoryCreation();
-        ClientConsumer mockCC = mock(ClientConsumer.class);
-        when(clientSession.createConsumer(anyString()))
-            .thenReturn(mockCC);
-        doThrow(new ActiveMQException(ActiveMQExceptionType.QUEUE_EXISTS))
-            .when(clientSession).createQueue(anyString(), anyString());
-        EventListener eventListener = mock(EventListener.class);
-        //invoke
-        eventSource.registerListener(eventListener);
-
-        //verify listener is still added.
-        verify(mockCC).setMessageHandler(any(ListenerWrapper.class));
-    }
-
-    @Test
-    public void shouldStopAndCloseSessionOnShutdown() throws Exception {
-        EventSource eventSource = createEventSourceStubbedWithFactoryCreation();
-
-        eventSource.shutDown();
-
-        verify(clientSession).stop();
-        verify(clientSession).close();
-    }
-
-    @Test
-    public void shouldStopAndCloseSessionOnShutdownWhenExceptionThrownOnStop()
-        throws Exception {
-        EventSource eventSource = createEventSourceStubbedWithFactoryCreation();
-        doThrow(new ActiveMQException()).when(clientSession).stop();
-
-        eventSource.shutDown();
-
-        assertTrue(true); //so sorry!
-    }
-
-    @Test
-    public void shouldStopAndCloseSessionOnShutdownWhenExceptionThrownOnClose()
-        throws Exception {
-        EventSource eventSource = createEventSourceStubbedWithFactoryCreation();
-        doThrow(new ActiveMQException()).when(clientSession).close();
-
-        eventSource.shutDown();
-        verify(this.clientSession).stop();
     }
 
 }
