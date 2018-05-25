26e7f525f493fa1c2b4f024ea47101ec2d1b4b65
Alex Wood <awood@redhat.com>
2018-05-25 11:42:21 -0400

    Use local DER tag constants.

--- server/src/main/java/org/candlepin/util/DERUtil.java
+++ server/src/main/java/org/candlepin/util/DERUtil.java
@@ -36,36 +36,37 @@ public class DERUtil {
     // Note that the tag value is only the 5 least significant bits in a tag byte.
     // These constants exist in the crypto provider libraries but for the purposes of making
     // some of our code provider independent, I'm defining them here.
-    public static final int BOOLEAN            = 0x01;
-    public static final int INTEGER            = 0x02;
-    public static final int BIT_STRING         = 0x03;
-    public static final int OCTET_STRING       = 0x04;
-    public static final int NULL               = 0x05;
-    public static final int OBJECT_IDENTIFIER  = 0x06;
-    public static final int EXTERNAL           = 0x08;
-    public static final int ENUMERATED         = 0x0a; // decimal 10
-    public static final int SEQUENCE           = 0x10; // decimal 16
-    public static final int SEQUENCE_OF        = 0x10; // used to model a SEQUENCE of the same type.
-    public static final int SET                = 0x11; // decimal 17
-    public static final int SET_OF             = 0x11; // used to model a SET of the same type.
+    public static final int BOOLEAN_TAG_NUM            = 0x01;
+    public static final int INTEGER_TAG_NUM            = 0x02;
+    public static final int BIT_STRING_TAG_NUM         = 0x03;
+    public static final int OCTET_STRING_TAG_NUM       = 0x04;
+    public static final int NULL_TAG_NUM               = 0x05;
+    public static final int OBJECT_IDENTIFIER_TAG_NUM  = 0x06;
+    public static final int EXTERNAL_TAG_NUM           = 0x08;
+    public static final int ENUMERATED_TAG_NUM         = 0x0a; // decimal 10
+    public static final int SEQUENCE_TAG_NUM           = 0x10; // decimal 16
+    public static final int SEQUENCE_OF_TAG_NUM        = 0x10; // used to model a SEQUENCE of the same type.
+    public static final int SET_TAG_NUM                = 0x11; // decimal 17
+    public static final int SET_OF_TAG_NUM             = 0x11; // used to model a SET of the same type.
 
-    public static final int NUMERIC_STRING     = 0x12; // decimal 18
-    public static final int PRINTABLE_STRING   = 0x13; // decimal 19
-    public static final int T61_STRING         = 0x14; // decimal 20
-    public static final int VIDEOTEX_STRING    = 0x15; // decimal 21
-    public static final int IA5_STRING         = 0x16; // decimal 22
-    public static final int UTC_TIME           = 0x17; // decimal 23
-    public static final int GENERALIZED_TIME   = 0x18; // decimal 24
-    public static final int GRAPHIC_STRING     = 0x19; // decimal 25
-    public static final int VISIBLE_STRING     = 0x1a; // decimal 26
-    public static final int GENERAL_STRING     = 0x1b; // decimal 27
-    public static final int UNIVERSAL_STRING   = 0x1c; // decimal 28
-    public static final int BMP_STRING         = 0x1e; // decimal 30
-    public static final int UTF8_STRING        = 0x0c; // decimal 12
+    public static final int NUMERIC_STRING_TAG_NUM     = 0x12; // decimal 18
+    public static final int PRINTABLE_STRING_TAG_NUM   = 0x13; // decimal 19
+    public static final int T61_STRING_TAG_NUM         = 0x14; // decimal 20
+    public static final int VIDEOTEX_STRING_TAG_NUM    = 0x15; // decimal 21
+    public static final int IA5_STRING_TAG_NUM         = 0x16; // decimal 22
+    public static final int UTC_TIME_TAG_NUM           = 0x17; // decimal 23
+    public static final int GENERALIZED_TIME_TAG_NUM   = 0x18; // decimal 24
+    public static final int GRAPHIC_STRING_TAG_NUM     = 0x19; // decimal 25
+    public static final int VISIBLE_STRING_TAG_NUM     = 0x1a; // decimal 26
+    public static final int GENERAL_STRING_TAG_NUM     = 0x1b; // decimal 27
+    public static final int UNIVERSAL_STRING_TAG_NUM   = 0x1c; // decimal 28
+    public static final int BMP_STRING_TAG_NUM         = 0x1e; // decimal 30
+    public static final int UTF8_STRING_TAG_NUM        = 0x0c; // decimal 12
 
-    public static final int CONSTRUCTED        = 0x20; // decimal 32
-    public static final int APPLICATION        = 0x40; // decimal 64
-    public static final int TAGGED             = 0x80; // decimal 128
+    public static final int CONSTRUCTED_TYPE           = 0x20; // decimal 32
+    public static final int APPLICATION_TYPE           = 0x40; // decimal 64
+    public static final int CONTEXT_SPECIFIC_TYPE      = 0x80; // decimal 128
+
     /**
      * When the length of an ASN1 encoded value changes then the number of bytes representing
      * the length of the value can change as well.  We need to calculate that change because
