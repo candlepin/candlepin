e2927f32f45f5e066faf1873a4560f73cb81dc5e
aparadkar <aparadka@redhat.com>
2018-07-03 17:05:23 -0400

    ENT-616: Update SubjectKeyIdentifierWriter for isolation [m] (#2050)
    * ENT-616: Update SubjectKeyIdentifierWriter for isolation
    
    - add toOctetString default method
    - keeps IT from having to use BouncyCastle directly
    - moved getSubjectKeyidentifier to the interface

---
+++ server/src/test/java/org/candlepin/pki/SubjectKeyIdentifierWriterTest.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2009 - 2018 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package org.candlepin.pki;
+
+import org.candlepin.pki.impl.DefaultSubjectKeyIdentifierWriter;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SubjectKeyIdentifierWriterTest {
+
+    @Test
+    public void testToOctetString() throws Exception {
+        DefaultSubjectKeyIdentifierWriter writer = new DefaultSubjectKeyIdentifierWriter();
+
+        // 0x04 is the DER tag for an octet string and 0x06 is the subsequent length of the octet string
+        byte[] foobarInAsn1 = {0x04, 0x06, 0x66, 0x6f, 0x6f, 0x62, 0x61, 0x72};
+        assertArrayEquals(foobarInAsn1, writer.toOctetString("foobar".getBytes("UTF-8")));
+    }
+}
