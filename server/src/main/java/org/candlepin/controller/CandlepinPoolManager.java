3f54a34b8b7da085441a479e8e1705fb4fe195c0
Chris Rog <crog@redhat.com>
2018-10-08 16:08:22 -0400

    1634040: Fixed an issue with object desync during pool cleanup
    - Fixed an issue where consumer instances could become desync'd
      during CPM.deletePools while recalculating compliance status
      for consumers in some cases, leading to an EntityExistsException

--- server/src/main/java/org/candlepin/controller/CandlepinPoolManager.java
+++ server/src/main/java/org/candlepin/controller/CandlepinPoolManager.java
@@ -70,7 +70,7 @@ import org.candlepin.util.Util;
 import org.candlepin.util.Traceable;
 import org.candlepin.util.TraceableParam;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Iterables;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
 
@@ -2224,22 +2224,19 @@ public class CandlepinPoolManager implements PoolManager {
                     this.enforcer.postUnbind(entitlement.getConsumer(), this, entitlement);
                 }
 
-                /* Save the consumer IDs and then detach all the keys in the consumerStackEnts map.
-                 * Otherwise during the status calculations, the facts proxy objects objects will be resolved
-                 * and the memory use will grow linearly with the number of consumers instead of remaining
-                 * constant as we calculate the status of each consumer.  See BZ 1584259 */
-                List<String> consumers = new ArrayList<String>(consumerStackedEnts.keySet().size());
-                for (Consumer c : consumerStackedEnts.keySet()) {
-                    consumers.add(c.getId());
-                }
-                this.consumerCurator.batchDetach(consumerStackedEnts.keySet());
-
-                log.info("Recomputing status for {} consumers", consumers.size());
+                log.info("Recomputing status for {} consumers", consumerStackedEnts.keySet().size());
 
                 // Recalculate status for affected consumers
-                for (List<String> subList : Lists.partition(consumers, 1000)) {
-                    for (Consumer consumer : this.consumerCurator.getConsumers(subList).list()) {
+                for (List<Consumer> subList : Iterables.partition(consumerStackedEnts.keySet(), 1000)) {
+                    for (Consumer consumer : subList) {
                         this.complianceRules.getStatus(consumer);
+
+                        // Detach the consumer object (and its children that receive cascaded detaches),
+                        // otherwise during the status calculations, the facts proxy objects objects will be
+                        // resolved and the memory use will grow linearly with the number of consumers
+                        // instead of remaining constant as we calculate the status of each consumer.
+                        //
+                        // See BZ 1584259 for details
                         this.consumerCurator.detach(consumer);
                     }
                     this.consumerCurator.flush();
