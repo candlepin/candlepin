8ff9602879b8768fc4d0929a7f7347ea01f74bfa
Chris Rog <crog@redhat.com>
2018-12-06 15:33:32 -0500

    Additional changes to the async job infrastructure
    - Moved the async jobs to a new job status table, tenatively named
      "cp2_async_jobs", with the matching job status entity using
      "AsyncJobStatus"
    - Added an AsyncJobStatusCurator for the new entity
    - Updated the JobManager to use the new AsyncJobStatus class where
      appropriate
    - Added stub classes for future changes to the way jobs are configured
      and queued: AsyncJob and JobBuilder

---
+++ server/src/main/java/org/candlepin/async/JobBuilder.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2009 - 2018 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.async;
+
+
+
+/**
+ * The JobManager manages the queueing, execution and general bookkeeping on jobs
+ */
+public class JobBuilder {
+
+
+
+
+
+}
+        //      JobBuilder builder = new JobBuilder()
+        //          .forTask(runnable job class or key here)
+        //          .setName("my_task")
+        //          .setTaskArgument("key", "value")
+        //          .setTaskArgument("key2", "value2")
+        //          .addUniqueRestriction("owner", owner_id_here)
+        //          .addUniqueRestriction("product", product_id_here)
+        //          .addTaskMetadata("correlation_id", cid)
+        //          .setRetryCount(3)
+        //
+        //      jobManager.queueJob(builder);
+
+
