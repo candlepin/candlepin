94b0dd58bd4bc5b4d1d8dc34241f06d98a093134
Alex Wood <awood@redhat.com>
2019-04-23 11:00:11 -0400

    Bring Gradle generated WAR in-line with Buildr WAR.
    * Disabling transitive dependencies in the "default" configuration
      wasn't sufficient to disable them in the generated WAR.  Originally, I
      had disabled them in the "default" configuration rather than the "all"
      configuration due to difficulties in getting the checkstyle plugin
      working, but that doesn't seem to be an issue any more.
    * Remove several dependencies that don't appear in our Buildr built WAR
      file.
    * Read the version out of the appropriate  *.spec.tmpl files for both
      Candlepin and Candlepin Common.
    * Currently the only differences between the Gradle WAR and the Buildr
      WAR are that the Build WAR includes BouncyCastle (which it isn't
      supposed to!) and the MySQL JDBC driver is in the Gradle WAR when it
      doesn't need to be.

--- buildSrc/src/main/java/org/candlepin/gradle/SpecVersion.java
+++ buildSrc/src/main/java/org/candlepin/gradle/SpecVersion.java
@@ -6,12 +6,26 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.ExtraPropertiesExtension;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Collections;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Class to pull the version number out of a spec file template
+ */
 public class SpecVersion implements Plugin<Project> {
-
     public void apply(Project project) throws GradleScriptException {
         // Read the version from the spec file
         String versionSearch = "\\s*Version:\\s*(.*?)\\s*$";
@@ -21,46 +35,69 @@ public class SpecVersion implements Plugin<Project> {
 
         String versionFromSpec = null;
         String releaseFromSpec = null;
-        String specFileName = project.getRootDir() + "/server/candlepin.spec.tmpl";
+        final Path[] specFile = new Path[1];
+
+        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:*.spec.tmpl");
+
         try {
-            File file = new File(specFileName);
+            Files.walkFileTree(project.getProjectDir().toPath(), Collections.emptySet(), 1,
+                new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                        if (pathMatcher.matches(file.getFileName())) {
+                            specFile[0] = file;
+                            return FileVisitResult.TERMINATE;
+                        }
+                        return FileVisitResult.CONTINUE;
+                    }
+                }
+            );
+
+            if (specFile[0] == null) {
+                throw new IOException("Could not file spec file in " + project.getProjectDir());
+            }
+
+            File file = specFile[0].toFile();
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 Matcher matcher = versionPattern.matcher(line);
-                if (matcher.find()){
+                if (matcher.find()) {
                     versionFromSpec = matcher.group(1);
                 }
                 matcher = releasePattern.matcher(line);
-                if (matcher.find()){
+                if (matcher.find()) {
                     releaseFromSpec = matcher.group(1);
                 }
-                if (versionFromSpec != null && releaseFromSpec != null)
+                if (versionFromSpec != null && releaseFromSpec != null) {
                     break;
+                }
             }
             fileReader.close();
-
-        } catch (IOException e) {
-            throw new GradleException("Error reading Spec File: " + specFileName, e);
+        }
+        catch (IOException e) {
+            throw new GradleException("Error reading spec file", e);
         }
 
         if (versionFromSpec != null) {
             project.getLogger().debug("Setting the project version to: " + versionFromSpec);
-            for (Project p : project.getAllprojects()){
+            for (Project p : project.getAllprojects()) {
                 p.setVersion(versionFromSpec);
             }
-        } else {
-            throw new GradleException("Unable to find a version in the spec file: " + specFileName);
+        }
+        else {
+            throw new GradleException("Unable to find a version in the spec file: " + specFile[0].toString());
         }
         if (releaseFromSpec != null) {
             project.getLogger().debug("Setting the project release to: " + releaseFromSpec);
-            for (Project p : project.getAllprojects()){
+            for (Project p : project.getAllprojects()) {
                 ExtraPropertiesExtension ep = p.getExtensions().getByType(ExtraPropertiesExtension.class);
                 ep.set("release", releaseFromSpec);
             }
-        } else {
-            throw new GradleException("Unable to find a version in the spec file: " + specFileName);
+        }
+        else {
+            throw new GradleException("Unable to find a version in the spec file: " + specFile[0].toString());
         }
     }
 }
