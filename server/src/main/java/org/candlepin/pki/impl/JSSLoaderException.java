44d36e37cc019a76867085fa9e1abfc786af8500
Nikos Moumoulidis <nmoumoul@redhat.com>
2019-07-31 14:48:22 +0200

    ENT-1408: Candlepin RHEL 8 support
    - Now the candlepin rpm depends on tomcatjss instead of
      jss and tomcat/pki-servlet-container/pki-servlet-engine
      to avoid these name changes between RHEL 7, RHEL8 and
      RHEL 8.1, since that package is comprehensively taking
      care of that itself.
    - The spec file and deploy script now update the
      META-INF/context.xml file to avoid breaking API changes
      between Tomcat 7 and 8+ when altering the classpath to
      include the jss jar from the filesystem.
    - Updated cpsetup script:
        * to be python3 compatible.
        * with server.xml and CA cert/key changes for Tomcat 8+
          compatibility.
    - JSSProvider/CryptoManager are now loaded once, during
      context initialization, and not during classloading of
      JSSProviderLoader.java.
    - Due to the fact that we no longer package the jss jar,
      but we load it from the OS, and because of compile-
      breaking changes between jss version 4.4.X and 4.5.0,
      we now use dynamic classloading and reflection to
      initialize the CryptoManager.
    - Added the *.fedorapeople.org repositories to parent
      pom to be able to do a maven build without having to
      download the artifacts they hold with buildr in the
      local .m2/repository.

---
+++ server/src/main/java/org/candlepin/pki/impl/JSSLoaderException.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2009 - 2019 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.pki.impl;
+
+/**
+ * Catastrophic error when initializing a JSS CryptoManager.
+ */
+class JSSLoaderException extends RuntimeException {
+
+    JSSLoaderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    JSSLoaderException(String message) {
+        super(message);
+    }
+}
