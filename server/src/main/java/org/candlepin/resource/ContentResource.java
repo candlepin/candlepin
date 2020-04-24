026bbb8ed47319be10aab4c64237c8938d72ff09
Ondrej Janus <ojanus@redhat.com>
2020-04-24 10:41:16 +0200

    ENT-1870: Port ContentResource to spec-first.

--- server/src/main/java/org/candlepin/resource/ContentResource.java
+++ server/src/main/java/org/candlepin/resource/ContentResource.java
@@ -14,86 +14,38 @@
  */
 package org.candlepin.resource;
 
-import org.candlepin.common.exceptions.BadRequestException;
 import org.candlepin.common.exceptions.NotFoundException;
-import org.candlepin.controller.PoolManager;
 import org.candlepin.dto.ModelTranslator;
 import org.candlepin.dto.api.v1.ContentDTO;
-import org.candlepin.model.CandlepinQuery;
 import org.candlepin.model.Content;
 import org.candlepin.model.ContentCurator;
-import org.candlepin.model.EnvironmentContentCurator;
-import org.candlepin.model.OwnerCurator;
-import org.candlepin.model.ProductCurator;
-import org.candlepin.service.UniqueIdGenerator;
 
 import com.google.inject.Inject;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.Authorization;
-
 import org.xnap.commons.i18n.I18n;
 
-import java.util.List;
+import java.util.Objects;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+public class ContentResource implements ContentApi {
 
-/**
- * ContentResource
- */
-@Path("/content")
-@Api(value = "content", authorizations = { @Authorization("basic") })
-public class ContentResource {
-
-    private ContentCurator contentCurator;
-    private I18n i18n;
-    private UniqueIdGenerator idGenerator;
-    private EnvironmentContentCurator envContentCurator;
-    private PoolManager poolManager;
-    private ProductCurator productCurator;
-    private OwnerCurator ownerCurator;
-    private ModelTranslator modelTranslator;
+    private final ContentCurator contentCurator;
+    private final I18n i18n;
+    private final ModelTranslator modelTranslator;
 
     @Inject
-    public ContentResource(ContentCurator contentCurator, I18n i18n, UniqueIdGenerator idGenerator,
-        EnvironmentContentCurator envContentCurator, PoolManager poolManager,
-        ProductCurator productCurator, OwnerCurator ownerCurator, ModelTranslator modelTranslator) {
-
-        this.i18n = i18n;
-        this.contentCurator = contentCurator;
-        this.idGenerator = idGenerator;
-        this.envContentCurator = envContentCurator;
-        this.poolManager = poolManager;
-        this.productCurator = productCurator;
-        this.ownerCurator = ownerCurator;
-        this.modelTranslator = modelTranslator;
+    public ContentResource(ContentCurator contentCurator, I18n i18n, ModelTranslator modelTranslator) {
+        this.i18n = Objects.requireNonNull(i18n);
+        this.contentCurator = Objects.requireNonNull(contentCurator);
+        this.modelTranslator = Objects.requireNonNull(modelTranslator);
     }
 
-    @ApiOperation(notes = "Retrieves list of Content", value = "list", response = ContentDTO.class,
-        responseContainer = "list")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public CandlepinQuery<ContentDTO> list() {
+    @Override
+    public Iterable<ContentDTO> listContent() {
         return this.modelTranslator.translateQuery(this.contentCurator.listAll(), ContentDTO.class);
     }
 
-    @ApiOperation(notes = "Retrieves a single Content", value = "getContent")
-    @ApiResponses({ @ApiResponse(code = 400, message = "") })
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/{content_uuid}")
-    public ContentDTO getContent(@PathParam("content_uuid") String contentUuid) {
+    @Override
+    public ContentDTO getContent(String contentUuid) {
         Content content = this.contentCurator.getByUuid(contentUuid);
 
         if (content == null) {
@@ -104,41 +56,4 @@ public class ContentResource {
         return this.modelTranslator.translate(content, ContentDTO.class);
     }
 
-    @ApiOperation(notes = "Creates a Content", value = "createContent")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public ContentDTO createContent(ContentDTO content) {
-        throw new BadRequestException(this.i18n.tr(
-            "Organization-agnostic content write operations are not supported."));
-    }
-
-    @ApiOperation(notes = "Creates Contents in bulk", value = "createBatchContent")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("/batch")
-    public Iterable<ContentDTO> createBatchContent(List<ContentDTO> contents) {
-        throw new BadRequestException(this.i18n.tr(
-            "Organization-agnostic content write operations are not supported."));
-    }
-
-    @ApiOperation(notes = "Updates a Content", value = "updateContent")
-    @PUT
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("/{content_uuid}")
-    public ContentDTO updateContent(@PathParam("content_uuid") String contentUuid, ContentDTO changes) {
-        throw new BadRequestException(this.i18n.tr(
-            "Organization-agnostic content write operations are not supported."));
-    }
-
-    @ApiOperation(notes = "Deletes a Content", value = "remove")
-    @DELETE
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/{content_uuid}")
-    public void remove(@PathParam("content_uuid") String contentUuid) {
-        throw new BadRequestException(this.i18n.tr(
-            "Organization-agnostic content write operations are not supported."));
-    }
 }
