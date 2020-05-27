72a68d738978d101da6e2050c428c06c3f93a8fa
Chris Rog <crog@redhat.com>
2020-05-27 16:59:45 +0000

    1840823: Fixed parameter overflow in bulk entitlement deletion
    - Fixed an issue during entitlement deletion that could cause a
      query to be generated that attempts to use more parameters than
      the backend database supports
    - Flagged the bulkSQLDelete method as deprecated, as it lacks
      protection for the database parameter limit

--- server/src/main/java/org/candlepin/controller/CandlepinPoolManager.java
+++ server/src/main/java/org/candlepin/controller/CandlepinPoolManager.java
@@ -2255,7 +2255,6 @@ public class CandlepinPoolManager implements PoolManager {
             Set<Entitlement> entitlements = new HashSet<>();
 
             if (!entitlementIds.isEmpty()) {
-                log.debug("IN BLOCK SIZE: {}", this.entitlementCurator.getInBlockSize());
                 Iterable<List<String>> blocks =
                     Iterables.partition(entitlementIds, this.entitlementCurator.getInBlockSize());
 
@@ -2340,6 +2339,7 @@ public class CandlepinPoolManager implements PoolManager {
                 this.poolCurator.updateAll(poolsToSave, false, false);
                 this.consumerCurator.updateAll(consumerStackedEnts.keySet(), false, false);
                 this.consumerCurator.flush();
+
                 log.info("Entitlement counts successfully updated for {} pools and {} consumers",
                     poolsToSave.size(), consumerStackedEnts.size());
 
