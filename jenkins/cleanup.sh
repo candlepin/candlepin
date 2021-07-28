4bbeba5bb2c20f4bf8c478c468a8d226b8a753d9
Ondrej Janus <ojanus@redhat.com>
2021-07-28 13:44:24 +0200

    test

--- jenkins/cleanup.sh
+++ jenkins/cleanup.sh
@@ -4,14 +4,23 @@ env | sort
 echo
 
 # The docker container test script will know to copy out
-echo "Using workspace: $WORKSPACE"
-mkdir -p $WORKSPACE/artifacts/
+echo "Using workspace: ${WORKSPACE}"
+mkdir -p ${WORKSPACE}/artifacts/
 
 # make selinux happy via http://stackoverflow.com/a/24334000
 chcon -Rt svirt_sandbox_file_t $WORKSPACE//artifacts/
 
+# PROJ_NAME should be set in a jenkins environment. It allows multiple
+#  instances of the compose to run without clobbering each other.
+DIR="$(git rev-parse --show-toplevel)/docker/"
+cd $DIR
+
 # Run the cleanup
-./docker/test -w -n "${STAGE_NAME}-${BUILD_TAG}"
+docker-compose $PROJ_NAME down
 RETVAL=$?
-sudo chown -R jenkins:jenkins $WORKSPACE/artifacts
+
+echo "return value: $RETVAL"
+cd -
+
+sudo chown -R jenkins:jenkins ${WORKSPACE}/artifacts
 exit $RETVAL
