46794ee0d2518f2bcee92a9d8b9e3f54af54a9a2
Chris Rog <crog@redhat.com>
2022-03-15 14:30:27 -0400

    Fixed hostname/address cert generation issues with gen_certs.sh
    - Fixed a bug where cert generation would fail on any system with
      multiple addresses or interfaces
    - Added options for passing various arguments to the key and
      cert generation operations
    - Updated cpsetup to avoid a similar issue with cert generation

--- bin/deployment/gen_certs.sh
+++ bin/deployment/gen_certs.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+SCRIPT_NAME=$(basename "$0")
+
+# This works fine for our dev deployment -- especially within Vagrant images -- but may not work as
+# expected on systems with multiple interfaces or multiple external IPs (i.e. IPv4 and IPv6). On
+# such systems, this script should be run with explicit arguments for these values.
+HOSTNAME=$(hostname)
+HOST_ADDR=$(hostname -I | cut -d ' ' -f 1)
+
 CERT_HOME='/etc/candlepin/certs'
 CERT_NAME='candlepin-ca'
 
@@ -26,8 +34,6 @@ generate_certs() {
     # Ensure we have all of our dependencies available
     check_dependency openssl
 
-    echo "Generating certificates..."
-
     # prep environment, noise and password files
     rm -rf $CERT_HOME
     mkdir -p $CERT_HOME
@@ -35,13 +41,15 @@ generate_certs() {
     openssl rand -base64 -out $PASSWORD_FILE 24
 
     # Generate keys
-    openssl genpkey -out $CA_KEY -pass "file:${PASSWORD_FILE}" -algorithm rsa -pkeyopt rsa_keygen_bits:$CERT_BIT_LENGTH 2> /dev/null
+    echo "Generating keys..."
+    openssl genpkey -out $CA_KEY -pass "file:${PASSWORD_FILE}" -algorithm rsa -pkeyopt rsa_keygen_bits:$CERT_BIT_LENGTH
     openssl pkey -pubout -in $CA_KEY -out $CA_PUB_KEY
 
     # Generate our cert, using the newer options if available
+    echo "Generating certificates..."
     if openssl req --help 2>&1 | grep -q addext; then
-        openssl req -new -x509 -days $CA_CERT_DAYS -key $CA_KEY -out $CA_CERT -subj "/CN=$(hostname)/C=US/L=Raleigh/" \
-            -addext "subjectAltName=IP:127.0.0.1, IP:$(hostname -I), DNS:localhost, DNS:$(hostname)"
+        openssl req -new -x509 -days $CA_CERT_DAYS -key $CA_KEY -out $CA_CERT -subj "/CN=${HOSTNAME}/C=US/L=Raleigh/" \
+            -addext "subjectAltName=IP:127.0.0.1, IP:${HOST_ADDR}, DNS:localhost, DNS:${HOSTNAME}"
     else
         openssl req -new -x509 -days $CA_CERT_DAYS -key $CA_KEY -out $CA_CERT \
             -subj "/CN=$(hostname)/C=US/L=Raleigh/" \
@@ -56,9 +64,9 @@ generate_certs() {
                 echo subjectAltName = @san
                 echo "[san]"
                 echo IP.1 = 127.0.0.1
-                echo IP.2 = $(hostname -I)
+                echo IP.2 = $HOST_ADDR
                 echo DNS.1 = localhost
-                echo DNS.2 = $(hostname) )
+                echo DNS.2 = $HOSTNAME )
     fi
 
     chmod a+r ${CERT_HOME}/*
@@ -90,13 +98,39 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-while getopts ":f" opt; do
+print_usage() {
+    cat <<USAGE
+usage: ${SCRIPT_NAME} [options]
+
+OPTIONS:
+  -a <addr>   The external address of the Candlepin host that will be using the
+              generated certs; defaults to the first address returned by
+              \"hostname -I\"
+  -H <host>   The hostname of the Candlepin host that will be using the
+              generated certs; defaults to the output of the "hostname" command
+  -b <bits>   The length in bits of the generated keys to use to generate the
+              certificates; defaults to 4096
+  -d <days>   The number of days for which the generated certificates will be
+              valid; defaults to 365
+  -f          Force certificate regeneration, even if the certificates have
+              already been generated
+  -h          Display command usage information
+USAGE
+}
+
+while getopts ":a:H:b:d:fh" opt; do
     case $opt in
-        f  ) FORCECERT="1" ;;
+        a  ) HOST_ADDR="${OPTARG}" ;;
+        H  ) HOSTNAME="${OPTARG}" ;;
+        b  ) CERT_BIT_LENGTH=$OPTARG ;;
+        d  ) CA_CERT_DAYS=$OPTARG ;;
+        f  ) FORCE_REGEN="1" ;;
+        h  ) print_usage; exit 0;;
+        ?  ) print_usage; exit 1;;
     esac
 done
 
-if [ -f $CA_KEY ] && [ -f $CA_CERT ] && [ "$FORCECERT" != "1" ]; then
+if [ -f $CA_KEY ] && [ -f $CA_CERT ] && [ "${FORCE_REGEN}" != "1" ]; then
     echo "Certificates already generated; using existing certs"
 else
     generate_certs
