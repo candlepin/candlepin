297c286fe5e26c98fe6c0cbd86b82853628aafeb
Ondrej Janus <ojanus@redhat.com>
2022-08-24 10:16:13 +0200

    Add option to use external config for spec tests
    - Check for existence of ~/.candlepin_spec.conf
    - Check for existence of ./.candlepin_spec.conf
    - If exists, use it to override default options
    - Command line options still have the highest priority

---
+++ spec-tests/src/main/java/org/candlepin/spec/bootstrap/client/ExternalProperties.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2009 - 2022 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package org.candlepin.spec.bootstrap.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.function.Supplier;
+
+/**
+ * Class for loading spec test configuration from external config files.
+ * <p>
+ * The order of config file paths determines their priority. Paths specified
+ * later have higher priority.
+ */
+public class ExternalProperties implements Supplier<Properties> {
+
+    private static final Logger log = LoggerFactory.getLogger(ExternalProperties.class);
+
+    private final List<Path> sources;
+
+    public ExternalProperties(List<Path> sources) {
+        if (sources == null || sources.isEmpty()) {
+            throw new IllegalArgumentException("At least one path to configuration file must be provided!");
+        }
+        this.sources = sources;
+    }
+
+    public ExternalProperties(Path... sources) {
+        this(Arrays.asList(sources));
+    }
+
+    @Override
+    public Properties get() {
+        Properties properties = new Properties();
+
+        this.sources.stream()
+            .filter(this::isRegularFile)
+            .forEach(path -> loadProperties(properties, path));
+
+        return properties;
+    }
+
+    private boolean isRegularFile(Path path) {
+        if (Files.isRegularFile(path)) {
+            return true;
+        }
+        log.warn("External config file: {} was not found!", path);
+        return false;
+    }
+
+    private void loadProperties(Properties properties, Path path) {
+        try (InputStream input = Files.newInputStream(path)) {
+            properties.load(input);
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Error occurred while reading the external properties!", e);
+        }
+    }
+
+}
---
+++ spec-tests/src/test/java/org/candlepin/spec/bootstrap/client/ExternalPropertiesTest.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2009 - 2022 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package org.candlepin.spec.bootstrap.client;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+class ExternalPropertiesTest {
+
+    private static final String KEY = "content";
+    private static final String VALUE = "bar";
+    private FileSystem fs;
+
+    @BeforeEach
+    void beforeEach() {
+        fs = Jimfs.newFileSystem(Configuration.unix());
+    }
+
+    @AfterEach
+    void afterEach() throws IOException {
+        fs.close();
+    }
+
+    @Test
+    void canReadProperties() {
+        Path configFile = fs.getPath("/config", ".candlepin_spec.conf");
+        createFile(configFile, KEY + "=" + VALUE);
+
+        ExternalProperties properties = new ExternalProperties(configFile);
+
+        assertThat(properties.get())
+            .containsEntry(KEY, VALUE);
+    }
+
+    @Test
+    void laterFilesShouldHavePriority() {
+        String priorityValue = VALUE;
+        Path configFile1 = fs.getPath("/config", ".candlepin_spec1.conf");
+        createFile(configFile1, KEY + "=" + VALUE);
+        Path configFile2 = fs.getPath("/other_config", ".candlepin_spec2.conf");
+        createFile(configFile2, KEY + "=" + priorityValue);
+
+        ExternalProperties properties = new ExternalProperties(configFile1, configFile2);
+
+        assertThat(properties.get())
+            .containsEntry(KEY, priorityValue);
+    }
+
+    @Test
+    void configPathsMustBeProvided() {
+        assertThatThrownBy(ExternalProperties::new)
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static void createFile(Path path, String content) {
+        try {
+            Files.createDirectories(path.getParent());
+            Files.createFile(path);
+            Files.write(path, List.of(content), StandardCharsets.UTF_8);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
