9f600d42dce22965040fb7f5b9366376abe028b4
Josh Albrecht <joshmalbrecht@gmail.com>
2022-09-29 17:10:07 -0400

    2131997: Updated GuestIdDeserializer to use existing ObjectMapper

--- src/main/java/org/candlepin/resteasy/JsonProvider.java
+++ src/main/java/org/candlepin/resteasy/JsonProvider.java
@@ -18,6 +18,7 @@ import org.candlepin.config.ConfigProperties;
 import org.candlepin.config.Configuration;
 import org.candlepin.dto.api.v1.AsyncJobStatusDTO;
 import org.candlepin.dto.api.v1.ConsumerTypeDTO;
+import org.candlepin.dto.api.v1.GuestIdDTO;
 import org.candlepin.dto.api.v1.PoolDTO;
 import org.candlepin.dto.api.v1.PoolQuantityDTO;
 import org.candlepin.dto.api.v1.ProductDTO;
@@ -99,14 +100,16 @@ public class JsonProvider extends JacksonJsonProvider {
             null));
         // Ensure our DateSerializer is used for all Date objects
         customModule.addSerializer(Date.class, new DateSerializer());
-        // Ensure we handle releaseVer fields properly
-        customModule.addDeserializer(ReleaseVerDTO.class, new ReleaseVersionWrapDeserializer());
-        customModule.addDeserializer(ConsumerTypeDTO.class, new ConsumerTypeDeserializer());
-        customModule.addDeserializer(GuestIdDTO.class, new GuestIdDeserializer());
-        mapper.registerModule(customModule);
 
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         configureHateoasObjectMapper(mapper, indentJson);
+
+        // Ensure we handle releaseVer fields properly
+        customModule.addDeserializer(ReleaseVerDTO.class, new ReleaseVersionWrapDeserializer());
+        customModule.addDeserializer(ConsumerTypeDTO.class, new ConsumerTypeDeserializer());
+        customModule.addDeserializer(GuestIdDTO.class, new GuestIdDeserializer(mapper.reader()));
+        mapper.registerModule(customModule);
+
         setMapper(mapper);
     }
 
