71b93227e00d75909c67ea29f9b884e36777f5bd
Barnaby Court <bcourt@redhat.com>
2022-10-03 15:50:07 -0400

    Try running unit tests natively

--- jenkins/unit-tests.sh
+++ jenkins/unit-tests.sh
@@ -5,14 +5,18 @@ echo
 
 # The docker container test script will know to copy out
 echo "Using workspace: $WORKSPACE"
-mkdir -p $WORKSPACE/artifacts/
+#mkdir -p $WORKSPACE/artifacts/
 
 # make selinux happy via http://stackoverflow.com/a/24334000
-chcon -Rt svirt_sandbox_file_t $WORKSPACE//artifacts/
+#chcon -Rt svirt_sandbox_file_t $WORKSPACE//artifacts/
 
 # Run the Candlepin unit tests
-./docker/test -x -c "cp-test -u -c ${CHANGE_BRANCH}" -n "${STAGE_NAME}-${BUILD_TAG}"
-RETVAL=$?
-sudo chown -R jenkins:jenkins $WORKSPACE/artifacts
-mv $WORKSPACE/artifacts "${WORKSPACE}/${STAGE_NAME}-artifacts"
-exit $RETVAL
+#./docker/test -x -c "cp-test -u -c ${CHANGE_BRANCH}" -n "${STAGE_NAME}-${BUILD_TAG}"
+#RETVAL=$?
+#sudo chown -R jenkins:jenkins $WORKSPACE/artifacts
+#mv $WORKSPACE/artifacts "${WORKSPACE}/${STAGE_NAME}-artifacts"
+#exit $RETVAL
+
+# The only thing we need to do is run gradle test using the local java as the unit tets don't need
+# a running candlepin or any other services than java.
+./gradlew test
