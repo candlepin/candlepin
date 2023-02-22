43ebaf0f7f9f5b7911c9fb0424d2f2f2a47d8294
Ondrej Janus <ojanus@redhat.com>
2023-02-22 10:31:42 +0100

    Fix workspace ownership
    When Jenkins stage runs in docker, all build output is created under
    root user which prevents unprivileged staages from working.
    
    This change returns ownership of workspace to jenkins user after each
    such stage.

--- jenkins/lint.sh
+++ jenkins/lint.sh
@@ -13,5 +13,5 @@ chcon -Rt svirt_sandbox_file_t $WORKSPACE//artifacts/
 # Run the linter
 ./docker/test -p -c "cp-test -l -c ${CHANGE_BRANCH}" -n "${STAGE_NAME}-${BUILD_TAG}"
 RETVAL=$?
-sudo chown -R jenkins:jenkins $WORKSPACE/artifacts
+sudo chown -R jenkins:jenkins $WORKSPACE
 exit $RETVAL
