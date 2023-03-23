809d5ddd7c8ab59d685b3d8e6d107bb7cbcc48d4
Ondrej Janus <ojanus@redhat.com>
2023-03-23 13:57:38 +0100

    Remove tests

---
+++ src/main/java/org/candlepin/cache/CandlepinCache.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2009 - 2012 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.cache;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import javax.inject.Singleton;
+
+/**
+ * Wrapper that makes it easier to retrieve various caches in Candlepin
+ * @author fnguyen
+ *
+ */
+@ApplicationScoped
+public class CandlepinCache {
+    /**
+     * We use our own version of a status cache as we ran
+     * into some issues with ehcache-jcache impl.
+     */
+    private final StatusCache statusCache;
+
+    @Inject
+    public CandlepinCache(StatusCache cache) {
+        // Safe to create this as many times as you'd like
+        // since the same static Status instance will be
+        // reused across all instances.
+        this.statusCache = cache;
+    }
+
+    /**
+     * Retrieves Candlepin Status cache. This cache will be used only to cache
+     * status resource responses.
+     *
+     * @return StatusCache for Status entity
+     */
+    public StatusCache getStatusCache() {
+        return this.statusCache;
+    }
+
+}
---
+++ src/main/java/org/candlepin/cache/JCacheManagerProvider.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2009 - 2012 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.cache;
+
+import static org.candlepin.config.ConfigProperties.CACHE_CONFIG_FILE_URI;
+
+import org.candlepin.config.Configuration;
+import org.candlepin.config.HibernateConfig;
+
+import com.google.inject.Provider;
+
+import java.net.URISyntaxException;
+import java.util.Objects;
+
+import javax.cache.CacheException;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+import javax.inject.Inject;
+
+/**
+ * Provides object cache by indexed by String.
+ *
+ * @author fnguyen
+ */
+public class JCacheManagerProvider implements Provider<CacheManager> {
+
+    private final HibernateConfig config;
+
+    @Inject
+    JCacheManagerProvider(HibernateConfig config) {
+        this.config = Objects.requireNonNull(config);
+    }
+
+    /**
+     * It is safe to bind this as singleton, the CacheManager is supposed to
+     * be thread safe.
+     * https://github.com/ehcache/ehcache-jcache/issues/41
+     */
+    @Override
+    public CacheManager get() {
+        CacheManager cacheManager = null;
+
+        CachingProvider cachingProvider = Caching.getCachingProvider();
+        String cacheManagerUri = config.cacheUri();
+
+        try {
+            ClassLoader defaultClassLoader = cachingProvider.getDefaultClassLoader();
+            cacheManager = cachingProvider
+                .getCacheManager(defaultClassLoader.getResource(cacheManagerUri).toURI(), defaultClassLoader);
+
+            return cacheManager;
+        }
+        catch (URISyntaxException e) {
+            throw new CacheException("Couldn't load URI from " + cacheManagerUri, e);
+        }
+    }
+}
