503fc2fc0fe603ce6004841a755008efe8109f8d
Pino Toscano <ptoscano@redhat.com>
2023-08-01 06:08:32 +0200

    Drop ruby/rvm/bundle leftovers
    ruby was mostly dropped few months ago, and few related bits of it are
    still there; since they are not actually of any use, drop them:
    - drop the custom gai.conf for glibc in containers: it was used only
      to workaround network issues when connecting to rubygems.org
    - stop installing ruby-devel in containers
    - stop installing rvm and ruby dependencies in containers
    - drop the "|| true" workaround in cp-test: apparently needed only for
      the rvm.sh shell profile script
    - drop Gemfile & Gemfile.lock, no more needed since bundle is no more
      called from anywhere (hopefully)
    - drop bits & comments in other places

--- containers/candlepin-base/setup-devel-env.sh
+++ containers/candlepin-base/setup-devel-env.sh
@@ -30,7 +30,6 @@ PACKAGES=(
     rpm-build
     rpm-sign
     rsyslog
-    ruby-devel
     tomcat
     which
     wget
@@ -61,23 +60,6 @@ BASHRC
 
 git clone https://github.com/candlepin/candlepin.git /candlepin
 cd /candlepin
-
-# Setup and install rvm, ruby and pals
-gpg2 --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-# turning off verbose mode, rvm is nuts with this
-set +v
-curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer
-curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer.asc
-gpg2 --verify rvm-installer.asc && bash rvm-installer stable
-source /etc/profile.d/rvm.sh || true
-
-rvm install 2.5.3
-rvm use --default 2.5.3
-set -v
-
-# Install all ruby deps
-gem install bundler -v 1.16.1
-bundle install --without=proton --retry=5 --verbose --full-index
 
 # Installs all Java deps into the image, big time saver
 ./gradlew --no-daemon dependencies
