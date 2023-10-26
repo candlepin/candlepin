163cd0f5b77115a3690acd02843b8c9b6573a409
William Poteat <wpoteat@redhat.com>
2023-10-26 13:42:40 -0400

    CANDLEPIN-693: Fix flaky VirtSpecTest

--- spec-tests/src/test/java/org/candlepin/spec/VirtSpecTest.java
+++ spec-tests/src/test/java/org/candlepin/spec/VirtSpecTest.java
@@ -52,6 +52,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -758,6 +759,7 @@ class VirtSpecTest {
 
     private PoolDTO findGuestPool(List<PoolDTO> pools) {
         return pools.stream()
+            .sorted(Comparator.comparing(PoolDTO::getId))
             .filter(poolDTO -> poolDTO.getSourceEntitlement() != null)
             .findFirst()
             .orElseThrow();
