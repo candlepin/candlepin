0214f27f28992548f1ab6954008de3f90d0571ea
Chris "Ceiu" Rog <crog@redhat.com>
2024-03-29 20:41:09 +0000

    CANDLEPIN-839: Updated schema to support environment content overrides
    - Updated the database schema to support environment content overrides
    - Added indexes to the consumer_id and the new environment_id
      columns to improve content override lookup performance
    - Added a constraint to the content override table to help prevent
      creating overrides in invalid states
    - Removed legacy constructors for content overrides and the various
      subclasses
    - Removed the allowance of null values on the content override
      subclasses which add a "parent" entity reference
    - Removed old XML annotations on the content override entities

---
+++ src/test/java/org/candlepin/model/EnvironmentContentOverrideTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2009 - 2024 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.junit.jupiter.api.Test;
+
+
+
+/**
+ * Test suite for the EnvironmentContentOverride class
+ */
+public class EnvironmentContentOverrideTest extends ContentOverrideTest<EnvironmentContentOverride> {
+
+    /**
+     * @{inheritDoc}
+     */
+    @Override
+    protected EnvironmentContentOverride getTestInstance() {
+        return new EnvironmentContentOverride();
+    }
+
+    @Test
+    public void testGetSetEnvironment() {
+        Environment environment = new Environment();
+        EnvironmentContentOverride override = this.getTestInstance();
+
+        // The getParent method is synonymous with fetching the environment for EnvContentOverrides
+        assertNull(override.getEnvironment());
+        assertNull(override.getParent());
+
+        EnvironmentContentOverride output = override.setEnvironment(environment);
+        assertSame(override, output);
+
+        assertEquals(environment, output.getEnvironment());
+        assertEquals(environment, output.getParent());
+    }
+
+    @Test
+    public void testGetSetNullEnvironment() {
+        EnvironmentContentOverride override = this.getTestInstance();
+
+        // The getParent method is synonymous with fetching the environment for EnvContentOverrides
+        assertNull(override.getEnvironment());
+        assertNull(override.getParent());
+
+        EnvironmentContentOverride output = override.setEnvironment(null);
+        assertSame(override, output);
+
+        assertNull(output.getEnvironment());
+        assertNull(output.getParent());
+    }
+
+    @Test
+    public void testGetSetParent() {
+        Environment environment = new Environment();
+        EnvironmentContentOverride override = this.getTestInstance();
+
+        // The getParent method is synonymous with fetching the environment for EnvContentOverrides
+        assertNull(override.getParent());
+        assertNull(override.getEnvironment());
+
+        EnvironmentContentOverride output = override.setParent(environment);
+        assertSame(override, output);
+
+        assertEquals(environment, output.getParent());
+        assertEquals(environment, output.getEnvironment());
+    }
+
+    @Test
+    public void testGetSetNullParent() {
+        EnvironmentContentOverride override = this.getTestInstance();
+
+        // The getParent method is synonymous with fetching the environment for EnvContentOverrides
+        assertNull(override.getParent());
+        assertNull(override.getEnvironment());
+
+        EnvironmentContentOverride output = override.setParent(null);
+        assertSame(override, output);
+
+        assertNull(output.getParent());
+        assertNull(output.getEnvironment());
+    }
+
+}
