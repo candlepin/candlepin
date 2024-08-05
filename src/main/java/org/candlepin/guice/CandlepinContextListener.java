8d8be8152d735982a943091ffd24a885a522ad56
Josh Albrecht <joshmalbrecht@gmail.com>
2024-08-05 15:42:39 -0400

    CANDLEPIN-898: Create EventAdapter interface
    - Created the EventAdapter interface.
    - Updated CandlepinContextListener class to call the EventAdapter
      startup and shutdown method during the application lifecycle.

--- src/main/java/org/candlepin/guice/CandlepinContextListener.java
+++ src/main/java/org/candlepin/guice/CandlepinContextListener.java
@@ -29,6 +29,7 @@ import org.candlepin.logging.LoggingConfigurator;
 import org.candlepin.messaging.CPMContextListener;
 import org.candlepin.resteasy.MethodLocator;
 import org.candlepin.resteasy.ResourceLocatorMap;
+import org.candlepin.service.EventAdapter;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Injector;
@@ -108,6 +109,7 @@ public class CandlepinContextListener extends GuiceResteasyBootstrapServletConte
 
     private ActiveMQContextListener activeMQContextListener;
     private JobManager jobManager;
+    private EventAdapter eventAdapter;
     private LoggerContextListener loggerListener;
 
     // a bit of application-initialization code. Not sure if this is the
@@ -206,6 +208,9 @@ public class CandlepinContextListener extends GuiceResteasyBootstrapServletConte
 
         loggerListener = injector.getInstance(LoggerContextListener.class);
 
+        eventAdapter = injector.getInstance(EventAdapter.class);
+        eventAdapter.initialize();
+
         this.injector = injector;
     }
 
@@ -249,6 +254,8 @@ public class CandlepinContextListener extends GuiceResteasyBootstrapServletConte
                 log.info("Failed to de-registering driver {}", driver, e);
             }
         }
+
+        eventAdapter.shutdown();
 
         if (config.getBoolean(ACTIVEMQ_ENABLED)) {
             activeMQContextListener.contextDestroyed(injector);
